c9ba8b5f895421b075cd7a83ab357255d806c19f
tkwasnik <ted.kwasnik@nrel.gov>
2016-12-16 18:05:16 -0700

    removed csrf middleware in settings files

--- reopt_api/dev_settings.py
+++ reopt_api/dev_settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = (
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
