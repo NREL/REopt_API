56a8a1974e8e6bdeb35a3a5f59522159b1956488
Nick DiOrio <nicholas.diorio@nrel.gov>
2017-02-08 15:27:12 -0700

    Add validation test for NEM, update migration to fix misspelling

--- reo/pvwatts.py
+++ reo/pvwatts.py
@@ -68,7 +68,6 @@ class PVWatts:
 
     def download_locations(self):
         url = self.make_url()
-        log("DEBUG", "PVWatts: " + str(url))
         r = requests.get(url, verify=True)
         data = json.loads(r.text)
         self.compute_prod_factor(data)
--- tests/test_library.py
+++ tests/test_library.py
@@ -2,16 +2,21 @@ import unittest
 
 from reo.library import DatLibrary
 from reo.pro_forma import ProForma
+from enum import Enum
 
 import json
 import os
 import shutil
 import filecmp
 
-class RunLibraryTest(unittest.TestCase):
 
-    scenario_num = 0
-    remove_files = False
+class ValidationCases(Enum):
+
+    BASE_CASE = 0
+    NEM_CASE = 1
+
+
+class RunLibraryTests(unittest.TestCase):
 
     # generated results
     path_run = None
@@ -25,58 +30,60 @@ class RunLibraryTest(unittest.TestCase):
     path_valid_output = None
     path_valid_output_bau = None
 
-    # filenames
-    filename_economics = None
-    filename_economics_bau = None
-    filename_GIS = None
-    filename_GIS_bau = None
-
     # data
     file_scenario = "post_json.txt"
-    json_data = {}
-    run_set = []
 
-    def setUp(self):
+    def setUpScenario(self, scenario_num):
 
-        self.path_valid_run = os.path.join("tests", "validation_scenario_" + str(self.scenario_num))
+        self.path_valid_run = os.path.join("tests", "validation_scenario_" + str(scenario_num))
         self.path_valid_input = os.path.join(self.path_valid_run, "Inputs")
         self.path_valid_output = os.path.join(self.path_valid_run, "Outputs")
         self.path_valid_output_bau = os.path.join(self.path_valid_run, "Outputs_bau")
 
-        self.filename_economics = "economics_" + str(self.scenario_num) + ".dat"
-        self.filename_economics_bau = "economics_" + str(self.scenario_num) + "_bau.dat"
-
-        self.filename_GIS = "GIS_" + str(self.scenario_num) + ".dat"
-        self.filename_GIS_bau = "GIS_" + str(self.scenario_num) + "_bau.dat"
-
         with open(os.path.join(self.path_valid_run, self.file_scenario)) as json_file:
-            self.json_data = json.load(json_file)
+            return json.load(json_file)
 
-    def tearDown(self):
+    def tearDownScenario(self):
 
-        if self.remove_files:
-            if os.path.exists(self.path_input):
-                shutil.rmtree(self.path_input)
-            if os.path.exists(self.path_output):
-                shutil.rmtree(self.path_output)
-            if os.path.exists(self.path_output_bau):
-                shutil.rmtree(self.path_output_bau)
+        if os.path.exists(self.path_input):
+            shutil.rmtree(self.path_input)
+        if os.path.exists(self.path_output):
+            shutil.rmtree(self.path_output)
+        if os.path.exists(self.path_output_bau):
+            shutil.rmtree(self.path_output_bau)
 
-    def test_run(self):
+    def get_output_paths(self, run_set):
 
-        self.run_set = DatLibrary(0, self.json_data)
-        self.run_set.run()
+        self.path_run = run_set.get_path_run()
+        self.path_input = run_set.get_path_run_inputs()
+        self.path_output = run_set.get_path_run_outputs()
+        self.path_output_bau = run_set.get_path_run_outputs_bau()
 
-        self.path_run = self.run_set.get_path_run()
-        self.path_input = self.run_set.get_path_run_inputs()
-        self.path_output = self.run_set.get_path_run_outputs()
-        self.path_output_bau = self.run_set.get_path_run_outputs_bau()
+    def compare_directory_contents(self):
 
-        self.assertTrue(filecmp.dircmp(self.path_input, self.path_valid_input), "Generated results for input not valid!")
-        self.assertTrue(filecmp.dircmp(self.path_output, self.path_valid_output), "Generated results for output not valid!")
-        self.assertTrue(filecmp.dircmp(self.path_output_bau, self.path_valid_output_bau), "Generated results for output not valid!")
+        self.assertTrue(filecmp.dircmp(self.path_input, self.path_valid_input),
+                        "Generated results for input not valid!")
+        self.assertTrue(filecmp.dircmp(self.path_output, self.path_valid_output),
+                        "Generated results for output not valid!")
+        self.assertTrue(filecmp.dircmp(self.path_output_bau, self.path_valid_output_bau),
+                        "Generated results for output not valid!")
 
-        self.remove_files = True
+    def run_scenario(self, scenario_num):
 
+        json_data = self.setUpScenario(scenario_num)
+        run_set = DatLibrary(scenario_num, json_data)
+        run_set.run()
+        self.get_output_paths(run_set)
+        self.compare_directory_contents()
+        self.tearDownScenario()
 
+    def test_base_case(self):
+
+        scenario_num = ValidationCases.BASE_CASE
+        self.run_scenario(scenario_num)
+
+    def test_nem_case(self):
+
+        scenario_num = ValidationCases.NEM_CASE
+        self.run_scenario(scenario_num)
 
