8ba08910cdd809a5b77f52acb93cbcf3c3b5b587
Nick Muerdter <nick.muerdter@nrel.gov>
2017-05-09 09:46:17 -0600

    Add API endpoint for returning API definitions in JSON format.
    This is for use in the web app so we can better ensure that our
    validations are lined up between the web app and API.

--- reo/urls.py
+++ reo/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$',views.index, name='index'),
+    url(r'^definition/',views.definition, name='definition'),
     url(r'^check_inputs/',views.check_inputs, name='check_inputs'),
     url(r'^downloads/',views.get_download, name='get_download'),
 ]
