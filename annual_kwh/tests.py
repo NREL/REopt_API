82a398f98134d614124fd469bfb4469a87b4283e
Nick Laws <laws.nick@gmail.com>
2017-08-06 17:18:21 -0600

    new endpoint for GET'ing annual kwh of built-in profile

---
+++ annual_kwh/tests.py
@@ -0,0 +1,19 @@
+import unittest
+
+
+class TestAnnualkWh(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_random_building(self):
+        pass
+
+    def test_bad_latitude(self):
+        pass
+
+    def test_bad_building_name(self):
+        pass
