4c8be6b14a50f39d418faa337e00102419394b4b
Nick Laws <laws.nick@gmail.com>
2017-08-09 09:50:28 -0600

    erroneously assumed year_one_battery_soc_series is not None
    If the outage simulator was hit with no battery (and with PV) then the
    validator raised error. This commit fixes this issue. Example get that
    was failing:
    https://reopt-dev-api1.nrel.gov/resilience_stats/?run_uuid=343d0eba-0a9b
    -47f8-943b-be28e2a40970
    
    Additional fixes to outage_simulator and validation for handling zero
    pv_kw and not requiring battery efficiency inputs unless batt_kw > 0.

--- resilience_stats/validators.py
+++ resilience_stats/validators.py
@@ -15,23 +15,21 @@ def validate_RunOutput_for_outage_simulator(ro):
     if ro.load_8760_kw is None:
         raise ValidationError('Scenario load profile undefined')
 
-    if ro.pv_kw_ac_hourly is None:
-        raise ValidationError('Scenario PV production undefined')
-
-    if ro.year_one_battery_soc_series is None:
-        raise ValidationError('Scenario battery state-of-charge undefined')
+    if ro.pv_kw > 0 and ro.pv_kw_ac_hourly is None:
+        raise ValidationError('Scenario PV production undefined (with pv_kw > 0)')
 
     if ro.crit_load_factor is None:
         raise ValidationError('Scenario critical load factor undefined')
 
-    if ro.batt_efficiency is None:
-        raise ValidationError('Scenario battery efficiency undefined')
+    if ro.batt_kw > 0 and ro.batt_efficiency is None:
+        raise ValidationError('Scenario battery efficiency undefined (with batt_kw > 0)')
 
-    if ro.batt_inverter_efficiency is None:
-        raise ValidationError('Scenario battery inverter efficiency undefined')
+    if ro.batt_kw > 0 and ro.batt_inverter_efficiency is None:
+        raise ValidationError('Scenario battery inverter efficiency undefined (with batt_kw > 0)')
 
-    if ro.batt_rectifier_efficiency is None:
-        raise ValidationError('Scenario battery rectifier efficiency undefined')
+    if ro.batt_kw > 0 and ro.batt_rectifier_efficiency is None:
+        raise ValidationError('Scenario battery rectifier efficiency undefined (with batt_kw > 0)')
 
-    if len(ro.load_8760_kw) != len(ro.year_one_battery_soc_series):
-        raise ValidationError('Scenario load profile does not have the same number of time steps as battery state-of-charge')
+    if ro.year_one_battery_soc_series is not None:
+        if len(ro.load_8760_kw) != len(ro.year_one_battery_soc_series):
+            raise ValidationError('Scenario load profile does not have the same number of time steps as battery state-of-charge')
