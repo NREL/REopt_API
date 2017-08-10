7bca72585412351615ff288d065a4ec46f61da8b
Nick Laws <laws.nick@gmail.com>
2017-08-10 17:37:19 -0600

    new site.financials
    not sure if this is where financials should live (or if all attributes
    were captured). this is one of the first steps of creating
    economics.dat in DatFileManager.

--- reo/src/site.py
+++ reo/src/site.py
@@ -1,8 +1,30 @@
 
 
+class Financials(object):
+
+    def __init__(self,
+                 rate_inflation=0.01,
+                 rate_escalation=0.02,
+                 owner_tax_rate=0.35,
+                 offtaker_tax_rate=0.35,
+                 owner_discount_rate=0.10,
+                 offtaker_discount_rate=0.08,
+                 analysis_period=25,
+                 **kwargs
+                 ):
+        self.rate_inflation = rate_inflation
+        self.rate_escalation = rate_escalation
+        self.owner_tax_rate = owner_tax_rate
+        self.offtaker_tax_rate = offtaker_tax_rate
+        self.owner_discount_rate = owner_discount_rate
+        self.offtaker_discount_rate = offtaker_discount_rate
+        self.analysis_period = analysis_period
+
+
 class Site(object):
 
     def __init__(self, land_area=None, roof_area=None, **kwargs):
 
         self.land_acres = land_area
         self.roof_squarefeet = roof_area
+        self.financials = Financials(**kwargs)
