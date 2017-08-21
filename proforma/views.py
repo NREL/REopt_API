2a9fef531e3a81091738232abff5ccff0b687330
Nick DiOrio <nicholas.diorio@nrel.gov>
2017-08-21 14:27:14 -0600

    Reformat code to comply with PyCharm

--- proforma/views.py
+++ proforma/views.py
@@ -1,4 +1,4 @@
-from tastypie.http import HttpResponse,HttpBadRequest
+from tastypie.http import HttpResponse, HttpBadRequest
 from tastypie.exceptions import ImmediateHttpResponse
 from models import ProForma, RunOutput
 import os
@@ -7,23 +7,23 @@ from wsgiref.util import FileWrapper
 
 
 def proforma(request):
-	uuid = request.GET.get('run_uuid')
+    uuid = request.GET.get('run_uuid')
 
-	try:
-		run_output = RunOutput.objects.get(uuid=uuid)
-        
-		try:
-			pf = ProForma.objects.get(run_output=run_output)
-		except:
-			pf = ProForma.create(run_output=run_output)
+    try:
+        run_output = RunOutput.objects.get(uuid=uuid)
 
-		pf.save()
-		
-		wrapper = FileWrapper(file(pf.output_file))
-		response = HttpResponse(wrapper, content_type='application/force-download')
-		response['Content-Length'] = os.path.getsize(pf.output_file)
-		response['Content-Disposition'] = 'attachment; filename=%s' % (pf.output_file_name)
-		return response
+        try:
+            pf = ProForma.objects.get(run_output=run_output)
+        except:
+            pf = ProForma.create(run_output=run_output)
 
-	except:
-		raise ImmediateHttpResponse(HttpBadRequest("Invalid UUID"))
+        pf.save()
+
+        wrapper = FileWrapper(file(pf.output_file))
+        response = HttpResponse(wrapper, content_type='application/force-download')
+        response['Content-Length'] = os.path.getsize(pf.output_file)
+        response['Content-Disposition'] = 'attachment; filename=%s' % (pf.output_file_name)
+        return response
+
+    except:
+        raise ImmediateHttpResponse(HttpBadRequest("Invalid UUID"))
