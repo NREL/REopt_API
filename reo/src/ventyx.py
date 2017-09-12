1b89f3bfe10fb070254398a0c6254017469fa9a9
Nick Laws <laws.nick@gmail.com>
2017-09-12 17:09:13 -0600

    fix Ventyx wind_prod_factor

--- reo/src/ventyx.py
+++ reo/src/ventyx.py
@@ -4,9 +4,10 @@ import pandas as pd
 
 class Ventyx(object):
 
+    @property
     def wind_prod_factor(self):
 
-        csv_file = os.path.join('..', '..', 'static', 'files', '50009-2011.csv')
+        csv_file = os.path.join('static', 'files', '50009-2011.csv')
         d = pd.DataFrame.from_csv(csv_file)
         wind_speeds = d.loc[:, 'wind speed at 100m (m/s)'].tolist()
         interval = len(wind_speeds)/8760
@@ -14,4 +15,5 @@ class Ventyx(object):
         hourly = []
         for idx in range(0, len(wind_speeds), interval):
             hourly.append(sum(wind_speeds[idx:idx+interval]) / float(interval))
-        print len(hourly)
+
+        return hourly
