9e82493439a82d8e2530bcb9a625d4fa99eed3fc
Nick Laws <laws.nick@gmail.com>
2017-09-15 08:18:28 -0600

    Ventyx returns wind-site prodfactor

--- reo/src/ventyx.py
+++ reo/src/ventyx.py
@@ -1,5 +1,5 @@
 import os
-import pandas as pd
+# import pandas as pd
 
 
 class Ventyx(object):
@@ -7,9 +7,14 @@ class Ventyx(object):
     @property
     def wind_prod_factor(self):
 
-        csv_file = os.path.join('static', 'files', '50009-2011.csv')
-        d = pd.DataFrame.from_csv(csv_file)
-        wind_speeds = d.loc[:, 'wind speed at 100m (m/s)'].tolist()
+        # csv_file = os.path.join('static', 'files', '50009-2011.csv')
+        # d = pd.DataFrame.from_csv(csv_file)
+        # wind_speeds = d.loc[:, 'wind speed at 100m (m/s)'].tolist()
+
+        src_file = os.path.join('static', 'files', 'NWTC_pfs.txt')
+        with open(src_file, 'r') as f:
+            wind_speeds = f.read().split('\r')
+        wind_speeds = [float(s) for s in wind_speeds]
         interval = len(wind_speeds)/8760
 
         hourly = []
