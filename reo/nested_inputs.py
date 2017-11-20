492117c3dd09cfe076765022aaa5614a45823868
TK-23 <tedkwasnik@gmail.com>
2017-11-20 16:02:57 -0700

    data types

--- reo/nested_inputs.py
+++ reo/nested_inputs.py
@@ -27,7 +27,7 @@ macrs_seven_year = [0.1429, 0.2449, 0.1749, 0.1249, 0.0893, 0.0892, 0.0893, 0.04
 
 
 def list_of_float(input):
-    return ["float"(i) for i in input]
+    return [float(i) for i in input]
 
 nested_input_definitions = {
 
