ee71b6afcd0b87b1ff441f0f179a3f6d5b76ecca
Nick Laws <laws.nick@gmail.com>
2017-11-29 17:12:22 -0700

    convert reopt.py to celery tasks

--- reo/src/reopt.py
+++ reo/src/reopt.py
@@ -1,34 +1,47 @@
+from __future__ import absolute_import, unicode_literals
 import os
 import subprocess32 as sp
 from shlex import split
 from reo.log_levels import log
 from reo.results import Results
+from celery import shared_task, chord
 
 
-class Command(object):
+@shared_task
+def parse_run_outputs(year, outputs, outputs_bau, templates, static_outputs):
 
-    def __init__(self, cmd):
-        self.cmd = cmd
-        self.process = None
-        self.error = False
+    output_file = os.path.join(outputs, "REopt_results.json")
 
-    def run(self, timeout):
+    if os.path.exists(output_file):
+        process_results = Results(templates, outputs, outputs_bau,
+                                  static_outputs, year)
+        return process_results.get_output()
 
-        try:
-            status = sp.check_output(split(self.cmd), stderr=sp.STDOUT, timeout=timeout)  # fails if returncode != 0
+    else:
+        msg = "Optimization failed to run. Output file does not exist: " + output_file
+        log("DEBUG", "Current directory: " + os.getcwd())
+        log("WARNING", msg)
+        raise RuntimeError('REopt', msg)
+    
+    
+@shared_task
+def call_xpress(cmd, timeout):
 
-        except sp.CalledProcessError as e:
-            msg = "REopt failed to start. Error code {}.\n{}".format(e.returncode, e.output)
-            log("ERROR", msg)
-            raise RuntimeError('REopt', msg)
+    try:
+        status = sp.check_output(split(cmd), stderr=sp.STDOUT, timeout=timeout)  # fails if returncode != 0
 
-        except sp.TimeoutExpired:
-            raise RuntimeError('REopt', "Optimization exceeded timeout: {} seconds, please email reopt@nrel.gov \
-                                         for support".format(timeout))
-        log("INFO", "REopt run successfully. Status {}".format(status))
+    except sp.CalledProcessError as e:
+        msg = "REopt failed to start. Error code {}.\n{}".format(e.returncode, e.output)
+        log("ERROR", msg)
+        raise RuntimeError('REopt', msg)
 
-        if status.strip() != 'optimal':
-            raise RuntimeError('REopt', "Could not find an optimal solution for these inputs.")
+    except sp.TimeoutExpired:
+        raise RuntimeError('REopt', "Optimization exceeded timeout: {} seconds, please email reopt@nrel.gov \
+                                     for support".format(timeout))
+    log("INFO", "REopt run successfully. Status {}".format(status))
+
+    if status.strip() != 'optimal':
+        raise RuntimeError('REopt', "Could not find an optimal solution for these inputs.")
 
 
 class REopt(object):
@@ -43,30 +56,41 @@ class REopt(object):
         self.dfm = dfm
         file_cmd = os.path.join(paths.inputs, "cmd.log")
         file_cmd_bau = os.path.join(paths.inputs, "cmd_bau.log")
-        self.output_file = os.path.join(paths.outputs, "REopt_results.json")
 
-        run_command = self.create_run_command(paths.outputs, REopt.xpress_model, self.dfm.DAT,
-                                              self.dfm.command_line_args, bau_string='', cmd_file=file_cmd)
+        self.run_command = self.create_run_command(paths.outputs, REopt.xpress_model, self.dfm.DAT,
+                                                   self.dfm.command_line_args, bau_string='', cmd_file=file_cmd)
 
-        run_command_bau = self.create_run_command(paths.outputs_bau, REopt.xpress_model, self.dfm.DAT_bau,
-                                                  self.dfm.command_line_args_bau, bau_string='Base',
-                                                  cmd_file=file_cmd_bau)
+        self.run_command_bau = self.create_run_command(paths.outputs_bau, REopt.xpress_model, self.dfm.DAT_bau,
+                                                       self.dfm.command_line_args_bau, bau_string='Base',
+                                                       cmd_file=file_cmd_bau)
 
-        log("INFO", "Initializing Command")
-        self.command = Command(run_command)
-        log("INFO", "Initializing Command BAU")
-        self.command_bau = Command(run_command_bau)
+        # log("INFO", "Initializing Command")
+        # self.command = Command(run_command)
+        # log("INFO", "Initializing Command BAU")
+        # self.command_bau = Command(run_command_bau)
 
     def run(self, timeout):
+        """
+        Use celery chord to run BAU and with-tech scenarios in parallel,
+        then pass self to parse_run_outputs() as callback function
 
-        log("INFO", "Running Command")
-        self.command.run(timeout)
+        do shared_task's have to be in tasks.py??? no!
+        BUT celery does not work with class methods <fog-horn>
 
-        log("INFO", "Running BAU")
-        self.command_bau.run(timeout)
+        :param timeout:
+        :return:
+        """
+        log("INFO", "Running REopt")
 
-        output_dict = self.parse_run_outputs()
-        return output_dict
+        jobs = (
+            call_xpress.s(self.run_command, timeout),
+            call_xpress.s(self.run_command_bau, timeout)
+        )
+
+        res = chord(jobs, parse_run_outputs.si(self.year, self.paths.outputs, self.paths.outputs_bau,
+                                               self.paths.templates, self.paths.static_outputs))()  # .si for immutable signature, no outputs passed
+        # can't pass objects? self.paths is not serializable
+        return res.get()
 
     def create_run_command(self, path_output, xpress_model, DATs, cmd_line_args, bau_string, cmd_file):
 
@@ -98,16 +122,3 @@ class REopt(object):
             f.write(cmd)
 
         return cmd
-
-    def parse_run_outputs(self):
-
-        if os.path.exists(self.output_file):
-            process_results = Results(self.paths.templates, self.paths.outputs, self.paths.outputs_bau,
-                                      self.paths.static_outputs, self.year)
-            return process_results.get_output()
-
-        else:
-            msg = "Optimization failed to run. Output file does not exist: " + self.output_file
-            log("DEBUG", "Current directory: " + os.getcwd())
-            log("WARNING", msg)
-            raise RuntimeError('REopt', msg)
