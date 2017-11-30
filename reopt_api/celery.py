8137c5ed4f953df23df637369ccfca2767b38895
Nick Laws <laws.nick@gmail.com>
2017-11-29 17:10:17 -0700

    update celery settings
    NOTE: currently hard coded for dev_settings.
    
    Need to make dynamic based on server, perhaps in __init__.py

--- reopt_api/celery.py
+++ reopt_api/celery.py
@@ -3,15 +3,28 @@ import os
 from celery import Celery
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proj.settings')
 
-app = Celery('proj')
+# env = os.environ['APP_ENV']
+# if env == 'development':
+#     raw_env = 'reopt_api.dev_settings'
+# elif env == 'staging':
+#     raw_env = 'reopt_api.staging_settings'
+# elif env == 'production':
+#     raw_env = 'reopt_api.production_settings'
+# else:
+#     raise TypeError('Unknown APP_ENV')
+#
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reopt_api.dev_settings')
+
+app = Celery('reopt_api')
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.conf.broker_url = 'redis://localhost:6379/0'
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
