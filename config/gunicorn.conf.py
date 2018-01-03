25fd16bbfbf2db442a9310745d4b099adc01ee93
Nick Laws <laws.nick@gmail.com>
2018-01-03 11:13:39 -0700

    add test/localhost option to gunicorn.conf.py
    If you wish to test locally, set `TEST="true"` in .env file (contained
    in project root directory).

--- config/gunicorn.conf.py
+++ config/gunicorn.conf.py
@@ -2,7 +2,10 @@ import os
 import multiprocessing
 
 # Bind to unix socket that nginx will proxy to.
-bind = 'unix:' + os.path.join(os.path.abspath(os.path.dirname(os.path.dirname(__file__))), 'tmp/gunicorn.sock')
+if os.environ.get('TEST') is None:
+    bind = 'unix:' + os.path.join(os.path.abspath(os.path.dirname(os.path.dirname(__file__))), 'tmp/gunicorn.sock')
+else:
+    bind = "127.0.0.1:8000"
 
 # Based the number of workers on the number of CPU cores.
 workers = multiprocessing.cpu_count()
