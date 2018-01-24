c420ab6b55ea8d4fb8255db593eb30a0572bdb5a
Nick Laws <laws.nick@gmail.com>
2018-01-24 16:20:23 -0700

    add celery task registration

--- reopt_api/production_settings.py
+++ reopt_api/production_settings.py
@@ -116,8 +116,10 @@ CELERY_RESULT_BACKEND = 'django-db'
 # celery task registration
 CELERY_IMPORTS = (
     'reo.src.reopt',
+    'reo.api',
+    'reo.scenario',
+    'reo.results',
 )
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
