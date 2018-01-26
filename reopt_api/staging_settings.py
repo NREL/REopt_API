8f82b959ef07153645b152b9403e1bda81953d86
Nick Laws <laws.nick@gmail.com>
2018-01-26 07:31:09 -0700

    ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".nrel.gov"] on prod, staging
    temporary solution? might need to change once we go live.

--- reopt_api/staging_settings.py
+++ reopt_api/staging_settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = secret_key_
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = [".reopt-stage-api1.nrel.gov"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".nrel.gov"]
 
 
 # Application definition
