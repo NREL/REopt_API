8ad84316b7abfa5bb682c080aab341d90d572dd3
tkwasnik <tedkwasnik@gmail.com>
2018-02-07 15:09:26 -0700

    added back in help url[C

--- reo/urls.py
+++ reo/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^annual_kwh/', views.annual_kwh, name='annual_kwh'),
     url(r'^invalid_urdb/', views.invalid_urdb, name='invalid_urdb'),
+    url(r'^help/', views.help, name='help'),
     url(r'^results/', views.results, name='results'),
 ]
