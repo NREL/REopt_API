2db0d6ab1d9092d0db1fb1af948a1c933203d881
Nick Laws <laws.nick@gmail.com>
2018-03-19 18:45:09 -0600

    add v1 in front of all URLs
    tried to get the POST endpoint for the reopt api to exist at <host>/v1/
    but the only thing that might have worked (a redirect) does not play
    nice with POST's.  So the app is at v1/reopt/, which we may need to
    redirect at the NGINX level (developer.nrel.gov?)

--- reopt_api/urls.py
+++ reopt_api/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import include, url
-from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from reo.api import RunInputResource
 from tastypie.api import Api
@@ -8,12 +7,10 @@ v1_api = Api(api_name='v1')
 v1_api.register(RunInputResource())
 
 urlpatterns = [
-    url(r'^$', include('reo.urls'), name='reopt'),
-    url(r'^proforma/', include('proforma.urls'), name='proforma'),
-    url(r'^reopt/', include('reo.urls'), name='reopt'),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^api/', include(v1_api.urls)),
-    url(r'^resilience_stats/', include('resilience_stats.urls'), name='resilience_stats'),
+    url(r'^v1/proforma/', include('proforma.urls'), name='proforma'),
+    url(r'^v1/resilience_stats/', include('resilience_stats.urls'), name='resilience_stats'),
+    url(r'^v1/', include('reo.urls'), name='reopt'),
+    url(r'', include(v1_api.urls)),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
