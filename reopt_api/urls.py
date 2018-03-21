6eb4deffb4c1f6dc0f4d8d436207db03b805b07a
Nick Laws <laws.nick@gmail.com>
2018-03-21 10:15:17 -0600

    add custom 404, trailing slash optional

--- reopt_api/urls.py
+++ reopt_api/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.http import Http404
 from reo.api import Job
 from tastypie.api import Api
 from reo import views
@@ -8,14 +9,26 @@ from proforma.views import proforma
 v1_api = Api(api_name='v1')
 v1_api.register(Job())
 
+
+def page_not_found(request, url):
+    """
+    If we could run with DEBUG = False, this custom 404 would not be necessary (see production_settings.py for more).
+    This custom 404 is to avoid showing users too much information (i.e. traceback information) when they mistype a url.
+    :param request:
+    :param url:
+    :return:
+    """
+    raise Http404("Invalid URL: {}".format(url))
+
 urlpatterns = [
-    url(r'^v1/job/(?P<run_uuid>[0-9a-f-]+)/proforma/$', proforma, name='proforma'),
-    url(r'^v1/job/(?P<run_uuid>[0-9a-f-]+)/resilience_stats/$', include('resilience_stats.urls'), name='resilience_stats'),
-    url(r'^v1/job/(?P<run_uuid>[0-9a-f-]+)/results/$', views.results, name='results'),
-    url(r'^v1/help/$', views.help, name='help'),
-    url(r'^v1/invalid_urdb/$', views.invalid_urdb, name='invalid_urdb'),
-    url(r'^v1/annual_kwh/$', views.annual_kwh, name='annual_kwh'),
+    url(r'^v1/job/(?P<run_uuid>[0-9a-f-]+)/proforma/?$', proforma, name='proforma'),
+    url(r'^v1/job/(?P<run_uuid>[0-9a-f-]+)/resilience_stats/?$', include('resilience_stats.urls'), name='resilience_stats'),
+    url(r'^v1/job/(?P<run_uuid>[0-9a-f-]+)/results/?$', views.results, name='results'),
+    url(r'^v1/help/?$', views.help, name='help'),
+    url(r'^v1/invalid_urdb/?$', views.invalid_urdb, name='invalid_urdb'),
+    url(r'^v1/annual_kwh/?$', views.annual_kwh, name='annual_kwh'),
     url(r'', include(v1_api.urls), name='job'),
+    url(r'(.*)', page_not_found, name='404'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
