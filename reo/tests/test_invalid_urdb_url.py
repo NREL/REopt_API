fe41af1440e5351ae5999fd1594d1fe3838e1866
Nick Laws <laws.nick@gmail.com>
2018-03-20 18:30:52 -0600

    fix url and make test run
    due to invalid indenting, etc. this test was not even running until now

--- reo/tests/test_invalid_urdb_url.py
+++ reo/tests/test_invalid_urdb_url.py
@@ -1,14 +1,17 @@
+import json
+import csv
 from django.test import TestCase
 from tastypie.test import ResourceTestCaseMixin
 
+
 class EntryResourceTest(ResourceTestCaseMixin, TestCase):
 
-    def setUp(self):
-    	super(EntryResourceTest, self).setUp()
-    	self.invalid_urdb_url = '/reopt/invalid_urdb/'
+	def setUp(self):
+		super(EntryResourceTest, self).setUp()
+		self.invalid_urdb_url = '/v1/invalid_urdb/'
 
 	def test_problems(self):
-	        invalid_list = json.loads(self.api_client.get(self.invalid_urdb_url,format='json').content)['Invalid IDs']
-	        hard_problems = [i[0] for i in csv.reader(open('reo/hard_problems.csv','rb'))]
-	        for hp in hard_problems:
-	            self.assertTrue(hp in invalid_list)
+		invalid_list = json.loads(self.api_client.get(self.invalid_urdb_url,format='json').content)['Invalid IDs']
+		hard_problems = [i[0] for i in csv.reader(open('reo/hard_problems.csv','rb'))]
+		for hp in hard_problems:
+			self.assertTrue(hp in invalid_list)
