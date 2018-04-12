ddd769615b5824a5f73d8656aea4efbfe447344d
Nick Laws <laws.nick@gmail.com>
2018-04-12 16:54:28 -0600

    add generator fuel_used_gal output

--- reo/results.py
+++ reo/results.py
@@ -196,6 +196,8 @@ def parse_run_outputs(self, dfm_list, data, meta, saveToDB=True):
                     self.nested_outputs["Scenario"]["Site"][name]["year_one_to_load_series_kw"] = self.po.get_grid_to_load()
                     self.nested_outputs["Scenario"]["Site"][name]["year_one_to_battery_series_kw"] = self.po.get_grid_to_batt()
                     self.nested_outputs["Scenario"]["Site"][name]["year_one_energy_supplied_kwh"] = self.results_dict.get("year_one_utility_kwh")
+                elif name == "Generator":
+                    self.nested_outputs["Scenario"]["Site"][name]["fuel_used_gal"] = self.results_dict.get("fuel_used_gal")
 
         def compute_total_power(self, tech):
             power_lists = list()
