67e75ae8c4754b535274d34a63d3aee4671ac0de
Nick Laws <laws.nick@gmail.com>
2018-05-25 14:59:39 -0600

    bug fix: outage_simulator pv_kw_ac_hourly used to be for 1kW system
    Not sure when this happened, but it used to be the case that
    `pv_kw_ac_hourly` was the normalize PV production from PVWatts, such
    that the `outage_simulator` also needed `pv_kw` as an input to
    determine the actual PV production profile. Somewhere along the way, we
    started using the actual PV production (not normalized), but continued
    to scale the hourly profile with `pv_kw`.
    
    These changes also use the new API output
    `load_profile.critical_load_series_kw` for the `outage_simulator` (and
    `outage_costs`).

--- reo/src/outage_costs.py
+++ reo/src/outage_costs.py
@@ -14,24 +14,19 @@ def calc_avoided_outage_costs(data, present_worth_factor):
     """
     site_inputs = data['inputs']['Scenario']['Site']
     site_outputs = data['outputs']['Scenario']['Site']
-    load = site_outputs['LoadProfile'].get('year_one_electric_load_series_kw', 8760*[0])
+    pv = site_outputs['PV']
+    load_profile = site_inputs['LoadProfile']
     batt_roundtrip_efficiency = site_inputs['Storage']['internal_efficiency_pct'] \
                                 * site_inputs['Storage']['inverter_efficiency_pct'] \
                                 * site_inputs['Storage']['rectifier_efficiency_pct']
-    critical_loads_kw=site_inputs['LoadProfile'].get("critical_loads_kw")
-    critical_loads_kw_is_net=site_inputs['LoadProfile'].get("critical_loads_kw_is_net")
-    crit_load_factor = site_inputs['LoadProfile'].get("critical_load_pct")
+    critical_load = site_outputs['LoadProfile']['critical_load_series_kw']
 
     results = simulate_outage(
-        pv_kw=site_inputs['PV'].get('existing_kw', 0) + site_outputs['PV'].get('size_kw', 0),
         batt_kwh=site_outputs['Storage'].get('size_kwh', 0),
         batt_kw=site_outputs['Storage'].get('size_kw', 0),
-        load=load,
-        pv_kw_ac_hourly=site_outputs['PV'].get('year_one_power_production_series_kw'),
+        pv_kw_ac_hourly=pv['year_one_power_production_series_kw'],
         init_soc=site_outputs['Storage'].get('year_one_soc_series_pct'),
-        critical_loads_kw=critical_loads_kw,
-        critical_loads_kw_is_net=critical_loads_kw_is_net,
-        crit_load_factor=crit_load_factor,
+        critical_loads_kw=critical_load,
         batt_roundtrip_efficiency=batt_roundtrip_efficiency,
         diesel_kw=site_inputs['Generator']['size_kw'],
         fuel_available=site_inputs['Generator']['fuel_avail_gal'],
@@ -40,9 +35,9 @@ def calc_avoided_outage_costs(data, present_worth_factor):
         diesel_min_turndown=site_inputs['Generator']['min_turn_down_pct']
     )
 
-    avg_crit_ld = sum(load) * crit_load_factor / len(load)
+    avg_crit_ld = sum(critical_load) / len(critical_load)
 
-    if site_inputs['LoadProfile'].get('outage_is_major_event') is True:
+    if load_profile['outage_is_major_event']:
         # assume that outage occurs only once in analysis period
         present_worth_factor = 1
 
