cc7b69039b09e1b687e9d07b778c2cb14f792387
Nick Laws <laws.nick@gmail.com>
2018-05-29 21:38:01 -0600

    migrations merge

---
+++ reo/migrations/0017_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('reo', '0016_pvmodel_existing_pv_om_cost_us_dollars'),
+        ('reo', '0016_generatormodel_year_one_to_load_series_kw'),
+    ]
+
+    operations = [
+    ]
