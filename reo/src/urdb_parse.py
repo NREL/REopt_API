NREL/REopt_API

commit 131bffb17d50940e0412eb553b6cf2108c368601

generator added to urdb_parse
where mosel parameters for generator defined because need to know
number of energy tiers in rate.

reo/src/urdb_parse.py
@@ -31,6 +31,8 @@ class REoptArgs:
         self.energy_avail_bau = []
         self.energy_burn_rate = []
         self.energy_burn_rate_bau = []
+        self.energy_burn_intercept = []
+        self.energy_burn_intercept_bau = []
 
         self.export_rates = []
         self.export_rates_bau = []
@@ -98,12 +100,15 @@ class UrdbParse:
     """
     Sub-function of DatFileManager.
     Makes all REopt args for dat files in Inputs/Utility directory
+
+    Note: (diesel) generator parameters for mosel are defined here because they depend on number of energy tiers in
+    utility rate.
     """
 
     days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     cum_days_in_yr = numpy.cumsum(calendar.mdays)
 
-    def __init__(self, paths, big_number, elec_tariff, techs, bau_techs, loads, excess_rate=0.0):
+    def __init__(self, paths, big_number, elec_tariff, techs, bau_techs, loads, excess_rate=0.0, gen=None):
 
         self.urdb_rate = elec_tariff.urdb_response
         self.year = elec_tariff.load_year
@@ -117,6 +122,14 @@ class UrdbParse:
         self.techs = techs
         self.bau_techs = bau_techs
         self.loads = loads
+        if gen is not None:
+            self.generator_fuel_slope = gen.fuel_slope
+            self.generator_fuel_intercept = gen.fuel_intercept
+            self.generator_fuel_avail = gen.fuel_avail
+        else:
+            self.generator_fuel_slope = 0
+            self.generator_fuel_intercept = 0
+            self.generator_fuel_avail = 0
 
         log("INFO", "URDB parse with year: " + str(self.year) + " net_metering: " + str(self.net_metering))
 
@@ -326,12 +339,18 @@ class UrdbParse:
         # FuelRate=array( Tech,FuelBin,TimeStep) is the cost of electricity from each Tech, so 0's for PV, PVNM
         self.reopt_args.energy_rates = []
 
-        for i in range(len(self.techs) - 1):
+        for tech in self.techs:
             for _ in range(self.reopt_args.energy_tiers_num):
-                self.reopt_args.energy_rates = operator.add(self.reopt_args.energy_rates, zero_array)
-                self.reopt_args.energy_avail.append(0)
-        self.reopt_args.energy_rates += energy_costs
-        self.reopt_args.energy_avail.append(self.big_number)
+                if tech.lower() == 'generator':
+                    # generator fuel is free for now since we are only modeling existing generators
+                    self.reopt_args.energy_rates = operator.add(self.reopt_args.energy_rates, zero_array)
+                    self.reopt_args.energy_avail.append(self.generator_fuel_avail)
+                elif tech.lower() == 'util':
+                    self.reopt_args.energy_rates += energy_costs
+                    self.reopt_args.energy_avail.append(self.big_number)
+                else:
+                    self.reopt_args.energy_rates = operator.add(self.reopt_args.energy_rates, zero_array)
+                    self.reopt_args.energy_avail.append(0)
 
         # ExportRate is the value of exporting a Tech to the grid under a certain Load bin
         # If there is net metering and no wholesale rate, appears to be zeros for all but 'PV' at '1W'
@@ -354,27 +373,47 @@ class UrdbParse:
 
         self.reopt_args.export_rates = tmp_list
 
-        #FuelBurnRateM = array(Tech,Load,FuelBin)
+        """
+        FuelBurnRateM = array(Tech,Load,FuelBin)
+        FuelBurnRateB = array(Tech,Load,FuelBin)
+        Either 1 or 0, except when modeling generator.
+        """
         FuelBurnRateM = []
+        FuelBurnRateB = []
+
         for tech in self.techs:
             for load in self.loads:
                 for _ in range(self.reopt_args.energy_tiers_num):
                     if tech.lower() == 'util':
                         FuelBurnRateM.append(1)
-                    else:
+                        FuelBurnRateB.append(0)
+                    elif tech.lower() == 'generator':
+                        FuelBurnRateM.append(self.generator_fuel_slope)
+                        FuelBurnRateB.append(self.generator_fuel_intercept)
+                    else:  # PV, Wind, and Storage do not use fuel
                         FuelBurnRateM.append(0)
+                        FuelBurnRateB.append(0)
 
         FuelBurnRateMBase = []
+        FuelBurnRateBBase = []
+
         for tech in self.bau_techs:
             for load in self.loads:
                 for _ in range(self.reopt_args.energy_tiers_num):
                     if tech.lower() == 'util':
                         FuelBurnRateMBase.append(1)
-                    else:
+                        FuelBurnRateBBase.append(0)
+                    elif tech.lower() == 'generator':
+                        FuelBurnRateM.append(self.generator_fuel_slope)
+                        FuelBurnRateBBase.append(self.generator_fuel_intercept)
+                    else:  # PV, Wind, and Storage do not use fuel
                         FuelBurnRateMBase.append(0)
+                        FuelBurnRateBBase.append(0)
                         
         self.reopt_args.energy_burn_rate = FuelBurnRateM
         self.reopt_args.energy_burn_rate_bau = FuelBurnRateMBase
+        self.reopt_args.energy_burn_intercept = FuelBurnRateB
+        self.reopt_args.energy_burn_intercept_bau = FuelBurnRateBBase
 
     def prepare_demand_periods(self, current_rate):
 

commit e49abf62702b811fb0311a1ea809346b15c769ec

bug fix: UTIL FuelCosts were being duplicated with energy tiers > 1
mistake introduced in commit 131bffb17d50940e0412eb553b6cf2108c368601
"generator added to urdb_parse".

The energy_costs array contains utility energy charges for each time
step and each energy tier. For example, with hourly time steps and 2
energy tiers, `len(energy_costs) = 17,520`.  The prior setup for
`energy_rates`, which ultimately becomes the `FuelRate` array in mosel,
assumed that `energy_costs` was an 8760 in all cases.

reo/src/urdb_parse.py
@@ -236,7 +236,7 @@ class UrdbParse:
         for energy_rate in current_rate.energyratestructure:
             energy_tiers.append(len(energy_rate))
 
-        period_with_max_tiers=energy_tiers.index(max(energy_tiers))
+        period_with_max_tiers = energy_tiers.index(max(energy_tiers))
         energy_tier_set = set(energy_tiers)
 
         if len(energy_tier_set) > 1:
@@ -337,17 +337,20 @@ class UrdbParse:
         energy_avail = []
 
         for tech in techs:
-            for _ in range(self.reopt_args.energy_tiers_num):
-                if tech.lower() == 'generator':
-                    # generator fuel is free for now since we are only modeling existing generators
-                    energy_rates = operator.add(energy_rates, zero_array)
-                    energy_avail.append(self.generator_fuel_avail)
-                elif tech.lower() == 'util':
-                    energy_rates += energy_costs
-                    energy_avail.append(self.big_number)
-                else:
-                    energy_rates = operator.add(energy_rates, zero_array)
-                    energy_avail.append(0)
+            if tech.lower() == 'util':
+                energy_rates += energy_costs
+                energy_avail += [self.big_number] * self.reopt_args.energy_tiers_num
+            else:
+                # have to rubber stamp other tech values for each energy tier so that array is filled appropriately
+                for _ in range(self.reopt_args.energy_tiers_num):
+                    if tech.lower() == 'generator':
+                        # generator fuel is free for now since we are only modeling existing generators
+                        energy_rates = operator.add(energy_rates, zero_array)
+                        energy_avail.append(self.generator_fuel_avail)
+                    else:
+                        # all other techs (PV, PVNM) have zero fuel and zero fuel cost
+                        energy_rates = operator.add(energy_rates, zero_array)
+                        energy_avail.append(0)
 
         # ExportRate is the value of exporting a Tech to the grid under a certain Load bin
         # If there is net metering and no wholesale rate, appears to be zeros for all but 'PV' at '1W'