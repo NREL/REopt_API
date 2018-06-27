27b33c64dc769e0b0f2f99be4953816a312cf2ff
Rob Spencer <robert.spencer@nrel.gov>
2018-06-27 16:54:32 -0600

    added other data models to summary app

--- summary/views.py
+++ summary/views.py
@@ -1,45 +1,53 @@
 import sys
 from django.http import JsonResponse
 from django.shortcuts import render
-from reo.models import ScenarioModel, PVModel
+from reo.models import ScenarioModel, PVModel, StorageModel, FinancialModel
 from reo.exceptions import UnexpectedError
 from django.forms.models import model_to_dict
 
 def summary(request, user_id):
-    """
-    Retrieve a summary of scenarios for given user_id
-    :param request:
-    :param user_id:
-    :return: [{
-    		  "user_id",
-    		  "run_uuid",
-              "name",
-              "description",
-              "created",
-              "status",
-              "Key Output"
-             }]
-    """
-    try:
-	    scenarios = ScenarioModel.objects.filter(user_id=user_id).order_by('-created')
-	    json = {"user_id": user_id, "scenarios": []}
-	    for scenario in scenarios:
-	    	results = {}
-	    	pv = PVModel.objects.filter(run_uuid=scenario.run_uuid).first()
+	"""
+	Retrieve a summary of scenarios for given user_id
+	:param request:
+	:param user_id:
+	:return: [{
+			  "user_id",
+			  "run_uuid",
+			  "name",
+			  "description",
+			  "created",
+			  "status",
+			  "Key Output"
+			 }]
+	"""
+	try:
+		scenarios = ScenarioModel.objects.filter(user_id=user_id).order_by('-created')
+		json = {"user_id": user_id, "scenarios": []}
+		for scenario in scenarios:
+			results = {}
 
-	    	results['run_uuid'] = str(scenario.run_uuid)
-	    	results['created'] = scenario.created
-	    	results['status'] = scenario.status
-	    	if pv.max_kw > 0:
-	    		results['pv_max_kw'] = pv.max_kw
-	    	else:
-	    		results['pv_max_kw'] = 'not evaluated'
+			batt = StorageModel.objects.filter(run_uuid=scenario.run_uuid).first()
+			pv = PVModel.objects.filter(run_uuid=scenario.run_uuid).first()
+			financial = FinancialModel.objects.filter(run_uuid=scenario.run_uuid).first()
 
-	    	json['scenarios'].append(results)
-	    response = JsonResponse(json)
-	    return response
+			results['run_uuid'] = str(scenario.run_uuid)
+			results['created'] = scenario.created
+			results['status'] = scenario.status
+			results['npv_us_dollars'] = financial.npv_us_dollars
+			if pv.max_kw > 0:
+				results['pv_kw'] = pv.size_kw
+			else:
+				results['pv_kw'] = 'not evaluated'
+			if batt.max_kw > 0:
+				results['batt_kw'] = batt.size_kw
+			else:
+				results['batt_kw'] = 'not evaluated'
 
-    except Exception as e:
-        return JsonResponse({"Error": e}, status=500)
+			json['scenarios'].append(results)
+		response = JsonResponse(json)
+		return response
+
+	except Exception as e:
+		return JsonResponse({"Error": e}, status=500)
 
 
