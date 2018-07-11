c3a9f5f43f3ef95b70c8475065fab3fabbdca69a
Rob Spencer <robert.spencer@nrel.gov>
2018-07-11 10:29:57 -0600

    migration merge

---
+++ reo/migrations/0020_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('reo', '0019_merge'),
+        ('reo', '0018_auto_20180705_2158'),
+    ]
+
+    operations = [
+    ]
