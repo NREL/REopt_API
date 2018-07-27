e1cba6158fd671aabe54f29d541a85fc712ae476
Nick Laws <laws.nick@gmail.com>
2018-07-27 14:41:04 -0600

    split ALTER statements for double type to real into separate migrations
    attempt at speeding up the database migrations for production servers

---
+++ reo/migrations/0023_alter_table_reo_loadprofile_type_real.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('reo', '0017_merge'),
+    ]
+
+    operations = [
+
+        migrations.RunSQL("ALTER TABLE reo_loadprofilemodel ALTER COLUMN monthly_totals_kwh TYPE real[], ALTER COLUMN loads_kw TYPE real[], ALTER COLUMN year_one_electric_load_series_kw TYPE real[], ALTER COLUMN critical_loads_kw TYPE real[], ALTER COLUMN critical_load_series_kw TYPE real[];"),
+
+    ]
