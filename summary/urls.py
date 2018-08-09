8d096f082899786e1eb735b19f079997cca4f553
Rob Spencer <robert.spencer@nrel.gov>
2018-08-09 14:42:24 -0600

    added endpoint and functionality for updating the user_id associated with a run_uuid

--- summary/urls.py
+++ summary/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.summary, name='summary'),
+    url(r'^(?P<user_id>[0-9a-z-]+)/summary/?$', views.summary, name='summary'),
+    url(r'rename/?$', views.update_user_id, name='update_user_id'),
 ]
