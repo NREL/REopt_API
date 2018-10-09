710ebc783440a2aceb55e947a7c7e0200b8f0018
Sakshi <sakshi.mishra@nrel.gov>
2018-10-09 14:16:26 -0600

    change in size_class description

--- reo/nested_inputs.py
+++ reo/nested_inputs.py
@@ -335,8 +335,8 @@ nested_input_definitions = {
           "type": "float",
           "min": 0,
           "max": 1e5,
-          "default": 3000,  # if the default value of 3 goes in techs.py, there is a logic to assign actual defaul cost based on 'size_class'
-          "description": "Total upfront installed costs in US dollars/kW. Determined by size_class. For the 'large' (>1MW) size_class the cost is $1,874/kW. For the 'medium' size_class the cost is $4,111/kW and for the 'commercial' size_class the cost is $4,989/kW."
+          "default": 3000,  # if the default value of 3000 goes in techs.py, there is a logic to assign actual defaul cost based on 'size_class'
+          "description": "Total upfront installed costs in US dollars/kW. Determined by size_class. For the 'large' (>2MW) size_class the cost is $1,874/kW. For the 'medium commercial' size_class the cost is $4,111/kW. For the 'small commercial' size_class the cost is $4,989/kW and for the 'residential' size_class the cost is $10,792/kW "
         },
         "om_cost_us_dollars_per_kw": {
           "type": "float",
