35105b22c641e87cc1f15e222f121f0a4f9e2f0b
Kwasnik <tkwasnik@loaner-30342s.nrel.gov>
2019-02-21 16:30:37 -0700

    update data pull

--- scripts/pull_data.py
+++ scripts/pull_data.py
@@ -2,13 +2,32 @@ import os
 import numpy as np
 import pandas as pd
 import multiprocessing as mp
-from reo.models import ErrorModel, BadPost, MessageModel
+from reo.models import ErrorModel, BadPost, MessageModel, ElectricTariffModel,ScenarioModel
 import datetime
 import pytz
 import copy
 
 recent_interval_days = 7
 
+def harmonize_traceback_lookups(urdb_results,error_results):
+	common_lookup = copy.deepcopy(urdb_results['traceback_lookup'])
+	
+	for tb_id, tb in copy.deepcopy(error_results['traceback_lookup'].items()):
+		
+		del error_results['traceback'][tb_id]
+		
+		if tb in common_lookup.values():
+			new_id = [k for k,v in common_lookup.items() if v==tb][0]
+		else:
+			new_id = max(common_lookup.keys()) + 1
+			common_lookup[new_id] = tb
+		
+		error_results['traceback'][new_id] = tb
+	
+	del urdb_results['traceback_lookup'] 
+	del error_results['traceback_lookup']
+	
+	return urdb_results,error_results,common_lookup
 
 def process_bad_post_set(bp_set):
 	result = {}
@@ -81,60 +100,180 @@ def process_error_set(e_set):
 def combine_error_summaries(error_summaries):
 
 	result = {'traceback_lookup':{}}
-	for es in error_summaries:
+	tb_recode = {}
+	for i, es in enumerate(error_summaries):
 		if es is not None:
-			tb_recode = {}
+			tb_recode[i] = {}
 			for tb_id, tb in es['traceback_lookup'].items():
 				if tb in result['traceback_lookup'].values():
-					tb_recode[tb_id] = [k for k,v in result['traceback_lookup'].items() if v==tb][0]
+					tb_recode[i][tb_id] = [k for k,v in result['traceback_lookup'].items() if v==tb][0]
 				else:
-					new_tb_id = len(result['traceback_lookup']) 
-					result['traceback_lookup'][new_tb_id] = tb
-					tb_recode[tb_id] = new_tb_id
-			
-	for es in error_summaries:		
-		if es is not None:
-			for k in es.keys():
-				if k != 'traceback_lookup':
-					if k not in result.keys():
-						result[k] = copy.deepcopy(es[k])
+					if tb_id not in result['traceback_lookup'].keys():
+						new_tb_id = tb_id
+						
 					else:
-						for v in es[k].keys():
-							if k == 'traceback':
-								v = tb_recode[v]
-							
-							if v not in result[k].keys():
-								result[k][v] = copy.deepcopy(es[k][v])
+						if len(result['traceback_lookup'].keys()) ==0:
+							new_tb_id =0
+						else:
+							new_tb_id = max(result['traceback_lookup'].keys()) +1
+						
+						
+					tb_recode[i][tb_id] = new_tb_id
+					result['traceback_lookup'][new_tb_id] = tb
+			del es['traceback_lookup']
+				
+	try:
+		for i,es in enumerate(error_summaries):		
+			if es is not None:
+				for error_type in es.keys():
+					if error_type != 'traceback_lookup':
+						if error_type not in result.keys():
+							result[error_type] = copy.deepcopy(es[error_type])
+						else:
+							for error_type_id in es[error_type].keys():
+								if error_type == 'traceback':
+									value =  copy.deepcopy(es[error_type][error_type_id])
+									new_error_type_id = tb_recode[i][error_type_id]
+									del  es[error_type][error_type_id]
+									error_type_id = new_error_type_id
+									es[error_type][error_type_id] = value
+								
+								if error_type_id not in result[error_type].keys():
+									result[error_type][error_type_id] = copy.deepcopy(es[error_type][error_type_id])
+								else:
+									for attr_name, attr in es[error_type][error_type_id].items():
+										if attr_name.startswith('count'):
+											if attr_name in result[error_type][error_type_id].keys():
+												result[error_type][error_type_id][attr_name] += attr
+											else:
+												result[error_type][error_type_id][attr_name] = attr
+										elif attr_name == 'most_recent':
+											if result[error_type][error_type_id][attr_name] < attr:
+												result[error_type][error_type_id][attr_name] = attr
+										else:	
+											result[error_type][error_type_id][attr_name]+=attr
+	except Exception as e:
+		from IPython import embed
+		embed()
+	return result
+
+
+def process_urdb_set(urdb_set):
+	if len(urdb_set) == 0:
+		return None
+	
+	result = {"count_none":0,"count":0,'count_errors':0}
+	for u in urdb_set:
+		result['count'] += 1
+		label = u[0] or u[1]['label'] or None
+		if label == None:
+			result['count_none'] += 1
+		else:
+			if label in result.keys():
+				result[label].append(str(u[2]))
+			else:
+				result[label] = [str(u[2])]
+
+
+	for k,v in result.items():
+		if not k.startswith('count'):
+			e_set = [i for i in ErrorModel.objects.filter(run_uuid__in=v)]
+			result[k] = process_error_set(e_set) or {}
+			result[k]['count_total']=len(v)
+			result[k]['count_errors']= len(e_set)
+			result['count_errors']+=len(e_set)
+	return result
+
+def combine_urdb_summaries(urdb_rates):
+	result = {'traceback_lookup':{}}
+	tb_recode = {}
+	for u in urdb_rates:
+		if u is not None:
+			for rate, rate_stats in u.items():
+				if not rate.startswith('count'):
+					if 'traceback_lookup' in rate_stats.keys():
+						tb_recode[rate] = {}
+						for tb_id, tb in rate_stats['traceback_lookup'].items():
+							if tb in result['traceback_lookup'].values():
+								tb_recode[rate][tb_id] = [k for k,v in result['traceback_lookup'].items() if v==tb][0]
 							else:
-								for attr_name, attr in es[k][v].items():
-									if attr_name.startswith('count'):
-										if v in result[k].keys():
-											result[k][v][attr_name] += attr
-										else:
-											result[k][v][attr_name] = attr
-									elif attr_name == 'most_recent':
-										if result[k][v][attr_name] < attr:
-											result[k][v][attr_name] = attr
-									else:	
-										result[k][v][attr_name]+=attr
+								new_tb_id = len(result['traceback_lookup']) 
+								result['traceback_lookup'][new_tb_id] = tb
+								tb_recode[rate][tb_id] = new_tb_id
+						del rate_stats['traceback_lookup']
+	for u in urdb_rates:		
+		if u is not None:
+			for rate, rate_stats in u.items():
+				if not rate.startswith('count'):
+					if 'traceback' in rate_stats.keys():
+						new_traceback_data = {}
+						for tb_id,tb_info in rate_stats['traceback'].items():
+							new_traceback_data[tb_recode[rate][tb_id]] = tb_info
+						rate_stats['traceback'] = new_traceback_data
+						del new_traceback_data
+
+					if rate not in result.keys():
+						result[rate] = copy.deepcopy(u[rate])
+					else:
+						for k,v in rate_stats.items():
+							if type(v)==int:
+								if k in result[rate].keys():
+									 result[rate][k] += v
+								else:
+									result[rate][k] = v
+							else:
+								if k not in result[rate].keys():
+									result[rate][k] = copy.deepcopy(v)
+								else:
+									for sub_group, sub_group_data in v.items():
+										for attr_name, attr in sub_group_data.items():
+											if attr_name.startswith('count'):
+												if attr_name in result[rate][k][sub_group].keys():
+													result[rate][k][sub_group][attr_name] += attr
+												else:
+													result[rate][k][sub_group][attr_name] = attr
+											elif attr_name == 'most_recent':
+												if result[rate][k][sub_group][attr_name] < attr:
+													result[rate][k][sub_group][attr_name] = attr
+											else:
+												result[rate][k][sub_group][attr_name]+=attr
+				else:
+
+					if rate in result.keys():
+						result[rate] += rate_stats
+					else:
+						result[rate] = rate_stats
 	return result
 
 def run(*args):
-	CORES = 1
+	CORES = 6
 	
 	if 'pull_data' not in globals().values():
 		p = mp.Pool(processes=CORES)	
-		
 
+		print ScenarioModel.objects.all().count()
 		
+		urdb_sets = np.array_split(np.array(ElectricTariffModel.objects.values_list("urdb_label","urdb_response","run_uuid").all()),CORES)
+		urdb_results = combine_urdb_summaries(p.map(process_urdb_set, urdb_sets))
+
 		error_sets = np.array_split(np.array(ErrorModel.objects.all()),CORES)
 		error_results = combine_error_summaries(p.map(process_error_set, error_sets))
 
-		badpost_sets = np.array_split(np.array(BadPost.objects.all()),CORES)
-		bad_post_results = combine_bad_post_summaries(p.map(process_bad_post_set, badpost_sets))
-		
+		urdb_results,error_results, common_lookup = harmonize_traceback_lookups(urdb_results,error_results)
+
+		with open('urdb_results.txt','w+') as output_file:
+			output_file.write(str(urdb_results))
+
 		with open('error_results.txt','w+') as output_file:
 			output_file.write(str(error_results))
 
+		with open('common_lookup.txt','w+') as output_file:
+			output_file.write(str(common_lookup))
+
+		badpost_sets = np.array_split(np.array(BadPost.objects.all()),CORES)
+		bad_post_results = combine_bad_post_summaries(p.map(process_bad_post_set, badpost_sets))
+		bad_post_results['all_posts'] = ScenarioModel.objects.all().count()
 		with open('bad_post_results.txt','w+') as output_file:
 			output_file.write(str(bad_post_results))
+
+
