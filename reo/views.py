c776341d8291a1dbff9277aabd1e15008f2f9fe9
Kwasnik <tkwasnik@loaner-30342s.nrel.gov>
2019-03-05 16:22:58 -0700

    remove print statement

--- reo/views.py
+++ reo/views.py
@@ -24,8 +24,6 @@ hard_problem_labels = [i[0] for i in csv.reader(open(hard_problems_csv, 'rb'))]
 from keys import admin_error_page_password
 
 def errors(request, page_uuid):
-    print admin_error_page_password
-    print page_uuid
     if admin_error_page_password == page_uuid:
         template= loader.get_template("errors.html")
         return HttpResponse(template.render())
