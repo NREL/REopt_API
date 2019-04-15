d729e7774a28b7493c503eaa2e521c31f4a31e50
Kwasnik <tkwasnik@tkwasnik-28886s.nrel.gov>
2019-04-15 12:51:18 -0600

    updated errors

--- reo/static/reo/js/data/summary_info.js
+++ reo/static/reo/js/data/summary_info.js
@@ -1,1 +1,1 @@
-var summary_info = {"count_all_posts": 1238150, "last_updated": 1554239302, "count_bad_posts": 742}
+var summary_info = {"count_all_posts": 1268461, "last_updated": 1555353856, "count_bad_posts": 765}
