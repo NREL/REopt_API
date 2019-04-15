1a7bb0caab99af01d6de66a10a722dc17b142487
Kwasnik <tkwasnik@tkwasnik-28886s.nrel.gov>
2019-04-15 13:01:05 -0600

    updated errors

--- reo/static/reo/js/data/summary_info.js
+++ reo/static/reo/js/data/summary_info.js
@@ -1,2 +1,1 @@
-
 var summary_info = {"count_all_posts": 1268461, "last_updated": 1555353856, "count_bad_posts": 765}
--- reopt_api/dev_settings.py
+++ reopt_api/dev_settings.py
@@ -153,7 +153,7 @@ CELERY_TASK_EAGER_PROPAGATES_EXCEPTIONS = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-if os.environ.get('BUILD_TYPE') == 'jenkins':
+if not os.environ.get('BUILD_TYPE') == 'jenkins':
     STATIC_URL = '/static/'
 else:
     STATIC_URL = '/'
