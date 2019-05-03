5b8749d667af313c424e6e42ef785ee223419637
Sakshi <sakshi.mishra@nrel.gov>
2019-05-03 11:28:54 -0600

    reshaping and giving custom indexing to all the arrays

--- reo_jump/reopt.jl
+++ reo_jump/reopt.jl
@@ -11,40 +11,104 @@ include("utils.jl")
 include("reo_structs.jl")
 
 init_data = importData("./wtch_data.json")
-init_data["TechClass"] = Symbol.(init_data["TechClass"])
-init_data["Tech"] = Symbol.(init_data["Tech"])
-init_data["load"] = Symbol.(init_data["load"])
-init_data["NMILRegime"] = Symbol.(init_data["NMILRegime"])
+TechClassNames = Symbol.(init_data["TechClass"])
+TechNames = Symbol.(init_data["Tech"])
+LoadNames = Symbol.(init_data["load"])
+NMILRegimeNames = Symbol.(init_data["NMILRegime"])
 
 
-TimeStepCount = init_data["TimeStepCount"]
-TechCount = length(init_data["Tech"])
-LoadCount = length(init_data["load"])
+BattLevelCount = 1 #resolve this
+CapCostSegCount = init_data["CapCostSegCount"]
+DemandBinCount = init_data["DemandBinCount"]
+DemandMonthsBinCount = init_data["DemandMonthsBinCount"]
 FuelBinCount = init_data["FuelBinCount"]
+LoadCount = length(LoadNames)
+NumRatchets = init_data["NumRatchets"]
+TechCount = length(TechNames)
+TimeStepCount = init_data["TimeStepCount"]
+TimeStepScaling = init_data["TimeStepScaling"]
 
-# reshaping all the matrices
-# Juila being column-first language reshapes the 1-D into matrices
-# in transposed way, thus rearranging the dimensions along with reshaping
+
+Seg = collect(1:CapCostSegCount)
+Points = collect(0:CapCostSegCount)
+Month = collect(1:12)
+Ratchets = collect(1:NumRatchets)
+TimeSteps = collect(1:TimeStepCount)
+FuelBin = collect(1:FuelBinCount)
+DemandBin =  collect(1:DemandBinCount)
+DemandMonthsBin = collect(1:DemandMonthsBinCount)
+BattLevel = collect(1:BattLevelCount)
+
+
+
+# reshaping parameters (vectors) into multi-D arrays
+# Juila being column-first language reshapes the 1-D vector into multi-D
+# arrays in a transposed way, thus rearranging the dimensions as well
 ProdFactor = NamedArray(permutedims(reshape(init_data["ProdFactor"],
-                            (timestepcount, loadcount, techcount)), [3,2,1]),
-                            (init_data["Tech"], init_data["load"],
-                            collect(1:timestepcount)))
-# indexing: prodfactor[:PV, Symbol("1X"), 569:571]
+                            (TimeStepCount, LoadCount, TechCount)), [3,2,1]),
+                            (TechNames, LoadNames, TimeSteps))
+# How to index: ProdFactor[:PV, Symbol("1X"), 569:571]
 
 TechToLoadMatrix = NamedArray(permutedims(reshape(
                                 init_data["TechToLoadMatrix"],
-                                (loadcount,techcount)), [2,1]), (init_data["Tech"],
-                                init_data["load"]))
-#indexing techtoloadmatrix[:PV, Symbol("1R")]
+                                (LoadCount,TechCount)), [2,1]), (TechNames,
+                                LoadNames))
+# How to index: TechToLoadMatrix[:PV, Symbol("1R")]
 
 FuelRate = NamedArray(permutedims(reshape(init_data["FuelRate"],
-          (timestepcount, FuelBinCount, techcount)), [3,2,1]),
-          (init_data["Tech"], collect(1:FuelBinCount),
-          collect(1:timestepcount)))
+          (TimeStepCount, FuelBinCount, TechCount)), [3,2,1]),
+          (TechNames, FuelBin, TimeSteps))
+# How to index: FuelRate[:UTIL1, 1, 456:459]
 
 FuelAvail = NamedArray(permutedims(reshape(init_data["FuelAvail"],
-        (collect(1:FuelBinCount), init_data["Tech"])), [2,1]),
-        (init_data["Tech"], collect(1:FuelBinCount)))
+        (FuelBinCount, TechCount)), [2,1]),
+        (TechNames, FuelBin))
+# How to index: FuelAvail[:WINDNM, 1]
+
+
+FuelBurnRateB = NamedArray(permutedims(reshape(init_data["FuelBurnRateB"],
+(FuelBinCount, LoadCount, TechCount)), [3,2,1]),
+(TechNames, LoadNames, FuelBin))
+# How to index: FuelBurnRateB[:WIND, Symbol("1W"), 1]
+
+FuelBurnRateM = NamedArray(permutedims(reshape(init_data["FuelBurnRateM"],
+(FuelBinCount, LoadCount, TechCount)), [3,2,1]),
+(TechNames, LoadNames, FuelBin))
+# How to index: FuelBurnRateM[:WINDNM, Symbol("1X"), 1]
+
+# the commaa after TechNames is necessary
+LevelizationFactor = NamedArray(init_data["LevelizationFactor"], (TechNames,))
+# How to index: LevelizationFactor[:WIND]
+
+LevelizationFactorProdIncent = NamedArray(init_data["LevelizationFactorProdIncent"],
+                                        (TechNames,))
+
+LoadProfile = NamedArray(permutedims(reshape(init_data["LoadProfile"],
+              (TimeStepCount, LoadCount)), [2,1]),
+              (LoadNames, TimeSteps))
+# How to index: LoadProfile[Symbol("1R"), 5683:5690]
+
+MaxDemandInTier = NamedArray(init_data["MaxDemandInTier"],
+                 (DemandBin,))
+
+MaxProdIncent = NamedArray(init_data["MaxProdIncent"], (TechNames,))
+
+MaxSize = NamedArray(init_data["MaxSize"], (TechNames,))
+
+MaxSizeForProdIncent = NamedArray(init_data["MaxSizeForProdIncent"],
+                        (TechNames,))
+
+MaxUsageInTier = NamedArray(init_data["MaxUsageInTier"], (FuelBin,))
+
+MinTurnDown = NamedArray(init_data["MinTurndown"], (TechNames,))
+
+
+
+
+
+
+
+
 
 
 """
