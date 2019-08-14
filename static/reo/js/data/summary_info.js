1ec7dee88350cdadd3dadb6b4f195d16bbde3887
Kwasnik, Ted <Ted.Kwasnik@nrel.gov>
2019-08-14 11:47:13 -0600

    Update summary_info.js

--- static/reo/js/data/summary_info.js
+++ static/reo/js/data/summary_info.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 var summary_info = {"count_all_posts": 1550821, "last_updated": 1565733447, "count_bad_posts": 994}
-=======
-var summary_info = {"count_all_posts": 1503262, "last_updated": 1563998728, "count_bad_posts": 944}
->>>>>>> 2e63cc57ca9f74b4c238d4e1f7a8e51faacdfa4f
