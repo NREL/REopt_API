4888b18d1c002cf8f246fac8a526470c680b5402
Sakshi <sakshi.mishra@nrel.gov>
2019-09-12 18:39:57 +0000

    hot fix - fuel amount default correction

--- reo/nested_inputs.py
+++ reo/nested_inputs.py
@@ -854,7 +854,7 @@ nested_input_definitions = {
           "type": "float",
           "min": 0,
           "max": 1e9,
-          "default": 0,
+          "default": 100,
           "description": "On-site generator fuel available in gallons."
         },
         "min_turn_down_pct": {
