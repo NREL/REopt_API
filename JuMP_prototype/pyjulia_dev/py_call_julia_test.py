311b22946d3f76b3de040101ce3bc52be8f235d3
Nick Laws <nick.laws@nrel.gov>
2019-10-16 10:00:40 -0600

    python script for calling julia now callable

--- JuMP_prototype/pyjulia_dev/py_call_julia_test.py
+++ JuMP_prototype/pyjulia_dev/py_call_julia_test.py
@@ -1,7 +1,16 @@
 import julia
-julia.install()
+# julia.install()
 import time
+import sys
 # from py_call_julia_test import call_julia_func
+
+def array_of_techs(array, techs):
+    tech_symbols = str([":"+t for t in techs])
+    str_array = "[" + " ".join([str(l) for l in array]) + "]"
+    a = "AxisArray({:s}, 1:{:d}, Axis{{:Tech}}({:s}))".format(
+        str_array, len(techs), tech_symbols
+    )
+    return a
 
 def call_julia_func():
     dvRatedProd = dict(
@@ -19,10 +28,26 @@ def call_julia_func():
     t0 = time.time()
     j.include("julia_func.jl")
     # j.mock_reopt(dvRatedProd)
-    j.eval("mock_reopt(AxisArray([[1; 2.1] [2; 1.1]], 1:2, Axis{:Tech}([:pv,:util])))")
+    j.eval("mock_reopt(AxisArray([[1, 2.1] [2, 1.1]], 1:2, Axis{:Tech}([:pv,:util])))")
     """
     The above 'eval' runs successfully, but will require creating a gigantic
     string for all of the values to be passed to Julia.
     """
     elapsed = time.time() - t0
     print("... took {0:.3f} seconds.".format(elapsed))
+
+
+if __name__ == "__main__":
+    techs = ["util", "pv"]
+    array = [[1]*35040]*10 # using list of lists as array since that is what dfm uses
+    a = array_of_techs(array, techs)
+    print("Size of array = {}".format((len(array[0]), len(array))))
+    print("Memory of array = {} MB.".format(sys.getsizeof(array) / 1e6))
+    print("Memory of string = {} MB.".format(sys.getsizeof(a) / 1e6))
+
+"""
+Compare speed of methods:
+1. Create strings from python list of lists with Julia type info, pass to julia via j.eval
+2. Write out python values and read in with type info in julia
+3. Pass as dicts or other? to Julia, convert in Julia
+"""
