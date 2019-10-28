ac8b0e98b792f2185e64dded71c568b2412e8fd5
Nick Laws <nick.laws@nrel.gov>
2019-10-28 16:44:38 -0600

    Update test_blended_rate.py
    - was only testing if blended rate inputs were accepted, not checking results
    - still checking if PV station information is filled into results response

--- reo/tests/test_blended_rate.py
+++ reo/tests/test_blended_rate.py
@@ -2,10 +2,12 @@ import json
 from django.test import TestCase
 from tastypie.test import ResourceTestCaseMixin
 
-# original tariff
 
-#"blended_monthly_rates_us_dollars_per_kwh": [0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2,0.2],
-#"blended_monthly_demand_charges_us_dollars_per_kw": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+class ClassAttributes:
+    def __init__(self, dictionary):
+        for k, v in dictionary.items():
+            setattr(self, k, v)
+
 
 class TestBlendedRate(ResourceTestCaseMixin, TestCase):
 
@@ -29,14 +31,13 @@ class TestBlendedRate(ResourceTestCaseMixin, TestCase):
 
                 "ElectricTariff": {
                     "urdb_rate_name": "custom",
-                    "blended_monthly_rates_us_dollars_per_kwh": [0.15, 0.2, 0.21, 0.23, 0.27, 0.19, 0.22, 0.17, 0.24, 0.26, 0.18,0.2],
+                    "blended_monthly_rates_us_dollars_per_kwh": [0.15, 0.2, 0.21, 0.23, 0.27, 0.19, 0.22, 0.17, 0.24, 0.26, 0.18, 0.2],
                     "blended_monthly_demand_charges_us_dollars_per_kw": [0.08, 0.11, 0, 0, 0.15, 0.14, 0.09, 0.06, 0, 0, 0.05, 0]
                 },
 
                 "PV": {
                     "max_kw": "200",
                     "existing_kw" : "0"
-
                 },
 
                 "Storage": {
@@ -55,45 +56,34 @@ class TestBlendedRate(ResourceTestCaseMixin, TestCase):
     def get_response(self, data):
         initial_post = self.api_client.post(self.submit_url, format='json', data=data)
         uuid = json.loads(initial_post.content)['run_uuid']
-
         response = json.loads(self.api_client.get(self.results_url.replace('<run_uuid>', str(uuid))).content)
-        
         return response
 
-
     def test_blended_rate(self):
-
-        class ClassAttributes:
-            def __init__(self, dictionary):
-                for k, v in dictionary.items():
-                    setattr(self, k, v)
-
+        """
+        Test that self.post scenario, with monthly rates, returns the expected LCC and PV station attributes
+        """
         response = self.get_response(self.post)
 
         pv_out = ClassAttributes(response['outputs']['Scenario']['Site']['PV'])
-        load_out = ClassAttributes(response['outputs']['Scenario']['Site']['LoadProfile'])
         financial = ClassAttributes(response['outputs']['Scenario']['Site']['Financial'])
-        messages = ClassAttributes(response['messages'])
 
         self.assertEqual(pv_out.station_distance_km, 0.0)
         self.assertEqual(pv_out.station_latitude, 35.25)
         self.assertAlmostEqual(pv_out.station_longitude, -91.74, 1)
-
+        self.assertAlmostEqual(financial.lcc_us_dollars, 437842, -1)
 
     def test_blended_annual_rate(self):
-
-        class ClassAttributes:
-            def __init__(self, dictionary):
-                for k, v in dictionary.items():
-                    setattr(self, k, v)
-
-
-        #self.post["Scenario"]["Site"]["ElectricTariff"]["blended_annual_rates_us_dollars_per_kwh"] = 0.2
-        #self.post["Scenario"]["Site"]["ElectricTariff"]["blended_annual_demand_charges_us_dollars_per_kw"] = 0.0
+        """
+        Test that self.post scenario returns the expected LCC with annual blended rates
+        """
+        # remove monthly rates (because they are used before annual rates)
+        self.post["Scenario"]["Site"]["ElectricTariff"]["blended_monthly_rates_us_dollars_per_kwh"] = None
+        self.post["Scenario"]["Site"]["ElectricTariff"]["blended_monthly_demand_charges_us_dollars_per_kw"] = None
+        # add annual rates
+        self.post["Scenario"]["Site"]["ElectricTariff"]["blended_annual_rates_us_dollars_per_kwh"] = 0.2
+        self.post["Scenario"]["Site"]["ElectricTariff"]["blended_annual_demand_charges_us_dollars_per_kw"] = 0.0
 
         response = self.get_response(self.post)
-
-        pv_out = ClassAttributes(response['outputs']['Scenario']['Site']['PV'])
-        load_out = ClassAttributes(response['outputs']['Scenario']['Site']['LoadProfile'])
         financial = ClassAttributes(response['outputs']['Scenario']['Site']['Financial'])
-        messages = ClassAttributes(response['messages'])
+        self.assertAlmostEqual(financial.lcc_us_dollars, 422437, -1)
