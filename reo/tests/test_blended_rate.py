ad5142b75791b9fbf3812d93aa79158162da250a
Nick Laws <nick.laws@nrel.gov>
2019-10-29 09:39:47 -0600

    uncomment skipped tests

--- reo/tests/test_blended_rate.py
+++ reo/tests/test_blended_rate.py
@@ -63,34 +63,34 @@ class TestBlendedRate(ResourceTestCaseMixin, TestCase):
         response = json.loads(self.api_client.get(self.results_url.replace('<run_uuid>', str(uuid))).content)
         return response
 
-    # def test_blended_rate(self):
-    #     """
-    #     Test that self.post scenario, with monthly rates, returns the expected LCC and PV station attributes
-    #     """
-    #     response = self.get_response(self.post)
-    #
-    #     pv_out = ClassAttributes(response['outputs']['Scenario']['Site']['PV'])
-    #     financial = ClassAttributes(response['outputs']['Scenario']['Site']['Financial'])
-    #
-    #     self.assertEqual(pv_out.station_distance_km, 0.0)
-    #     self.assertEqual(pv_out.station_latitude, 35.25)
-    #     self.assertAlmostEqual(pv_out.station_longitude, -91.74, 1)
-    #     self.assertAlmostEqual(financial.lcc_us_dollars, 437842, -1)
-    #
-    # def test_blended_annual_rate(self):
-    #     """
-    #     Test that self.post scenario returns the expected LCC with annual blended rates
-    #     """
-    #     # remove monthly rates (because they are used before annual rates)
-    #     self.post["Scenario"]["Site"]["ElectricTariff"]["blended_monthly_rates_us_dollars_per_kwh"] = None
-    #     self.post["Scenario"]["Site"]["ElectricTariff"]["blended_monthly_demand_charges_us_dollars_per_kw"] = None
-    #     # add annual rates
-    #     self.post["Scenario"]["Site"]["ElectricTariff"]["blended_annual_rates_us_dollars_per_kwh"] = 0.2
-    #     self.post["Scenario"]["Site"]["ElectricTariff"]["blended_annual_demand_charges_us_dollars_per_kw"] = 0.0
-    #
-    #     response = self.get_response(self.post)
-    #     financial = ClassAttributes(response['outputs']['Scenario']['Site']['Financial'])
-    #     self.assertAlmostEqual(financial.lcc_us_dollars, 422437, -1)
+    def test_blended_rate(self):
+        """
+        Test that self.post scenario, with monthly rates, returns the expected LCC and PV station attributes
+        """
+        response = self.get_response(self.post)
+
+        pv_out = ClassAttributes(response['outputs']['Scenario']['Site']['PV'])
+        financial = ClassAttributes(response['outputs']['Scenario']['Site']['Financial'])
+
+        self.assertEqual(pv_out.station_distance_km, 0.0)
+        self.assertEqual(pv_out.station_latitude, 35.25)
+        self.assertAlmostEqual(pv_out.station_longitude, -91.74, 1)
+        self.assertAlmostEqual(financial.lcc_us_dollars, 437842, -1)
+
+    def test_blended_annual_rate(self):
+        """
+        Test that self.post scenario returns the expected LCC with annual blended rates
+        """
+        # remove monthly rates (because they are used before annual rates)
+        self.post["Scenario"]["Site"]["ElectricTariff"]["blended_monthly_rates_us_dollars_per_kwh"] = None
+        self.post["Scenario"]["Site"]["ElectricTariff"]["blended_monthly_demand_charges_us_dollars_per_kw"] = None
+        # add annual rates
+        self.post["Scenario"]["Site"]["ElectricTariff"]["blended_annual_rates_us_dollars_per_kwh"] = 0.2
+        self.post["Scenario"]["Site"]["ElectricTariff"]["blended_annual_demand_charges_us_dollars_per_kw"] = 0.0
+
+        response = self.get_response(self.post)
+        financial = ClassAttributes(response['outputs']['Scenario']['Site']['Financial'])
+        self.assertAlmostEqual(financial.lcc_us_dollars, 422437, -1)
 
     def test_time_of_export_rate(self):
         """
