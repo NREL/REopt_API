1d76b69e96fbe5641a6105c4700ac778e113db4e
Nick Laws <nick.laws@nrel.gov>
2019-10-30 10:26:32 -0600

    remove django.contrib.admin app since it is unused and now requires more middleware

--- reopt_api/staging_settings.py
+++ reopt_api/staging_settings.py
@@ -35,7 +35,6 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
