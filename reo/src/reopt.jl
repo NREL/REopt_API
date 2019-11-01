5b7d90eadf1b0789c8019f32a783ddfe3210b8e1
Nick Laws <nick.laws@nrel.gov>
2019-11-01 12:07:12 -0600

    pass MAXTIME to Xpress

--- reo/src/reopt.jl
+++ reo/src/reopt.jl
@@ -4,9 +4,15 @@ reopt:
 - Author: nlaws
 - Date: 2019-11-01
 =#
+using JuMP
+using Xpress
 
 
 function reopt(data)
    data["outputs"]["Scenario"]["status"] = "optimal"
-   return data
+    MAXTIME = data["inputs"]["Scenario"]["timeout_seconds"]
+    REopt = direct_model(Xpress.Optimizer(MAXTIME=MAXTIME))
+    @objective(REopt, Min, 1)
+    optimize!(REopt)
+    return data
 end
