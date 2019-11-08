a56e070afb2ab08717c7be6b82d040412114bfcc
Nick Laws <nick.laws@nrel.gov>
2019-11-08 16:58:20 -0700

    reo.api: replace reopt.py with run_jump_model
    as well as new call_back for process_results

--- reo/api.py
+++ reo/api.py
@@ -13,9 +13,8 @@ from scenario import setup_scenario
 from reo.log_levels import log
 from reo.models import ModelManager, BadPost
 from reo.src.profiler import Profiler
-from reo.src.reopt import reopt
+from reo.process_results import process_results
 from reo.src.run_jump_model import run_jump_model
-from reo.results import parse_run_outputs
 from reo.exceptions import REoptError, UnexpectedError
 from celery import group, chain
 
@@ -142,14 +141,14 @@ class Job(ModelResource):
                                                          content_type='application/json',
                                                          status=500))  # internal server error
         setup = setup_scenario.s(run_uuid=run_uuid, data=data, raw_post=bundle.data)
-        call_back = parse_run_outputs.s(data=data, meta={'run_uuid': run_uuid, 'api_version': api_version})
+        call_back = process_results.s(data=data, meta={'run_uuid': run_uuid, 'api_version': api_version})
         # (use .si for immutable signature, if no outputs were passed from reopt_jobs)
-        # rjm = run_jump_model.s(data=data, run_uuid=run_uuid)
-        # rjm_bau = run_jump_model.s(data=data, run_uuid=run_uuid, bau=True)
+        rjm = run_jump_model.s(data=data, run_uuid=run_uuid)
+        rjm_bau = run_jump_model.s(data=data, run_uuid=run_uuid, bau=True)
 
         log.info("Starting celery chain")
         try:
-            chain(setup | group(reopt.s(data=data, run_uuid=run_uuid, bau=False), reopt.s(data=data, run_uuid=run_uuid, bau=True))  | call_back)()
+            chain(setup | group(rjm, rjm_bau) | call_back)()
         except Exception as e:
             if isinstance(e, REoptError):
                 pass  # handled in each task
