49b7ee42d59d817506d308efde267b4fe53730bc
Nick Muerdter <12112+GUI@users.noreply.github.com>
2019-12-03 10:31:28 -0700

    Adjusting staging domains for new *reopt-stage-api.its.nrel.gov setup.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,8 +2,8 @@ server "reopt-stage-api1.nrel.gov", :user => "deploy", :roles => ["web", "app", 
 
 set :app_env, "staging"
 set :django_settings_module, "reopt_api.staging_settings"
-set :base_domain, "reopt-stage-api.nrel.gov"
-set :base_domain_aliases, ["reopt-stage-api1.nrel.gov", "reopt-stage-api2.nrel.gov"]
+set :base_domain, "reopt-stage-api.its.nrel.gov"
+set :base_domain_aliases, ["reopt-stage-api.nrel.gov", "reopt-stage-api1.nrel.gov", "reopt-stage-api2.nrel.gov"]
 
 # TODO: Remove this if we get more flexible branched deployments setup (with
 # our normal DNS subdomain support).
