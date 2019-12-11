ab4d190fe56f3d1a3125422a350612fef88622be
Nick Laws <nick.laws@nrel.gov>
2019-12-11 15:52:20 -0700

    rm some unneeded paths from paths.py
    these were making now obsolete directories

--- reo/src/paths.py
+++ reo/src/paths.py
@@ -12,19 +12,13 @@ class Paths(object):
         self.egg = os.getcwd()
         self.templates = os.path.join(self.egg, "Xpress")
         self.xpress = os.path.join(self.egg, "Xpress")
-
         self.run = os.path.join(self.xpress, "Run" + str(run_uuid))
-        self.files_to_download = os.path.join(self.xpress, "Downloads")
-
         self.inputs = os.path.join(self.run, "Inputs")
-        self.outputs = os.path.join(self.run, "Outputs")
-        self.outputs_bau = os.path.join(self.run, "Outputs_bau")
         self.static_outputs = os.path.join(self.egg, "static", "files", str(run_uuid))
-        self.utility = os.path.join(self.inputs, "Utility")
 
         if os.path.exists(self.run):
             shutil.rmtree(self.run)
 
-        for f in [self.run, self.inputs, self.outputs, self.outputs_bau, self.static_outputs, self.utility]:
+        for f in [self.run, self.inputs, self.static_outputs]:
             os.mkdir(f)
             check_directory_created(f)
