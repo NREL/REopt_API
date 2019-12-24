1ea653be08b7541494460643e6f5d665fddeca09
TK-23 <tedkwasnik@gmail.com>
2019-12-24 12:56:36 -0700

    add whitespace

--- resilience_stats/views.py
+++ resilience_stats/views.py
@@ -166,10 +166,10 @@ def resilience_stats(request, run_uuid=None, financial_check=None):
                     }
                     kwargs_dict["bau"] = kwargs
 
-                
                 p = {k: pool.apply_async(simulate_outage, tuple(), v) for k, v in kwargs_dict.items()}
                 pool.close()
                 pool.join()
+
                 for k, v in p.items():
                     if k == 'wtch':
                         results.update(v.get())
