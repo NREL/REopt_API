a873a98c1d785d26c0899b36d817e3db85d047ac
Nick Laws <nick.laws@nrel.gov>
2020-01-17 14:19:16 -0700

    add timeout to cbc model

--- reo/src/reopt_cbc_model.jl
+++ reo/src/reopt_cbc_model.jl
@@ -1,9 +1,9 @@
 using JuMP
 using Cbc
 
-function reopt_model()
+function reopt_model(seconds::Float64)
    
-    REopt = Model(with_optimizer(Cbc.Optimizer, logLevel=1))
+    REopt = Model(with_optimizer(Cbc.Optimizer, logLevel=1, seconds=seconds))
 	return REopt
 
 end
