281eaaa9b245f48a99bebfc76e712be1c208032e
Nick Laws <NLaws@users.noreply.github.com>
2020-02-04 15:41:29 -0700

    Update make_keys.py.sh

--- .github/scripts/make_keys.py.sh
+++ .github/scripts/make_keys.py.sh
@@ -3,4 +3,4 @@
 cp keys.py.template keys.py
 echo "pvwatts_api_key = '${NREL_DEV_API_KEY}'" >> keys.py
 echo "developer_nrel_gov_key = '${NREL_DEV_API_KEY}'" >> keys.py
-
+echo "Successfully created keys.py"
