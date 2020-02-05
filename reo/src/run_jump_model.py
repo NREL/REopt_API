63ae5a5353e67d5b02d0a4f3b829587a33b68383
jpohl <josiahpohl@live.com>
2020-02-05 12:04:16 -0700

    removed try except statement patch

--- reo/src/run_jump_model.py
+++ reo/src/run_jump_model.py
@@ -59,11 +59,7 @@ def run_jump_model(self, dfm, data, run_uuid, bau=False):
         j.using("Pkg")
         if os.environ.get("SOLVER") == "xpress":
             j.eval('Pkg.activate("./julia_envs/Xpress/")')
-            try:
-                j.include("reo/src/reopt_xpress_model.jl")
-            except ImportError:
-                j.eval("Pkg.instantiate()")
-                j.include("reo/src/reopt_xpress_model.jl")
+            j.include("reo/src/reopt_xpress_model.jl")
             model = j.reopt_model(data["inputs"]["Scenario"]["timeout_seconds"])
         elif os.environ.get("SOLVER") == "cbc":
             j.eval('Pkg.activate("./julia_envs/Cbc/")')
