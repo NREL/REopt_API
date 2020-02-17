665d48775ab8396024f1285237f0f730a4e344e6
Nick Laws <nick.laws@nrel.gov>
2020-02-17 12:39:23 -0700

    clarify what "financial_check" does
    outage_simulator was sometimes not actually simulating any outages and just checking if sizes matched between resilience and financial scenarios

--- resilience_stats/outage_simulator_LF.py
+++ resilience_stats/outage_simulator_LF.py
@@ -111,7 +111,7 @@ class NoBattery():
 
 def simulate_outage(batt_kwh=0, batt_kw=0, pv_kw_ac_hourly=0, init_soc=0, critical_loads_kw=[], wind_kw_ac_hourly=None,
                     batt_roundtrip_efficiency=0.829, diesel_kw=0, fuel_available=0, b=0, m=0, diesel_min_turndown=0.3,
-                    financial_check=None, resilience_run_site_result={}, financial_run_site_result={}):
+                    ):
     """
     :param batt_kwh: float, battery storage capacity
     :param batt_kw: float, battery inverter capacity
@@ -125,20 +125,9 @@ def simulate_outage(batt_kwh=0, batt_kw=0, pv_kw_ac_hourly=0, init_soc=0, critic
     :param b: float, diesel fuel burn rate intercept coefficient (y = m*x + b*rated_capacity)  [gal/kwh/kw]
     :param m: float, diesel fuel burn rate slope (y = m*x + b*rated_capacity)  [gal/kWh]
     :param diesel_min_turndown: minimum generator turndown in fraction of generator capacity (0 to 1)
-    :param financial_check: string, check financial and resilience system size if equal "financial_check"
-    :param resilience_run_site_result: dict, resilience run results{ "pv_size_kw": float,
-                                                                    "storage_size_kw": float,
-                                                                    "storage_size_kwh": float,
-                                                                    "gen_size_kw": float,
-                                                                    "wind_size_kw": float }
-    :param financial_run_site_result: dict, financial run results{ "pv_size_kw": float,
-                                                                    "storage_size_kw": float,
-                                                                    "storage_size_kwh": float,
-                                                                    "gen_size_kw": float,
-                                                                    "wind_size_kw": float }
     :return: list of hours survived for outages starting at every time step, plus min,max,avg of list
-    :return: boolean, survives_specified_outage
     """
+<<<<<<< HEAD
     # capping fuel availability to 660 - default fuel availability is set at 1e9 for the optimization process
     # for the outage simulator, the fuel availability is capped at 660.
     if fuel_available == 1e9:
@@ -156,149 +145,149 @@ def simulate_outage(batt_kwh=0, batt_kw=0, pv_kw_ac_hourly=0, init_soc=0, critic
         else:
             return False
 
+=======
+    n_timesteps = len(critical_loads_kw)
+    n_steps_per_hour = n_timesteps / 8760  # type: int
+
+    r = [0] * n_timesteps
+
+    # NOTE: not making hourly load assumptions: a kW is not equivalent to a kWh!!!
+
+    if batt_kw == 0 or batt_kwh == 0:
+        init_soc = [0] * n_timesteps  # default is None
+
+        if ((pv_kw_ac_hourly in [None, []]) or (sum(pv_kw_ac_hourly) == 0)) and diesel_kw == 0:  # no pv, generator, nor battery --> no resilience
+
+            months = 12
+            hours = 24
+            probs_of_surviving_by_month = [[0] for _ in range(months)]
+            probs_of_surviving_by_hour_of_the_day = [[0] for _ in range(hours)]
+            return {"resilience_by_timestep": r,
+                    "resilience_hours_min": 0,
+                    "resilience_hours_max": 0,
+                    "resilience_hours_avg": 0,
+                    "outage_durations": [],
+                    "probs_of_surviving": [],
+                    "probs_of_surviving_by_month": probs_of_surviving_by_month,
+                    "probs_of_surviving_by_hour_of_the_day": probs_of_surviving_by_hour_of_the_day,
+                    }
+
+    if pv_kw_ac_hourly in [None, []]:
+        pv_kw_ac_hourly = [0] * n_timesteps
+    if wind_kw_ac_hourly in [None, []]:
+        wind_kw_ac_hourly = [0] * n_timesteps
+
+    def load_following(critical_load, pv, wind, generator, battery, n_steps_per_hour):
+        """
+        Dispatch strategy for one time step
+        """
+
+        # distributed generation minus load is the burden on battery
+        unmatch = (critical_load - pv - wind)  # kw
+
+        if unmatch < 0:    # pv + wind> critical_load
+            # excess PV power to charge battery
+            charge = battery.batt_charge(-unmatch, n_steps_per_hour)
+            unmatch = 0
+
+        elif generator.genmin <= generator.gen_avail(n_steps_per_hour) and 0 < generator.kw:
+            gen_output = generator.fuel_consume(unmatch, n_steps_per_hour)
+            # charge battery with excess energy if unmatch < genmin
+            charge = battery.batt_charge(max(gen_output - unmatch, 0), n_steps_per_hour)  # prevent negative charge
+            discharge = battery.batt_discharge(max(unmatch - gen_output, 0), n_steps_per_hour)  # prevent negative discharge
+            unmatch -= (gen_output + discharge - charge)
+
+        elif unmatch <= battery.batt_avail(n_steps_per_hour):   # battery can carry balance
+            discharge = battery.batt_discharge(unmatch, n_steps_per_hour)
+            unmatch = 0
+
+        return unmatch, generator, battery
+
+    '''
+    Simulation starts here
+    '''
+    if diesel_kw == 0 or fuel_available == 0:
+        GEN = NoGenerator(diesel_kw, fuel_available, b, m, diesel_min_turndown)
+>>>>>>> 6cf671b3... clarify what "financial_check" does
     else:
-        # Do outage simulator
-        n_timesteps = len(critical_loads_kw)
-        n_steps_per_hour = n_timesteps / 8760  # type: int
+        GEN = Generator(diesel_kw, fuel_available, b, m, diesel_min_turndown)
 
-        r = [0] * n_timesteps
+    if batt_kw == 0 or batt_kwh == 0:
+        BATT = NoBattery(batt_kwh, batt_kw, batt_roundtrip_efficiency)
+    else:
+        BATT = Battery(batt_kwh, batt_kw, batt_roundtrip_efficiency)
 
-        # NOTE: not making hourly load assumptions: a kW is not equivalent to a kWh!!!
+    for time_step in range(n_timesteps):
+        gen = copy.deepcopy(GEN)
+        batt = copy.deepcopy(BATT)
+        # outer loop: do simulation starting at each time step
+        batt.soc = init_soc[time_step]   # reset battery for each simulation
 
-        if batt_kw == 0 or batt_kwh == 0:
-            init_soc = [0] * n_timesteps  # default is None
+        for i in range(n_timesteps):    # the i-th time step of simulation
+            # inner loop: step through all possible surviving time steps
+            # break inner loop if can not survive
+            t = (time_step + i) % n_timesteps
 
-            if ((pv_kw_ac_hourly in [None, []]) or (sum(pv_kw_ac_hourly) == 0)) and diesel_kw == 0:  # no pv, generator, nor battery --> no resilience
+            unmatch, gen, batt = load_following(
+                critical_loads_kw[t], pv_kw_ac_hourly[t], wind_kw_ac_hourly[t], gen, batt, n_steps_per_hour)
 
-                months = 12
-                hours = 24
-                probs_of_surviving_by_month = [[0] for _ in range(months)]
-                probs_of_surviving_by_hour_of_the_day = [[0] for _ in range(hours)]
-                return {"resilience_by_timestep": r,
-                        "resilience_hours_min": 0,
-                        "resilience_hours_max": 0,
-                        "resilience_hours_avg": 0,
-                        "outage_durations": [],
-                        "probs_of_surviving": [],
-                        "probs_of_surviving_by_month": probs_of_surviving_by_month,
-                        "probs_of_surviving_by_hour_of_the_day": probs_of_surviving_by_hour_of_the_day,
-                        }
+            if unmatch > 0 or i == (n_timesteps-1):  # cannot survive
+                r[time_step] = float(i) / float(n_steps_per_hour)
+                break
 
-        if pv_kw_ac_hourly in [None, []]:
-            pv_kw_ac_hourly = [0] * n_timesteps
-        if wind_kw_ac_hourly in [None, []]:
-            wind_kw_ac_hourly = [0] * n_timesteps
+    r_min = min(r)
+    r_max = max(r)
+    r_avg = round((float(sum(r)) / float(len(r))), 2)
 
-        def load_following(critical_load, pv, wind, generator, battery, n_steps_per_hour):
-            """
-            Dispatch strategy for one time step
-            """
+    # Create a time series of 8760*n_steps_per_hour elements starting on 1/1/2017
+    time = pd.date_range('1/1/2017', periods=8760*n_steps_per_hour, freq='{}min'.format(n_steps_per_hour*60))
+    r_series = pd.Series(r, index=time)
 
-            # distributed generation minus load is the burden on battery
-            unmatch = (critical_load - pv - wind)  # kw
+    r_group_month = r_series.groupby(r_series.index.month)
+    r_group_hour = r_series.groupby(r_series.index.hour)
 
-            if unmatch < 0:    # pv + wind> critical_load
-                # excess PV power to charge battery
-                charge = battery.batt_charge(-unmatch, n_steps_per_hour)
-                unmatch = 0
+    x_vals = list(range(1, int(floor(r_max)+1)))
+    y_vals = list()
+    y_vals_group_month = list()
+    y_vals_group_hour = list()
 
-            elif generator.genmin <= generator.gen_avail(n_steps_per_hour) and 0 < generator.kw:
-                gen_output = generator.fuel_consume(unmatch, n_steps_per_hour)
-                # charge battery with excess energy if unmatch < genmin
-                charge = battery.batt_charge(max(gen_output - unmatch, 0), n_steps_per_hour)  # prevent negative charge
-                discharge = battery.batt_discharge(max(unmatch - gen_output, 0), n_steps_per_hour)  # prevent negative discharge
-                unmatch -= (gen_output + discharge - charge)
+    for hrs in x_vals:
+        y_vals.append(round(float(sum([1 if h >= hrs else 0 for h in r])) / float(n_timesteps), 4))
 
-            elif unmatch <= battery.batt_avail(n_steps_per_hour):   # battery can carry balance
-                discharge = battery.batt_discharge(unmatch, n_steps_per_hour)
-                unmatch = 0
+    width = 0
+    for k, v in r_group_month:
+        tmp = list()
+        max_hr = int(v.max()) + 1
+        for hrs in range(max_hr):
+            tmp.append(round(float(sum([1 if h >= hrs else 0 for h in v])) / float(len(v)), 4))
+        y_vals_group_month.append(tmp)
+        if max_hr > width:
+            width = max_hr
 
-            return unmatch, generator, battery
+    # PostgreSQL requires that the arrays are rectangular
+    for i, v in enumerate(y_vals_group_month):
+        y_vals_group_month[i] = v + [0] * (width - len(v))
 
-        '''
-        Simulation starts here
-        '''
-        if diesel_kw == 0 or fuel_available == 0:
-            GEN = NoGenerator(diesel_kw, fuel_available, b, m, diesel_min_turndown)
-        else:
-            GEN = Generator(diesel_kw, fuel_available, b, m, diesel_min_turndown)
+    width = 0
+    for k, v in r_group_hour:
+        tmp = list()
+        max_hr = int(v.max()) + 1
+        for hrs in range(max_hr):
+            tmp.append(round(float(sum([1 if h >= hrs else 0 for h in v])) / float(len(v)), 4))
+        y_vals_group_hour.append(tmp)
+        if max_hr > width:
+            width = max_hr
 
-        if batt_kw == 0 or batt_kwh == 0:
-            BATT = NoBattery(batt_kwh, batt_kw, batt_roundtrip_efficiency)
-        else:
-            BATT = Battery(batt_kwh, batt_kw, batt_roundtrip_efficiency)
+    # PostgreSQL requires that the arrays are rectangular
+    for i, v in enumerate(y_vals_group_hour):
+        y_vals_group_hour[i] = v + [0] * (width - len(v))
 
-        for time_step in range(n_timesteps):
-            gen = copy.deepcopy(GEN)
-            batt = copy.deepcopy(BATT)
-            # outer loop: do simulation starting at each time step
-            batt.soc = init_soc[time_step]   # reset battery for each simulation
-
-            for i in range(n_timesteps):    # the i-th time step of simulation
-                # inner loop: step through all possible surviving time steps
-                # break inner loop if can not survive
-                t = (time_step + i) % n_timesteps
-
-                unmatch, gen, batt = load_following(
-                    critical_loads_kw[t], pv_kw_ac_hourly[t], wind_kw_ac_hourly[t], gen, batt, n_steps_per_hour)
-
-                if unmatch > 0 or i == (n_timesteps-1):  # cannot survive
-                    r[time_step] = float(i) / float(n_steps_per_hour)
-                    break
-
-        r_min = min(r)
-        r_max = max(r)
-        r_avg = round((float(sum(r)) / float(len(r))), 2)
-
-        # Create a time series of 8760*n_steps_per_hour elements starting on 1/1/2017
-        time = pd.date_range('1/1/2017', periods=8760*n_steps_per_hour, freq='{}min'.format(n_steps_per_hour*60))
-        r_series = pd.Series(r, index=time)
-
-        r_group_month = r_series.groupby(r_series.index.month)
-        r_group_hour = r_series.groupby(r_series.index.hour)
-
-        x_vals = list(range(1, int(floor(r_max)+1)))
-        y_vals = list()
-        y_vals_group_month = list()
-        y_vals_group_hour = list()
-
-        for hrs in x_vals:
-            y_vals.append(round(float(sum([1 if h >= hrs else 0 for h in r])) / float(n_timesteps), 4))
-
-        width = 0
-        for k, v in r_group_month:
-            tmp = list()
-            max_hr = int(v.max()) + 1
-            for hrs in range(max_hr):
-                tmp.append(round(float(sum([1 if h >= hrs else 0 for h in v])) / float(len(v)), 4))
-            y_vals_group_month.append(tmp)
-            if max_hr > width:
-                width = max_hr
-
-        # PostgreSQL requires that the arrays are rectangular
-        for i, v in enumerate(y_vals_group_month):
-            y_vals_group_month[i] = v + [0] * (width - len(v))
-
-        width = 0
-        for k, v in r_group_hour:
-            tmp = list()
-            max_hr = int(v.max()) + 1
-            for hrs in range(max_hr):
-                tmp.append(round(float(sum([1 if h >= hrs else 0 for h in v])) / float(len(v)), 4))
-            y_vals_group_hour.append(tmp)
-            if max_hr > width:
-                width = max_hr
-
-        # PostgreSQL requires that the arrays are rectangular
-        for i, v in enumerate(y_vals_group_hour):
-            y_vals_group_hour[i] = v + [0] * (width - len(v))
-
-        return {"resilience_by_timestep": r,
-                "resilience_hours_min": r_min,
-                "resilience_hours_max": r_max,
-                "resilience_hours_avg": r_avg,
-                "outage_durations": x_vals,
-                "probs_of_surviving": y_vals,
-                "probs_of_surviving_by_month": y_vals_group_month,
-                "probs_of_surviving_by_hour_of_the_day": y_vals_group_hour,
-                }
+    return {"resilience_by_timestep": r,
+            "resilience_hours_min": r_min,
+            "resilience_hours_max": r_max,
+            "resilience_hours_avg": r_avg,
+            "outage_durations": x_vals,
+            "probs_of_surviving": y_vals,
+            "probs_of_surviving_by_month": y_vals_group_month,
+            "probs_of_surviving_by_hour_of_the_day": y_vals_group_hour,
+            }
