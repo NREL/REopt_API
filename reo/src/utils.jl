060db15d925a491b1fec87993fa4218fbcd9146f
Nick Laws <nick.laws@nrel.gov>
2020-06-04 18:19:20 -0600

    specify TechsNoTurndown::Array{String,1} in utils.jl

--- reo/src/utils.jl
+++ reo/src/utils.jl
@@ -77,7 +77,7 @@ Base.@kwdef struct Parameter
 	 #ElectricChillers::Array{String,1}  # T^{ec} \subset T: Electric chillers  (IGNORE) 
 	 FuelBurningTechs::Array{String,1}  # T^{f} \subset T: Fuel-burning technologies
 	 #HeatingTechs::Array{String,1}  # T^{ht} \subset T: Heating technologies (IGNORE)
-	 #TechsNoTurndown::Array{String,1}  # T^{ac} \subset T: Technologies that cannot turn down, i.e., PV and wind
+	 TechsNoTurndown::Array{String,1}  # T^{ac} \subset T: Technologies that cannot turn down, i.e., PV and wind
 	 #TechsTurndown::Array{String,1}  # This is just T \setminus T^{ac}; not in the math
 	 #PricingTiersByTech::AxisArray{Array{Int64,1},1,Array{Array{String,1},1},Tuple{Axis{:row,UnitRange{Int64}}}}   # U_t \subset U:  Pricing tiers accessible by technology td
 	 #PricingTiersNM::Array{Int64}  # U^{nm} \subset U: Pricing Tiers Used in net-metering
@@ -211,7 +211,6 @@ Base.@kwdef struct Parameter
      SubdivisionByTech
      TechsInClass
      TechsByFuelType
-     TechsNoTurndown
      SalesTiers
      StorageSalesTiers
      NonStorageSalesTiers
