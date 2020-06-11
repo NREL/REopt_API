f2699619a8e5bf0cc09fdaf8097d5ddd4f5fdf88
Nick Laws <nick.laws@nrel.gov>
2020-06-10 20:38:39 -0600

    Update internal_c110p_settings.py

--- reopt_api/internal_c110p_settings.py
+++ reopt_api/internal_c110p_settings.py
@@ -1,3 +1,32 @@
+# *********************************************************************************
+# REopt, Copyright (c) 2019-2020, Alliance for Sustainable Energy, LLC.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+# Redistributions of source code must retain the above copyright notice, this list
+# of conditions and the following disclaimer.
+#
+# Redistributions in binary form must reproduce the above copyright notice, this
+# list of conditions and the following disclaimer in the documentation and/or other
+# materials provided with the distribution.
+#
+# Neither the name of the copyright holder nor the names of its contributors may be
+# used to endorse or promote products derived from this software without specific
+# prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+# OF THE POSSIBILITY OF SUCH DAMAGE.
+# *********************************************************************************
 from keys import *
 import sys
 """
@@ -45,7 +74,6 @@ INSTALLED_APPS = (
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'rollbar.contrib.django.middleware.RollbarNotifierMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -108,10 +136,10 @@ CELERY_RESULT_BACKEND = 'django-db'
 
 # celery task registration
 CELERY_IMPORTS = (
-    'reo.src.reopt',
     'reo.api',
     'reo.scenario',
-    'reo.results',
+    'reo.process_results',
+    'reo.src.run_jump_model',
     'resilience_stats.outage_simulator_LF',
 )
 
@@ -123,14 +151,6 @@ if 'test' in sys.argv:
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-ROLLBAR = {
-    'access_token': rollbar_access_token,
-    'environment': 'internal_c110p',
-    'root': BASE_DIR,
-    'branch': os.environ.get('BRANCH_NAME')
-}
 
-import rollbar
-rollbar.init(**ROLLBAR)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "reopt_api.internal_c110p_settings")
 django.setup()
