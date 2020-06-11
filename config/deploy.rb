01a888b4f19d0aa48d1ff4793863c89955a2eba5
Nick Laws <nick.laws@nrel.gov>
2020-06-11 16:09:51 -0600

    fix quotes in deploy.rb julie execute

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ namespace :app do
         execute "./env/bin/pip3", "install", "-r", "requirements.txt"
         execute "julia", "--project='./julia_envs/Xpress/'", "-e", "'import Pkg; Pkg.instantiate();'"
         execute "julia", "--project='./julia_envs/Xpress/'", "./julia_envs/Xpress/precompile.jl"
-        execute "julia", "-e", "'ENV[\"PYTHON\"]=#{release_path}*\"/env/bin/python\"; import Pkg; Pkg.build(\"PyCall\")'"
+        execute "julia", "-e", "'ENV[\"PYTHON\"]=\"#{release_path}\"*\"/env/bin/python\"; import Pkg; Pkg.build(\"PyCall\")'"
         execute "./env/bin/python", "-c", "'import julia; julia.install()'"
       end
     end
