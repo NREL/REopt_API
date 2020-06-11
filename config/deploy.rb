df6b69e1997e3ca4592dc7e9291bd6271933c334
Nick Laws <nick.laws@nrel.gov>
2020-06-10 19:35:06 -0600

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,11 +21,11 @@ namespace :app do
   task :pip_install do
     on roles(:app) do
       within release_path do
+        execute "rm", "-rf", "env"
         execute "virtualenv", "env", "--python=/bin/python3"
         execute "./env/bin/pip3", "install", "-r", "requirements.txt"
         execute "julia", "--project='./julia_envs/Xpress/'", "-e", "'import Pkg; Pkg.instantiate();'"
         execute "julia", "--project='./julia_envs/Xpress/'", "./julia_envs/Xpress/precompile.jl"
-        execute "rm", "-rf", "home/deploy/.julia/conda"
         execute "./env/bin/python", "-c", "'import julia; julia.install()'"
       end
     end
