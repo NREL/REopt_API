e35f3b92cbf1f634d761d73b1e7ba8780f29bad4
Nick Laws <NLaws@users.noreply.github.com>
2020-06-19 09:50:50 -0600

    Merge deploy branch into develop (#63)
    - Mostly changes for deploying the API to NREL servers
        - supporting develop and c110p servers so far
        - using a Julia system image to speed up API on servers
            - image is built with `PackageCompiler.jl` in new `build_julia_image.jl`
    - updated Julia packages to get latest bug fixes and efficiency improvements
    - tested with Julia 1.3 and 1.4
    - add detailed timing outputs for Julia parts of code for assessing improvements

---
+++ julia_envs/Xpress/build_julia_image.jl
@@ -0,0 +1,37 @@
+using Pkg
+using PackageCompiler
+
+
+function build_julia_image(project_path::String)
+    
+    # cwd = home/deploy ! PyCall not in Project?
+    Pkg.activate(joinpath(project_path, "julia_envs", "Xpress"))
+    Pkg.instantiate(verbose=true)
+    Pkg.add("PackageCompiler")
+
+    # must point pycall to the python path we want it to use
+    ENV["PYTHON"] = joinpath(project_path, "env", "bin", "python")
+    println("Python path for PyCall: ", ENV["PYTHON"])
+    Pkg.build("PyCall")
+    Pkg.build("Xpress")
+    include(joinpath(project_path, "julia_envs", "Xpress", "precompile.jl"))
+
+    # TODO create REopt module to attach to Base for system image
+    # include(joinpath(project_path, "reo", "src", "reopt_xpress_model.jl"))
+    # include(joinpath(project_path, "reo", "src", "reopt.jl"))
+
+    if Sys.islinux()
+        ext = ".so"
+    elseif Sys.isapple()
+        ext = ".dylib"
+    elseif Sys.iswindows()
+        ext = ".dll"
+    else
+        error("Unsupported operating system")
+    end
+
+    PackageCompiler.create_sysimage(
+        [:AxisArrays, :JuMP, :MathOptInterface, :PyCall, :Xpress, :MutableArithmetics],
+        sysimage_path=joinpath(project_path, "julia_envs", "Xpress", "JuliaXpressSysimage" * ext)
+    )
+end
