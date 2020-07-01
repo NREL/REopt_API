407317540b570d38fe3b09156e9e7c3acb89c75d
Nick Laws <NLaws@users.noreply.github.com>
2020-07-01 15:12:50 -0600

    set memory limits for reopt_xpress_model.jl
    to address issues with celery tasks getting killed due to OOM on develop server
    also need to limit number of concurrent workers

--- reo/src/reopt_xpress_model.jl
+++ reo/src/reopt_xpress_model.jl
@@ -3,7 +3,9 @@ using Xpress
 
 function reopt_model(MAXTIME)
    
-    REopt = direct_model(Xpress.Optimizer(MAXTIME=-MAXTIME, OUTPUTLOG = 0))
-	return REopt
+    REopt = direct_model(Xpress.Optimizer(MAXTIME=-MAXTIME, OUTPUTLOG = 0, 
+		RESOURCESTRATEGY = 1, MAXMEMORYSOFT = 4000, TREEMEMORYLIMIT = 4000))
+    # assuming that MAXMEMORYSOFT units are megabytes (b/c TREEMEMORYLIMIT units are megabytes)
+    return REopt
 
 end
