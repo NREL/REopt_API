6d7e5b89d44dca510564a4952af82d72b6c0ec9c
zolanaj <alexander.zolan@nrel.gov>
2020-07-15 08:18:33 -0600

    add 'load' and 'util' to cleanup in scenario.py

--- reo/scenario.py
+++ reo/scenario.py
@@ -336,7 +336,7 @@ def setup_scenario(self, run_uuid, data, raw_post):
 
         # delete python objects, which are not serializable
 
-        for k in ['storage', 'hot_tes', 'cold_tes', 'site', 'elec_tariff', 'fuel_tariff', 'pvs', 'pvnms'] + dfm.available_techs:
+        for k in ['storage', 'hot_tes', 'cold_tes', 'site', 'elec_tariff', 'fuel_tariff', 'pvs', 'pvnms', 'load', 'util'] + dfm.available_techs:
             if dfm_dict.get(k) is not None:
                 del dfm_dict[k]
 
