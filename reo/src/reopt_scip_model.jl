881d734842cbebec3bb4b6c0b45c917ab92c8856
zolanaj <alexander.zolan@nrel.gov>
2020-07-16 15:25:01 -0600

    adding model attributes, optimality_tolerance argument for all solvers
    - adds the mip gap tolerance when creating a SCIP or Cbc model
    - adds solver_name, timeout_seconds, and optimality_tolerance as members of the empty model for use in the decomposition

--- reo/src/reopt_scip_model.jl
+++ reo/src/reopt_scip_model.jl
@@ -1,9 +1,12 @@
 using JuMP
 using SCIP
 
-function reopt_model(time_limit_seconds::Real)
+function reopt_model(time_limit_seconds::Real, limits_gap::Real)
 
-    REopt = Model(with_optimizer(SCIP.Optimizer, display_verblevel=0, limits_time=time_limit_seconds))
+    REopt = Model(with_optimizer(SCIP.Optimizer, display_verblevel=0, limits_time=time_limit_seconds, limits_gap=limits_gap))
+	REopt[:solver_name] = "SCIP"
+	REopt[:timeout_sections] = time_limit_seconds
+	REopt[:optimality_tolerance] = limits_gap
 	return REopt
 
 end
