5a2b054748de70b9605340c0cafb0e365a85d5b3
zolanaj <alexander.zolan@nrel.gov>
2020-07-17 14:41:39 -0600

    added method add_decomp_models
    uses starting model as input, then generates copies of the empty model using the same solver, optimality tolerance and timeout threshold provided as input.

--- reo/src/reopt_scip_model.jl
+++ reo/src/reopt_scip_model.jl
@@ -5,7 +5,7 @@ function reopt_model(time_limit_seconds::Real, limits_gap::Real)
 
     REopt = Model(with_optimizer(SCIP.Optimizer, display_verblevel=0, limits_time=time_limit_seconds, limits_gap=limits_gap))
 	REopt[:solver_name] = "SCIP"
-	REopt[:timeout_sections] = time_limit_seconds
+	REopt[:timeout_seconds] = time_limit_seconds
 	REopt[:optimality_tolerance] = limits_gap
 	return REopt
 
