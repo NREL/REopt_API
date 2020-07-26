b1ab6152d8f5065ee0d402ba4fbe62ff2178eed3
Sakshi Mishra <sakshi.m@outlook.com>
2020-07-26 14:00:06 -0600

    set up new POST endpoint for res_stats

---
+++ resilience_stats/api.py
@@ -0,0 +1,52 @@
+# *********************************************************************************
+# REopt, Copyright (c) 2019-2020, Alliance for Sustainable Energy, LLC.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+# Redistributions of source code must retain the above copyright notice, this list
+# of conditions and the following disclaimer.
+#
+# Redistributions in binary form must reproduce the above copyright notice, this
+# list of conditions and the following disclaimer in the documentation and/or other
+# materials provided with the distribution.
+#
+# Neither the name of the copyright holder nor the names of its contributors may be
+# used to endorse or promote products derived from this software without specific
+# prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+# OF THE POSSIBILITY OF SUCH DAMAGE.
+# *********************************************************************************
+
+from tastypie.resources import ModelResource
+from resilience_stats.views import resilience_stats
+from tastypie.authorization import ReadOnlyAuthorization
+from tastypie.serializers import Serializer
+
+# POST data:{"run_uuid": UUID, "bau": True}
+
+
+class SimJob(ModelResource):
+    class Meta:
+        resource_name = 'simjob'
+        allowed_methods = ['post']
+        detail_allowed_methods = []
+        authorization = ReadOnlyAuthorization()
+        serializer = Serializer(formats=['json'])
+        always_return_data = True
+        object_class = None
+
+    def obj_create(self, bundle, **kwargs):
+        run_uuid = bundle.data["run_uuid"]
+        bau = bundle.data["bau"]
+        resilience_stats({"bau": bau}, run_uuid)
