de562ed63bd7891ab5e1b5570dcf85b04ec87230
Sakshi Mishra <sakshi.m@outlook.com>
2020-07-29 15:28:38 -0600

    Updated outage sim job endpoint handler; Validates job, launches sim job and returns JSON withHTTP 204 status immediately

--- resilience_stats/api.py
+++ resilience_stats/api.py
@@ -29,12 +29,21 @@
 # *********************************************************************************
 import json
 
+from celery import shared_task
+from django.core.exceptions import ValidationError
+from django.http import JsonResponse, HttpResponse
 from tastypie.resources import ModelResource
+from tastypie.exceptions import ImmediateHttpResponse
 from tastypie.bundle import Bundle
 from tastypie.validation import Validation
-from resilience_stats.views import resilience_stats
 from tastypie.authorization import ReadOnlyAuthorization
 from tastypie.serializers import Serializer
+
+from reo.models import ScenarioModel
+from reo.exceptions import SaveToDatabase
+from resilience_stats.views import resilience_stats
+from resilience_stats.models import ResilienceModel
+from resilience_stats.validators import validate_run_uuid
 
 # POST data:{"run_uuid": UUID, "bau": True}
 
@@ -54,12 +63,11 @@ class OutageSimJob(ModelResource):
         kwargs = {}
 
         if isinstance(bundle_or_obj, Bundle):
-            # Primary key is not currently used by our API but
-            # it seems to be a required field for tastypie
-            # so, setting it to a const.
-            kwargs['pk'] = "1"  # bundle_or_obj.obj.id
+            # Primary key is set to be the run_uuid for 1 to1 matching
+            # between scenario UUID and outage sim UUID
+            kwargs['pk'] = bundle_or_obj.data["run_uuid"]  # bundle_or_obj.obj.id
         else:
-            kwargs['pk'] = "1"  # bundle_or_obj['id']
+            kwargs['pk'] = bundle_or_obj.get("id")
 
         return kwargs
 
@@ -72,7 +80,40 @@ class OutageSimJob(ModelResource):
     def obj_create(self, bundle, **kwargs):
         run_uuid = bundle.data["run_uuid"]
         bau = bundle.data["bau"]
-        response = resilience_stats({"bau": bau}, run_uuid)
-        bundle.data = json.loads(response.getvalue())
-        # print("resp.getval:", json.loads(response.getvalue()))
+        # Handle invalid uuids
+        try:
+            validate_run_uuid(run_uuid)
+        except ValidationError as err:
+            return JsonResponse({"Error": str(err.message)}, status=400)
+
+        # Handle non-existent scenario runs
+        try:
+            scenario = ScenarioModel.objects.get(run_uuid=run_uuid)
+        except ScenarioModel.DoesNotExist:
+            msg = "Scenario {} does not exist.".format(run_uuid)
+            return JsonResponse({"Error": msg}, content_type='application/json', status=404)
+
+        if scenario.status == "Optimizing...":
+            return JsonResponse({"Error": "The scenario is still optimizing. Please try again later."},
+                                content_type='application/json', status=500)
+        elif "error" in scenario.status.lower():
+            return JsonResponse(
+                {"Error": "An error occurred in the scenario. Please check the messages from your results."},
+                content_type='application/json', status=500)
+        # Kick-off outage sim run
+        run_outage_sim.s(run_uuid, bau, scenario)
+        bundle.data = {"run_uuid": run_uuid, "bau": bau, "Success": True, "Status": 204}
+
         return bundle
+
+
+@shared_task
+def run_outage_sim(run_uuid, bau, scenario):
+
+        results = run_outage_sim(run_uuid, with_tech=True, bau=bau)
+        rm = ResilienceModel.create(scenariomodel=scenario)
+
+        try:
+            ResilienceModel.objects.filter(id=rm.id).update(**results)
+        except SaveToDatabase as e:
+            raise ImmediateHttpResponse(HttpResponse(json.dumps({"Error": e.message}), content_type='application/json', status=201))
