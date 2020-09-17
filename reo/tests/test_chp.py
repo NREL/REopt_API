4bb9678ae4b940f624139ada73175b9292df3db0
Bill Becker <William.Becker@nrel.gov>
2020-09-17 08:32:23 -0600

    Add test_chp_sizing_monolith_1pct and specify previous test_chp
    Specify previous test_chp_sizing based on using the decomposition with 7% optimization_tolerance_techs.
    
    Create a new test_chp_sizing_monolith_1pct which uses the monolith optimization model witha 1% optimality_tolerance_techs.

--- reo/tests/test_chp.py
+++ reo/tests/test_chp.py
@@ -23,7 +23,7 @@ class CHPTest(ResourceTestCaseMixin, TestCase):
         return self.api_client.post(self.reopt_base, format='json', data=data)
 
     #@skip("CHP test")
-    def test_chp_sizing(self):
+    def test_chp_sizing_decomposition_7pct(self):
         """
         Validation to ensure that:
          1) CHP Sizing is consistent
@@ -35,6 +35,11 @@ class CHPTest(ResourceTestCaseMixin, TestCase):
 
         # Call API, get results in "d" dictionary
         nested_data = json.load(open(self.test_post, 'rb'))
+        nested_data["Scenario"]["timeout_seconds"] = 420
+        nested_data["Scenario"]["optimality_tolerance_bau"] = 0.001
+        nested_data["Scenario"]["optimality_tolerance_techs"] = 0.07
+        nested_data["Scenario"]["use_decomposition_model"] = True
+        nested_data["Scenario"]["optimality_tolerance_decomp_subproblem"] = 0.03
         resp = self.get_response(data=nested_data)
         self.assertHttpCreated(resp)
         r = json.loads(resp.content)
@@ -53,6 +58,51 @@ class CHPTest(ResourceTestCaseMixin, TestCase):
         d_expected['boiler_total_fuel_cost_us_dollars'] = 4555.27
         d_expected['chp_total_fuel_cost_us_dollars'] = 3721693.94
         d_expected['total_opex_costs'] = 811596.0
+
+        try:
+            check_common_outputs(self, c, d_expected)
+        except:
+            print("Run {} expected outputs may have changed. Check the Outputs folder.".format(run_uuid))
+            print("Error message: {}".format(d['messages'].get('error')))
+            raise
+
+            # @skip("CHP test")
+
+    def test_chp_sizing_monolith_1pct(self):
+        """
+        Validation to ensure that:
+         1) CHP Sizing is consistent
+
+         This test was verified to be withing 1.5% gap after 10 mins of the Mosel/Xpress monolith using windows Xpress IVE
+
+        :return:
+        """
+
+        # Call API, get results in "d" dictionary
+        nested_data = json.load(open(self.test_post, 'rb'))
+        nested_data["Scenario"]["timeout_seconds"] = 420
+        nested_data["Scenario"]["optimality_tolerance_bau"] = 0.001
+        nested_data["Scenario"]["optimality_tolerance_techs"] = 0.01
+        nested_data["Scenario"]["use_decomposition_model"] = False
+        nested_data["Scenario"]["optimality_tolerance_decomp_subproblem"] = 0.03
+        resp = self.get_response(data=nested_data)
+        self.assertHttpCreated(resp)
+        r = json.loads(resp.content)
+        run_uuid = r.get('run_uuid')
+        d = ModelManager.make_response(run_uuid=run_uuid)
+        c = nested_to_flat_chp(d['outputs'])
+
+        # The values compared to the expected values may change if optimization parameters were changed
+        d_expected = dict()
+        d_expected['lcc'] = 13476072.0
+        d_expected['npv'] = 1231748.579
+        d_expected['chp_kw'] = 468.718
+        d_expected['chp_year_one_fuel_used_mmbtu'] = 30555.6
+        d_expected['chp_year_one_electric_energy_produced_kwh'] = 3086580.645
+        d_expected['chp_year_one_thermal_energy_produced_mmbtu'] = 9739.972
+        d_expected['boiler_total_fuel_cost_us_dollars'] = 7211.964
+        d_expected['chp_total_fuel_cost_us_dollars'] = 3271904.872
+        d_expected['total_opex_costs'] = 686311.0
 
         try:
             check_common_outputs(self, c, d_expected)
