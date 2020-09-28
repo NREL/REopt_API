346597bd0aa2d019cb7286bcab939009e48c19d1
zolanaj <alexander.zolan@nrel.gov>
2020-09-28 14:53:48 -0600

    Add solver settings to Xpress models
    Note: BARGAPSTOP is not included as an option in the Xpress.jl interface and so hasn't been populated

--- reo/src/reopt_xpress_model.jl
+++ reo/src/reopt_xpress_model.jl
@@ -7,6 +7,11 @@ function reopt_model(MAXTIME, MIPRELSTOP)
 	REopt[:solver_name] = "Xpress"
 	REopt[:timeout_seconds] = MAXTIME
 	REopt[:optimality_tolerance] = MIPRELSTOP
+	
+	set_optimizer_attribute(REopt, "FEASTOL", 1.0e-6)
+	set_optimizer_attribute(REopt, "BACKTRACK", 5)
+	set_optimizer_attribute(REopt, "LPTHREADS", 3)
+	
 	return REopt
 
 end
