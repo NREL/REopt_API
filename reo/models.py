d967f7894e5c7e704024e7a2254d07176504cadf
ted <ted.kwasnik@nrel.gov>
2020-09-29 17:02:29 -0600

    2-party > third_party, net present cost

--- reo/models.py
+++ reo/models.py
@@ -153,7 +153,7 @@ class FinancialModel(models.Model):
     offtaker_tax_pct = models.FloatField()
     value_of_lost_load_us_dollars_per_kwh = models.FloatField(null=True, blank=True)
     microgrid_upgrade_cost_pct = models.FloatField(null=True, blank=True)
-    two_party_ownership = models.BooleanField(default=False)
+    third_party_ownership = models.BooleanField(default=False)
     owner_discount_pct = models.FloatField(null=True, blank=True)
     owner_tax_pct = models.FloatField(null=True, blank=True)
 
@@ -171,6 +171,7 @@ class FinancialModel(models.Model):
     initial_capital_costs_after_incentives = models.FloatField(null=True, blank=True)
     simple_payback_years = models.FloatField(null=True, blank=True)
     irr_pct = models.FloatField(null=True, blank=True)
+    net_present_cost_us_dollars = models.FloatField(null=True, blank=True)
 
     @classmethod
     def create(cls, **kwargs):
