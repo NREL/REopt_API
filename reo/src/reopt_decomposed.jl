4f0d39ae9efa18400f30e25722ae64c65cd2eda9
zolanaj <alexander.zolan@nrel.gov>
2020-11-18 21:32:02 -0700

    add Xpress solver setting CUTSTRATEGY=3

--- reo/src/reopt_decomposed.jl
+++ reo/src/reopt_decomposed.jl
@@ -883,7 +883,7 @@ end
 
 function add_decomp_model(m, p::Parameter, model_type::String, mth::Int64)
 	if m[:solver_name] == "Xpress"
-		sub_model = direct_model(Xpress.Optimizer(MAXTIME=-p.DecompTimeOut, MIPRELSTOP=p.DecompOptTol, OUTPUTLOG = 0))
+		sub_model = direct_model(Xpress.Optimizer(MAXTIME=-p.DecompTimeOut, MIPRELSTOP=p.DecompOptTol, CUTSTRATEGY = 3, OUTPUTLOG = 0))
 	elseif m[:solver_name] == "Cbc"
 		sub_model = Model(with_optimizer(Cbc.Optimizer, logLevel=0, seconds=p.DecompTimeOut, ratioGap=p.DecompOptTol))
 	elseif m[:solver_name] == "SCIP"
--- reo/src/reopt_xpress_model.jl
+++ reo/src/reopt_xpress_model.jl
@@ -3,7 +3,7 @@ using Xpress
 
 function reopt_model(MAXTIME, MIPRELSTOP)
    
-    REopt = direct_model(Xpress.Optimizer(MAXTIME=-MAXTIME, MIPRELSTOP=MIPRELSTOP, OUTPUTLOG = 0))
+    REopt = direct_model(Xpress.Optimizer(MAXTIME=-MAXTIME, MIPRELSTOP=MIPRELSTOP, CUTSTRATEGY = 3, OUTPUTLOG = 0))
 	REopt[:solver_name] = "Xpress"
 	REopt[:timeout_seconds] = MAXTIME
 	REopt[:optimality_tolerance] = MIPRELSTOP
