8a66b0500a477d6bdb5b54272ef9e571614e0ef0
t-kwasnik <tedkwasnik@gmail.com>
2020-12-15 10:15:03 -0700

    fix failing test

--- reo/tests/test_reopt_url.py
+++ reo/tests/test_reopt_url.py
@@ -144,7 +144,7 @@ class EntryResourceTest(ResourceTestCaseMixin, TestCase):
         response = self.get_response(data)
         err_msg = str(json.loads(response.content)['messages']['input_errors'])
 
-        self.assertTrue("outage_end_hour must be larger than outage_end_hour cannot be the same" in err_msg)
+        self.assertTrue("LoadProfile outage_end_time_step must be larger than outage_start_time_step and these inputs cannot be equal" in err_msg)
 
     def test_valid_data_ranges(self):
 
