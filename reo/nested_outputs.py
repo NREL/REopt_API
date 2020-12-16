cd421853ca8fb4d4cdbb200701e58a4338c8f501
TKwasnik <tedkwasnik@gmail.com>
2020-12-15 18:45:36 -0700

    Update nested_outputs.py
    fix typos from replace all

--- reo/nested_outputs.py
+++ reo/nested_outputs.py
@@ -2,13 +2,13 @@
 # REopt, Copyright (c) 2019-2020, Alliance for Sustainable Energy, LLC.
 # All rights reserved.
 #
-# Redi"str"ibution and use in source and binary forms, with or without modification,
+# Redistribution and use in source and binary forms, with or without modification,
 # are permitted provided that the following conditions are met:
 #
-# Redi"str"ibutions of source code must retain the above copyright notice, this list
+# Redistributions of source code must retain the above copyright notice, this list
 # of conditions and the following disclaimer.
 #
-# Redi"str"ibutions in binary form must reproduce the above copyright notice, this
+# Redistributions in binary form must reproduce the above copyright notice, this
 # list of conditions and the following disclaimer in the documentation and/or other
 # materials provided with the di"str"ibution.
 #
@@ -22,8 +22,8 @@
 # IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
 # INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
 # BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS "int"ERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-# LIABILITY, WHETHER IN CONTRACT, "str"ICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
 # OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
 # OF THE POSSIBILITY OF SUCH DAMAGE.
 # *********************************************************************************
