0a0d1702a5356b1df692a58035cb785cd915f827
Nick Laws <nick.laws@nrel.gov>
2020-12-23 18:40:49 -0700

    fix TotalPerUnitProdOMCosts in reopt.jl for sub-hourly problems

--- reo/src/reopt.jl
+++ reo/src/reopt.jl
@@ -77,7 +77,7 @@ function add_cost_expressions(m, p)
 		sum( p.OMperUnitSize[t] * m[:dvSize][t] for t in p.Tech ) 
 	)
     if !isempty(m[:GeneratorTechs])
-		m[:TotalPerUnitProdOMCosts] = @expression(m, p.two_party_factor * p.pwf_om * 
+		m[:TotalPerUnitProdOMCosts] = @expression(m, p.two_party_factor * p.pwf_om * p.TimeStepScaling * 
 			sum( p.OMcostPerUnitProd[t] * m[:dvRatedProduction][t,ts] for t in p.FuelBurningTechs, ts in p.TimeStep ) 
 		)
     else
