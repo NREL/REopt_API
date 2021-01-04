8d235d552c7844339d7a2b651cfb6090a938af2b
hdunham <70401017+hdunham@users.noreply.github.com>
2021-01-04 15:20:18 -0800

    undo change to Parameter() (was testing write access)

--- reo/src/utils.jl
+++ reo/src/utils.jl
@@ -233,7 +233,6 @@ function Parameter(d::Dict)
     d[:TimeStepBat] = 0:d["TimeStepCount"]
     n_location = length(d["MaxSizesLocation"])
     d[:Location] = 1:n_location
-    d[:CPPeriod] = 1:d["CoincidentPeakPeriodCount"]
 
     # the following array manipulation may have to adapt once length(d["Subdivision"]) > 1
     seg_min_size_array = reshape(transpose(reshape(d["SegmentMinSize"], length(d[:Seg]), length(d["Tech"]))), 
