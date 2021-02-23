f685296020ba9a3814e9973b702f44bdf44fe293
Nick Laws <nick.laws@nrel.gov>
2021-02-23 08:57:16 -0700

    julia api running jump model

--- julia_src/api.jl
+++ julia_src/api.jl
@@ -1,12 +1,23 @@
 using Genie, Genie.Router, Genie.Requests, Genie.Renderer.Json
+include("REopt.jl")
+using .REopt
 
 Genie.config.run_as_server = true
 
 route("/jsonpayload", method = POST) do
-  @show jsonpayload()
-  @show rawpayload()
+  data = jsonpayload() # Dict{String,Any}
+  m = xpress_model(100.0, 0.001)  # TODO add timeout and tolerance to data dict
+  results = reopt(m, data)
+"""
+1. json to dict?
+2. get results
+3. return
 
-  json("Hello $(jsonpayload()["name"])")
+...
+
+multi thread bau scenario
+"""
+  json(results)
 end
 
-Genie.startup()
+Genie.startup(8081)
