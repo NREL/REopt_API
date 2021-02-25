2648214e9727816d44fe7e8c61ef1233b24a3cb4
Nick Laws <nick.laws@nrel.gov>
2021-02-25 10:45:04 -0700

    Update http.jl

--- julia_src/http.jl
+++ julia_src/http.jl
@@ -1,11 +1,13 @@
 using HTTP, JSON
 include("REopt.jl")
+
 using .REopt
 
 function job(req::HTTP.Request)
     d = JSON.parse(String(req.body))
     m = xpress_model(100.0, 0.001)  # TODO add timeout and tolerance to data dict
     results = reopt(m, d)
+    # TODO @info run_uuid and status
     return HTTP.Response(200, JSON.json(results))
 end
 
@@ -14,3 +16,4 @@ const ROUTER = HTTP.Router()
 
 HTTP.@register(ROUTER, "POST", "/job", job)
 HTTP.serve(ROUTER, "0.0.0.0", 8081)
+# TODO sometimes the server is not ready even though all containers are running?
