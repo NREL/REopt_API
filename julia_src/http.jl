926593e2831c307adcff560b640e725d09eea9c6
Nick Laws <nick.laws@nrel.gov>
2021-02-25 13:12:38 -0700

    Update http.jl

--- julia_src/http.jl
+++ julia_src/http.jl
@@ -11,9 +11,14 @@ function job(req::HTTP.Request)
     return HTTP.Response(200, JSON.json(results))
 end
 
+function health(req::HTTP.Request)
+    return HTTP.Response(200, JSON.json(Dict("Julia-api"=>"healthy!")))
+end
+
 # define REST endpoints to dispatch to "service" functions
 const ROUTER = HTTP.Router()
 
 HTTP.@register(ROUTER, "POST", "/job", job)
+HTTP.@register(ROUTER, "GET", "/health", health)
 HTTP.serve(ROUTER, "0.0.0.0", 8081)
 # TODO sometimes the server is not ready even though all containers are running?
