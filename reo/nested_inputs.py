0893d5a03269209bb0b196f439b8dccf9c7d8400
bill-becker <William.Becker@nrel.gov>
2021-03-01 15:15:16 -0700

    Add export/curtail params to SteamTurbine

--- reo/nested_inputs.py
+++ reo/nested_inputs.py
@@ -1964,6 +1964,26 @@ nested_input_definitions = {
         "om_cost_us_dollars_per_kwh": {
           "type": "float", "min": 0.0, "max": 1.0e9,
           "description": "Electric energy-based variable O&M cost"
+        },
+        "can_net_meter": {
+          "type": "bool",
+          "default": False,
+          "description": "True/False for if technology has option to participate in net metering agreement with utility. Note that a technology can only participate in either net metering or wholesale rates (not both)."
+        },
+        "can_wholesale": {
+          "type": "bool",
+          "default": False,
+          "description": "True/False for if technology has option to export energy that is compensated at the wholesale_rate_us_dollars_per_kwh. Note that a technology can only participate in either net metering or wholesale rates (not both)."
+        },
+        "can_export_beyond_site_load": {
+          "type": "bool",
+          "default": False,
+          "description": "True/False for if technology can export energy beyond the annual site load (and be compensated for that energy at the wholesale_rate_above_site_load_us_dollars_per_kwh)."
+        },
+        "can_curtail": {
+          "type": "bool",
+          "default": False,
+          "description": "True/False for if technology can curtail energy produced."
         }
       }      
     }
