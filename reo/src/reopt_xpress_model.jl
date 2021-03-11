e5565ba78948e0b4719ebc80e9adf3778a7904e0
lixiangk1 <lixiangk1@users.noreply.github.com>
2021-03-11 08:02:40 -0700

    Enable debugging

--- reo/src/reopt_xpress_model.jl
+++ reo/src/reopt_xpress_model.jl
@@ -3,7 +3,7 @@ using Xpress
 
 function reopt_model(MAXTIME, MIPRELSTOP)
    
-    REopt = direct_model(Xpress.Optimizer(MAXTIME=-MAXTIME, MIPRELSTOP=MIPRELSTOP, OUTPUTLOG = 0))
+    REopt = direct_model(Xpress.Optimizer(MAXTIME=-MAXTIME, MIPRELSTOP=MIPRELSTOP,  logfile="output.log"))#OUTPUTLOG = 0))
 	REopt[:solver_name] = "Xpress"
 	REopt[:timeout_seconds] = MAXTIME
 	REopt[:optimality_tolerance] = MIPRELSTOP
