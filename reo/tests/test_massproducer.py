7bce54f5c71d7571a225087f3971d481a11973e7
bill-becker <William.Becker@nrel.gov>
2021-03-23 05:40:04 -0600

    Add WIP test_massproducer

---
+++ reo/tests/test_massproducer.py
@@ -0,0 +1,106 @@
+# *********************************************************************************
+# REopt, Copyright (c) 2019-2020, Alliance for Sustainable Energy, LLC.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+# Redistributions of source code must retain the above copyright notice, this list
+# of conditions and the following disclaimer.
+#
+# Redistributions in binary form must reproduce the above copyright notice, this
+# list of conditions and the following disclaimer in the documentation and/or other
+# materials provided with the distribution.
+#
+# Neither the name of the copyright holder nor the names of its contributors may be
+# used to endorse or promote products derived from this software without specific
+# prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+# OF THE POSSIBILITY OF SUCH DAMAGE.
+# *********************************************************************************
+import json
+import os
+from tastypie.test import ResourceTestCaseMixin
+from reo.nested_to_flat_output import nested_to_flat_chp
+from unittest import TestCase, skip  # have to use unittest.TestCase to get tests to store to database, django.test.TestCase flushes db
+from reo.models import ModelManager
+from reo.utilities import check_common_outputs, MMBTU_TO_KWH
+
+class MassProducerTest(ResourceTestCaseMixin, TestCase):
+    REopt_tol = 1e-2
+
+    def setUp(self):
+        super(MassProducerTest, self).setUp()
+        self.reopt_base = '/v1/job/'
+        self.test_post = os.path.join('reo', 'tests', 'posts', 'test_massproducer_POST.json')
+
+    def get_response(self, data):
+
+        return self.api_client.post(self.reopt_base, format='json', data=data)
+
+    @skip("Skip MassProducer test")
+    def test_massproducer_nuclear(self):
+        """
+        This first test is specific to a nuclear application with no conventional heating load.
+        The NewBoiler plus condensing-style SteamTurbine (representing Nuclear) can produce electricity for meeting the load or 
+        "hydrogen" in the MassProducer by feeding it electricity (from SteamTurbine) and hot thermal (from NewBoiler).
+        Fix the size of the SteamTurbine so that the site imports electricity to supplement the SteamTurbine to produce hydrogen
+        
+        Validation to ensure that:
+         1) MassProducer takes electricity from SteamTurbine and/or Grid and hot thermal from NewBoiler to make mass during expected times
+         2) 
+
+        :return:
+        """
+
+        # Call API, get results in "d" dictionary
+        nested_data = json.load(open(self.test_post, 'rb'))
+
+        # Add time of use rates to toggle on/off H2 production
+        elec_price_high = 0.14  # [$/kWh]
+        elec_price_low = 0.08  # [$/kWh]
+        time_low_start = 8  # timestep index (hour)
+        time_low_end = 16  # timestep index (hour)
+        mass_value = 10.1  # [$/kg]
+        nested_data["Scenario"]["Site"]["ElectricTariff"][
+            "tou_energy_rates_us_dollars_per_kwh"] = [elec_price_high] * 8760  # Higher price than value of H2 below ($0.12/kWh)
+        nested_data["Scenario"]["Site"]["ElectricTariff"][
+            "tou_energy_rates_us_dollars_per_kwh"][time_low_start:time_low_end] = \
+                                                    [elec_price_low] * (time_low_end - time_low_start)  # Lower price than H2 value below ($0.12/kWh)
+        nested_data["Scenario"]["Site"]["MassProducer"][
+            "mass_value_us_dollars_per_mass"] = mass_value  # Overwrites value in POST - eqivalent to value of $0.12/kWh electricity
+
+        resp = self.get_response(data=nested_data)
+        self.assertHttpCreated(resp)
+        r = json.loads(resp.content)
+        run_uuid = r.get('run_uuid')
+        d = ModelManager.make_response(run_uuid=run_uuid)
+     
+        # Test expected dispatch from SteamTurbine, NewBoiler, and Grid to the MassProducer and check the consumption of MassProducer
+        steamturbine_size_kw = d["outputs"]["Scenario"]["Site"]["SteamTurbine"]["size_kw"]
+        steamturbine_electric_to_mp = d["outputs"]["Scenario"]["Site"]["SteamTurbine"]["year_one_electric_to_massproducer_series_kw"] 
+        newboiler_thermal_to_mp_mmbtu_per_hr = d["outputs"]["Scenario"]["Site"]["NewBoiler"]["year_one_thermal_to_massproducer_series_mmbtu_per_hr"]
+        grid_electric_to_mp = d["outputs"]["Scenario"]["Site"]["ElectricTariff"]["year_one_to_massproducer_series_kw"]
+        mp_electric_consumed_kwh = d["outputs"]["Scenario"]["Site"]["MassProducer"]["year_one_electric_consumption_kwh"]
+        mp_thermal_consumed_mmbtu = d["outputs"]["Scenario"]["Site"]["MassProducer"]["year_one_thermal_consumption_mmbtu"]
+
+        # Check that all SteamTurbine electric production goes to MassProducer during the low electricity price time
+        expected_steamturbine_electric_to_mp = steamturbine_size_kw * (time_low_end - time_low_start)
+        self.assertEqual(expected_steamturbine_electric_to_mp, sum(steamturbine_electric_to_mp))
+        
+        # Check that total electric production to MassProducer is equal to total electric consumed by MassProducer
+        total_electric_to_mp = sum(steamturbine_electric_to_mp) + sum(grid_electric_to_mp)
+        self.assertEqual(mp_electric_consumed_kwh, total_electric_to_mp)
+
+        # Check that total thermal production from NewBoiler to MassProducer is equal to total thermal consumped by MassProducer
+        self.assertAlmostEqual(sum(newboiler_thermal_to_mp_mmbtu_per_hr), mp_thermal_consumed_mmbtu, places=3)
+
