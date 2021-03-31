05a7af3243aa27aabf97bf23bfc5c19e492a6e42
Nick Laws <nick.laws@nrel.gov>
2021-03-30 18:24:50 -0600

    update futurecosts/api.py

--- futurecosts/api.py
+++ futurecosts/api.py
@@ -45,17 +45,8 @@ from reo.exceptions import SaveToDatabase, UnexpectedError
 from reo.models import ScenarioModel
 from resilience_stats.validators import validate_run_uuid
 from futurecosts.models import FutureCostsJob
-from futurecosts.validators import InputValidator
 from futurecosts.tasks import setup_jobs
 log = logging.getLogger(__name__)
-
-
-def return400(data: dict, validator: InputValidator):
-    data["status"] = (
-        'Invalid inputs. No job has been created. See messages for details.'
-    )
-    data["messages"] = validator.validation_errors
-    raise ImmediateHttpResponse(HttpResponse(json.dumps(data), content_type='application/json', status=400))
 
 
 class FutureCostsAPI(ModelResource):
@@ -123,70 +114,32 @@ class FutureCostsAPI(ModelResource):
                 content_type='application/json',
                 status=208))
         else:  # This is the first POST for this run_uuid
+            try:
+                job = FutureCostsJob.create(**{"base_scenario": scenario, "run_uuid": run_uuid})
+                job.clean()
+                job.save()
 
-            job = FutureCostsJob.create(**{"base_scenario": scenario, "run_uuid": run_uuid})
-            job.clean()
-            job.save()
-
-            setup_jobs(run_uuid)
-            resp = {'status': 'Future Costs Job created for scenario {}'.format(run_uuid)}
-            raise ImmediateHttpResponse(HttpResponse(
-                json.dumps(resp),
-                content_type='application/json',
-                status=201
-            ))
-
-
-
-
-            # # Validate inputs
-            # # TODO don't need all this since only input is a run_uuid?
-            # try:
-            #     input_validator = InputValidator(bundle.data, run_uuid=run_uuid)
-            #     input_validator.clean_fields()  # step 1 check field values
-            #     if not input_validator.is_valid:
-            #         return400(data, input_validator)
-            #     input_validator.clean()  # step 2 check requirements that include more than one field (in one model)
-            #     if not input_validator.is_valid:
-            #         return400(data, input_validator)
-            #     input_validator.cross_clean()  # step 3 check requirements that include more than one model
-            #     if not input_validator.is_valid:
-            #         return400(data, input_validator)
-            # except ImmediateHttpResponse as e:
-            #     raise e
-            # except Exception:
-            #     exc_type, exc_value, exc_traceback = sys.exc_info()
-            #     err = UnexpectedError(exc_type, exc_value.args[0], traceback.format_tb(exc_traceback),
-            #                           task='InputValidator', run_uuid=run_uuid)
-            #     err.save_to_db()
-            #     data["status"] = (
-            #         'Internal Server Error during input validation. No optimization task has been created. '
-            #         'Please check your POST for bad values.')
-            #     # data['inputs'] = bundle.data
-            #     data['messages'] = {}
-            #     data['messages']['error'] = err.message  # "Unexpected Error."
-            #     log.error("Internal Server error: " + err.message)
-            #     raise ImmediateHttpResponse(HttpResponse(json.dumps(data),
-            #                                              content_type='application/json',
-            #                                              status=500))  # internal server error
-
-            """
-            Have to serialize the Django Models to pass to celery task!
-            """
-
-            # data["status"] = 'Optimizing...'
-            # try:
-            #     input_validator.save()
-            # except Exception:
-            #     log.error("Could not create and save run_uuid: {}\n Data: {}".format(run_uuid, data))
-            #     exc_type, exc_value, exc_traceback = sys.exc_info()
-            #     err = UnexpectedError(exc_type, exc_value.args[0], traceback.format_tb(exc_traceback),
-            #                           task='create_and_save',
-            #                           run_uuid=run_uuid)
-            #     # err.save_to_db()
-            #     data["status"] = "Internal Server Error during saving of inputs. Please see messages."
-            #     data['messages'] = err.message  # "Unexpected Error."
-            #     log.error("Internal Server error: " + err.message)
-            #     raise ImmediateHttpResponse(HttpResponse(json.dumps(data),
-            #                                              content_type='application/json',
-            #                                              status=500))  # internal server error
+                setup_jobs(run_uuid)
+                resp = {'status': 'Future Costs Job created for scenario {}'.format(run_uuid)}
+            except Exception:
+                log.error("Could not create and save futurecosts job for run_uuid: {}".format(run_uuid))
+                exc_type, exc_value, exc_traceback = sys.exc_info()
+                err = UnexpectedError(exc_type, exc_value.args[0], traceback.format_tb(exc_traceback),
+                                      task='futurecosts.api',
+                                      run_uuid=run_uuid)
+                err.save_to_db()
+                resp = {'status': ("Unexpected Error. "
+                                   "Could not create and save futurecosts job for run_uuid: {} "
+                                   "Please try again later.".format(run_uuid))
+                        }
+                raise ImmediateHttpResponse(HttpResponse(
+                    json.dumps(resp),
+                    content_type='application/json',
+                    status=500
+                ))  # internal server error
+            else:
+                raise ImmediateHttpResponse(HttpResponse(
+                    json.dumps(resp),
+                    content_type='application/json',
+                    status=201
+                ))
