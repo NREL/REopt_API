e901ba2e87970d6cf00394c357cea503b68383e7
Nick Laws <nick.laws@nrel.gov>
2021-06-23 15:52:42 -0600

    http.jl serve reopt endpoint for using Julia pkg

--- julia_src/http.jl
+++ julia_src/http.jl
@@ -1,19 +1,32 @@
 using HTTP, JSON
+using JuMP, Xpress
 include("REopt.jl")
-
-using .REopt
+import REoptLite
 
 function job(req::HTTP.Request)
     d = JSON.parse(String(req.body))
     timeout = pop!(d, "timeout_seconds")
     tol = pop!(d, "tolerance")
-    m = xpress_model(timeout, tol)
+    m = REopt.xpress_model(timeout, tol)
     @info "Starting REopt with timeout of $(timeout) seconds..."
-    results = reopt(m, d)
+    results = REopt.reopt(m, d)
 	# fix our memory leak? https://github.com/jump-dev/CPLEX.jl/issues/185
 	m = nothing
 	GC.gc()
     @info "REopt model solved with status $(results["status"])."
+    return HTTP.Response(200, JSON.json(results))
+end
+
+function reopt(req::HTTP.Request)
+    d = JSON.parse(String(req.body))
+	m = direct_model(
+			Xpress.Optimizer(
+				MAXTIME = -d["Optimization"]["timeout_seconds"],
+				MIPRELSTOP = d["Optimization"]["optimality_tolerance"],
+				OUTPUTLOG = 0
+			)
+		)
+	results = REoptLite.run_reopt(m, d)
     return HTTP.Response(200, JSON.json(results))
 end
 
@@ -25,5 +38,6 @@ end
 const ROUTER = HTTP.Router()
 
 HTTP.@register(ROUTER, "POST", "/job", job)
+HTTP.@register(ROUTER, "POST", "/reopt", reopt)
 HTTP.@register(ROUTER, "GET", "/health", health)
 HTTP.serve(ROUTER, "0.0.0.0", 8081, reuseaddr=true)
