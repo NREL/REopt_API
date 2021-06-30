58c470d9e157309e34899f16582f1e8d44ff975a
bill-becker <William.Becker@nrel.gov>
2021-06-30 11:21:52 -0600

    Modifications to load_profile_xyz for re-org files and space vs DHW split

--- reo/src/load_profile_boiler_fuel.py
+++ reo/src/load_profile_boiler_fuel.py
@@ -1,5 +1,5 @@
 from reo.src.load_profile import BuiltInProfile
-import json
+import pandas as pd
 import os
 import copy
 import numpy as np
@@ -9,53 +9,62 @@ class LoadProfileBoilerFuel(BuiltInProfile):
     """
     Boiler Load Profiles based on CRB defined load shapes or user-defined input
     """
-    # TODO for separating space heating and DHW - make these two reference files instance attributes defined by kwargs
+    # TODO for separating space heating and DHW - see reference file that Chris made
     # and then we can create two separate lpbf instances (lpbf_space and lpbf_dhw) in scenario.py and data_manager.py
-    # also must separate out applicable techs to supply each, e.g. ghp can only suppy space heating
-    with open(os.path.join(BuiltInProfile.library_path, 'reference_heating_mmbtu.json'), 'r') as f:
-        annual_loads = json.loads(f.read())
-
-    builtin_profile_prefix = "Heating8760_norm_"
+    # since ghpghx is run in Scenario.py we just need to send only space heating to it, and
+    # we can keep HeatingLoad as the total site heating load (space + DHW)
+    # maybe add space heating to sim_loads endpoint to supply 
     
-    def __init__(self, dfm=None, latitude = None, longitude = None, nearest_city = None, time_steps_per_hour = None, 
+    annual_heating_stats = pd.read_csv(os.path.join(BuiltInProfile.library_path_base, 'annual_heating_stats.csv'))
+
+    def __init__(self, load_type, dfm=None, latitude = None, longitude = None, nearest_city = None, time_steps_per_hour = None, 
                     year = None, **kwargs):
         """
+        :param load_type: required input of DHW or SpaceHeating
         :param dfm: (object) data_manager to which this load object will be added
         :param latitude: (float) site latitude
         :param longitude: (float) site longitude
         :param nearest_city: (str) site nearest_city
         :param time_steps_per_hour: (int) simulation time resolution
         :param year: (int) electric LoadProfile year
-        :param kwargs: (dict) Chiller specific inputs as defined in reo/nested_inputs
+        :param kwargs: (dict) LoadProfileBoilerFuel specific inputs as defined in reo/nested_inputs
         """
-        
+
+        self.load_type = load_type
         self.nearest_city = nearest_city
         self.latitude = latitude
         self.longitude = longitude
         self.time_steps_per_hour = time_steps_per_hour
         self.year = year
+
+        self.annual_loads = annual_heating_stats["load_type"]
+
+        self.addressable_fraction = kwargs.get("addressable_fraction", [1.0])
         
-        if kwargs.get('loads_mmbtu_per_hour') is not None: 
+        if kwargs.get('loads_mmbtu_per_hour') is not None:
             self.load_list = kwargs['loads_mmbtu_per_hour']
             self.annual_mmbtu = sum(self.load_list)
 
         else:  # building type and (annual_mmbtu OR monthly_mmbtu) defined by user
             doe_reference_name = kwargs['doe_reference_name']
             combine_loadlist = []
+            
             for i in range(len(doe_reference_name)):
                 # Monthly loads can only be used to scale a non-hybrid profile
-                kwargs['monthly_totals_energy'] = kwargs.get("monthly_mmbtu")
-                if len(doe_reference_name)>1:
+                self.monthly_totals_energy = kwargs.get("monthly_mmbtu")
+                if self.monthly_totals_energy not in [[], None]:
+                     kwargs['monthly_totals_energy'] = [self.monthly_totals_energy[i] * self.addressable_fraction[i] for i in range(12)]
+                if len(doe_reference_name) > 1:
                     kwargs['monthly_totals_energy'] = None
                 kwargs['annual_energy'] = None
                 # We only scale by percent share if a raw annual mmbtu value has not been provided
                 if kwargs.get("annual_mmbtu") is not None:
-                    kwargs['annual_energy'] = kwargs["annual_mmbtu"]
+                    kwargs['annual_energy'] = kwargs["annual_mmbtu"] * self.addressable_fraction[0]
                     percent_share = 100
                 else:
                     percent_share = kwargs.get("percent_share")[i]
                 kwargs['annual_loads'] = self.annual_loads
-                kwargs['builtin_profile_prefix'] = self.builtin_profile_prefix
+                kwargs['load_type'] = self.load_type
                 kwargs['latitude'] = latitude
                 kwargs['longitude'] = longitude
                 kwargs['doe_reference_name'] = doe_reference_name[i]
