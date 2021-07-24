d37453b546c27e23a8577d36bad35cc33d9e897d
bill-becker <William.Becker@nrel.gov>
2021-07-23 22:04:20 -0600

    Add test for hybrid heating and cooling loads

---
+++ reo/tests/test_hybridloads_heat_cool.py
@@ -0,0 +1,155 @@
+import json
+import copy
+import os
+import pandas as pd
+from tastypie.test import ResourceTestCaseMixin
+from reo.nested_to_flat_output import nested_to_flat
+from unittest import TestCase  # have to use unittest.TestCase to get tests to store to database, django.test.TestCase flushes db
+from unittest import skip
+from reo.models import ModelManager
+from reo.utilities import check_common_outputs
+from reo.validators import ValidateNestedInput
+import pandas as pd
+import numpy as np
+from unittest import skip
+from reo.utilities import TONHOUR_TO_KWHT
+
+post = {
+        "Scenario": {
+            "Site": {
+            "latitude": 37.78,
+            "longitude": -122.45,
+            "LoadProfile": {
+                # "annual_kwh": 100,
+                # "doe_reference_name": "Hospital",
+                #"doe_reference_name": ["Hospital", "LargeHotel"],
+                #"percent_share": [50, 50]
+            },
+            "LoadProfileBoilerFuel": {
+                # "doe_reference_name": ["Hospital", "LargeHotel"],
+                # "percent_share": [50, 50]
+            },
+            "LoadProfileChillerThermal": {
+                # "doe_reference_name": ["Hospital", "LargeHotel"],
+                # "percent_share": [50, 50],
+                # "chiller_cop": 3.4
+            },
+            "ElectricTariff": {
+                "urdb_label": "5cef0a415457a33576f60fe2"
+            },
+            # "FuelTariff": {
+            #     "boiler_fuel_blended_annual_rates_us_dollars_per_mmbtu": 11.0,
+            #     "chp_fuel_blended_annual_rates_us_dollars_per_mmbtu": 11.0
+            # },
+            # "Boiler": {
+            #     "existing_boiler_production_type_steam_or_hw": "hot_water"
+            # },
+            "PV": {
+                "min_kw": 0.0,
+                "max_kw": 0.0
+            },
+            "Storage": {
+                "min_kw": 0.0,
+                "max_kw": 0.0,
+                "min_kwh": 0.0,
+                "max_kwh": 0.0
+            }
+        }}}
+
+class HybridLoadsHeatCoolTest(ResourceTestCaseMixin, TestCase):
+    REopt_tol = 1e-2
+
+    def setUp(self):
+        super(HybridLoadsHeatCoolTest, self).setUp()
+        self.reopt_base = '/v1/job/'
+
+    def get_response(self, data):
+        return self.api_client.post(self.reopt_base, format='json', data=data)
+
+    def test_heat_cool_energy_balance(self):
+        """
+
+        Check that hybrid loads for electric is working as expected, and heating and cooling work too
+
+        """
+
+        hospital_pct = 75.0
+        hotel_pct = 100 - hospital_pct
+
+        # Hospital only
+        post["Scenario"]["Site"]["LoadProfile"]["annual_kwh"] = hospital_pct
+        post["Scenario"]["Site"]["LoadProfile"]["doe_reference_name"] = "Hospital"
+        post["Scenario"]["Site"]["LoadProfileBoilerFuel"]["annual_mmbtu"] = hospital_pct
+        post["Scenario"]["Site"]["LoadProfileBoilerFuel"]["doe_reference_name"] = "Hospital"
+        post["Scenario"]["Site"]["LoadProfileChillerThermal"]["doe_reference_name"] = "Hospital"
+
+        resp = self.get_response(data=post)
+        self.assertHttpCreated(resp)
+        r = json.loads(resp.content)
+        run_uuid = r.get('run_uuid')
+        d = ModelManager.make_response(run_uuid=run_uuid)
+
+        lp_hospital = d['outputs']['Scenario']['Site']['LoadProfile']["year_one_electric_load_series_kw"]
+        lpbf_hospital = d['outputs']['Scenario']['Site']['LoadProfileBoilerFuel']["year_one_boiler_fuel_load_series_mmbtu_per_hr"]
+        lpct_hospital = d['outputs']['Scenario']['Site']['LoadProfileChillerThermal']['year_one_chiller_electric_load_series_kw']
+        lpct_hospital_frac_of_total = [lpct_hospital[i] / lp_hospital[i] for i in range(len(lp_hospital))]
+
+        # Hotel only
+        post["Scenario"]["Site"]["LoadProfile"]["annual_kwh"] = hotel_pct
+        post["Scenario"]["Site"]["LoadProfile"]["doe_reference_name"] = "LargeHotel"
+        post["Scenario"]["Site"]["LoadProfileBoilerFuel"]["annual_mmbtu"] = hotel_pct
+        post["Scenario"]["Site"]["LoadProfileBoilerFuel"]["doe_reference_name"] = "LargeHotel"
+        post["Scenario"]["Site"]["LoadProfileChillerThermal"]["doe_reference_name"] = "LargeHotel"
+
+        resp = self.get_response(data=post)
+        self.assertHttpCreated(resp)
+        r = json.loads(resp.content)
+        run_uuid = r.get('run_uuid')
+        d = ModelManager.make_response(run_uuid=run_uuid)
+
+        lp_largehotel = d['outputs']['Scenario']['Site']['LoadProfile']["year_one_electric_load_series_kw"]
+        lpbf_largehotel = d['outputs']['Scenario']['Site']['LoadProfileBoilerFuel']["year_one_boiler_fuel_load_series_mmbtu_per_hr"]
+        lpct_largehotel = d['outputs']['Scenario']['Site']['LoadProfileChillerThermal']['year_one_chiller_electric_load_series_kw']
+        lpct_largehotel_frac_of_total = [lpct_largehotel[i] / lp_largehotel[i] for i in range(len(lp_largehotel))]
+
+        # Hybrid mix of hospital and hotel
+        annual_energy = hospital_pct + hotel_pct
+        building_list = ["Hospital", "LargeHotel"]
+        percent_share_list = [hospital_pct, hotel_pct]
+        post["Scenario"]["Site"]["LoadProfile"]["annual_kwh"] = annual_energy
+        post["Scenario"]["Site"]["LoadProfile"]["doe_reference_name"] = building_list
+        post["Scenario"]["Site"]["LoadProfile"]["percent_share"] = percent_share_list
+        
+        post["Scenario"]["Site"]["LoadProfileBoilerFuel"]["annual_mmbtu"] = annual_energy
+        post["Scenario"]["Site"]["LoadProfileBoilerFuel"]["doe_reference_name"] = building_list
+        post["Scenario"]["Site"]["LoadProfileBoilerFuel"]["percent_share"] = percent_share_list
+
+        # API should now use a weighted fraction of total electric profile if no annual_tonhour is provided
+        post["Scenario"]["Site"]["LoadProfileChillerThermal"]["doe_reference_name"] = building_list
+        post["Scenario"]["Site"]["LoadProfileChillerThermal"]["percent_share"] = percent_share_list
+
+        resp = self.get_response(data=post)
+        self.assertHttpCreated(resp)
+        r = json.loads(resp.content)
+        run_uuid = r.get('run_uuid')
+        d = ModelManager.make_response(run_uuid=run_uuid)
+
+        lp_combined = d['outputs']['Scenario']['Site']['LoadProfile']["year_one_electric_load_series_kw"]
+        lpbf_combined = d['outputs']['Scenario']['Site']['LoadProfileBoilerFuel']["year_one_boiler_fuel_load_series_mmbtu_per_hr"]
+        lpct_combined = d['outputs']['Scenario']['Site']['LoadProfileChillerThermal']['year_one_chiller_electric_load_series_kw']
+
+        # Check that the combined/hybrid load is the same as the sum of the individual loads in each timestep
+        self.assertAlmostEqual(0.0, sum([lp_combined[i] - (lp_hospital[i] + lp_largehotel[i]) for i in range(len(lp_combined))]), places=3)
+        self.assertAlmostEqual(0.0, sum([lpbf_combined[i] - (lpbf_hospital[i] + lpbf_largehotel[i]) for i in range(len(lpbf_combined))]), places=3)
+        
+        # Check that the cooling load is the weighted average of the default CRB fraction of total electric profiles
+        lpct_combined_expected = [lp_combined[i] * (lpct_hospital_frac_of_total[i] * hospital_pct / 100.0 + 
+                                                    lpct_largehotel_frac_of_total[i] * hotel_pct / 100.0)
+                                                    for i in range(len(lp_combined))]
+        self.assertAlmostEqual(0.0, sum([lpct_combined_expected[i] - lpct_combined[i] for i in range(len(lpct_combined))]), places=3)
+
+        
+
+
+
+
