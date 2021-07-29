45adff68916761b33c9120fe77fbe0d1bb30dd26
adfarth <adfarth@umich.edu>
2021-07-29 12:14:55 -0600

    update lifetime_emissions test

--- reo/tests/test_lifetime_emissions.py
+++ reo/tests/test_lifetime_emissions.py
@@ -172,9 +172,9 @@ class TestNOx(ResourceTestCaseMixin, TestCase):
               },
               "Financial": {
                 "escalation_pct": 0.026,
-                "offtaker_discount_pct": 0.081,
+                "offtaker_discount_pct": 0.083,
                 "value_of_lost_load_us_dollars_per_kwh": 100.0,
-                "analysis_years": 20,
+                "analysis_years": 25,
                 "microgrid_upgrade_cost_pct": 0.3,
                 "offtaker_tax_pct": 0.26,
                 "om_cost_escalation_pct": 0.025,
@@ -214,6 +214,75 @@ class TestNOx(ResourceTestCaseMixin, TestCase):
           }
         }
 
+        self.post_no_techs = {
+          "Scenario": {
+            "time_steps_per_hour": 1,
+            "include_climate_in_objective": True,
+            "include_health_in_objective": True,
+            "Site": {
+              "longitude": -93.2650,
+              "latitude": 44.9778,
+              "roof_squarefeet": None,
+              "land_acres": None,
+              "PV": {
+                "max_kw": 0.0,
+                "min_kw": 0.0,
+              },
+              "Generator": {
+                "max_kw": 0.0,
+                "min_kw": 0.0,
+                "existing_kw": 0.0,
+              },
+              "LoadProfile": {
+                "critical_loads_kw_is_net": False,
+                "year": 2020,
+                "loads_kw_is_net": True,
+                "outage_start_time_step": None,
+                "outage_end_time_step": None,
+                "doe_reference_name": "FlatLoad",
+                "annual_kwh": 8760.0
+              },
+              "Storage": {
+                "max_kwh": 0.0,
+                "min_kw": 0.0,
+                "max_kw": 0.0,
+                "min_kwh": 0.0,
+              },
+              "ElectricTariff": {
+                ## "emissions_factor_series_lb_CO2_per_kwh": [1.0],
+                "add_blended_rates_to_urdb_rate": False,
+                "wholesale_rate_us_dollars_per_kwh": 0.0,
+                "net_metering_limit_kw": 0.0,
+                "interconnection_limit_kw": 100000000.0,
+                "urdb_utility_name": "",
+                "urdb_label": "",
+                "wholesale_rate_above_site_load_us_dollars_per_kwh": 0.0,
+                "urdb_rate_name": "custom",
+                "urdb_response": None,
+                "blended_monthly_rates_us_dollars_per_kwh": [0.15, 0.2, 0.21, 0.23, 0.27, 0.19, 0.22, 0.17, 0.24, 0.26, 0.18, 0.2],
+                "blended_monthly_demand_charges_us_dollars_per_kw": [0.08, 0.11, 0, 0, 0.15, 0.14, 0.09, 0.06, 0, 0, 0.05, 0]
+              },
+              "Financial": {
+                "escalation_pct": 0.026,
+                "offtaker_discount_pct": 0.081,
+                "value_of_lost_load_us_dollars_per_kwh": 100.0,
+                "analysis_years": 20,
+                "microgrid_upgrade_cost_pct": 0.3,
+                "offtaker_tax_pct": 0.26,
+                "om_cost_escalation_pct": 0.025,
+                "co2_cost_us_dollars_per_tonne": 51.0,
+                "nox_cost_us_dollars_per_tonne": 51.0, # TODO: change to values from EASIUR! (and also make seasonal?)
+                "so2_cost_us_dollars_per_tonne": 51.0, # TODO: change to values from EASIUR! 
+                "pm_cost_us_dollars_per_tonne": 51.0 # TODO: change to values from EASIUR! 
+              },
+              "Wind": {
+                "max_kw": 0.0,
+                "min_kw": 0.0
+              }
+            }
+          }
+        }
+
     def get_response(self, data):
         initial_post = self.api_client.post(self.submit_url, format='json', data=data)
         
@@ -226,7 +295,6 @@ class TestNOx(ResourceTestCaseMixin, TestCase):
     
 
     def test_emissions_modeling(self):
-        ## expected_pv_size = 42 #41.6667
 
         response = self.get_response(self.post)
         
@@ -283,6 +351,75 @@ class TestNOx(ResourceTestCaseMixin, TestCase):
             # self.assertEqual(pv_out['size_kw'], expected_pv_size,
             #                  "AC size ({}) does not equal expected AC size ({})."
             #                  .format(pv_out['size_kw'], expected_pv_size))
+
+            
+        except Exception as e:
+            error_msg = None
+            if hasattr(messages, "error"):
+                error_msg = messages.error
+            print("test_flex_tech API error message: {}".format(error_msg))
+            print("Run uuid: {}".format(response['outputs']['Scenario']['run_uuid']))
+            raise e
+
+    def test_no_techs(self):
+
+        response = self.get_response(self.post_no_techs)
+        
+        region_expected = 'Upper Midwest' # lat long is Minneapolis
+        CO2_lb_yr1_expected = 16962.85 
+        NOx_lb_yr1_expected = 11.66
+
+        region_out = response['outputs']['Scenario']['Site']['ElectricTariff']['emissions_region']
+        CO2_lb_yr1_out = response['outputs']['Scenario']['Site']['year_one_emissions_lb_CO2']
+        NOx_lb_yr1_out = response['outputs']['Scenario']['Site']['year_one_emissions_lb_NOx']
+        
+        messages = response['messages']
+
+        try:
+            self.assertEqual(region_out, region_expected,
+                             "Region ({}) does not equal expected Region ({})."
+                             .format(region_out, region_expected))
+            
+            self.assertEqual(CO2_lb_yr1_out, CO2_lb_yr1_expected,
+                             "lb CO2 in yr 1 ({}) expected lb CO2 in yr 1 ({})."
+                             .format(CO2_lb_yr1_out, CO2_lb_yr1_expected))
+            
+            self.assertEqual(NOx_lb_yr1_out, NOx_lb_yr1_expected,
+                             "lb NOx in yr 1 ({}) expected lb NOx in yr 1 ({})."
+                             .format(NOx_lb_yr1_out, NOx_lb_yr1_expected))
+
+            print('Emissions Region: ', response['outputs']['Scenario']['Site']['ElectricTariff']['emissions_region'])
+            print('PV size: ', response['outputs']['Scenario']['Site']['PV']['size_kw'])
+            print('Batt kWh: ', response['outputs']['Scenario']['Site']['Storage']['size_kwh'])
+            print('Batt kW: ', response['outputs']['Scenario']['Site']['Storage']['size_kw'])
+            print('Generator kW: ', response['outputs']['Scenario']['Site']['Generator']['size_kw'])
+            print('Annual Load kWh: ', response['outputs']['Scenario']['Site']["LoadProfile"]["annual_calculated_kwh"])
+            print('Year 1 kWh from grid: ', response['outputs']['Scenario']['Site']["ElectricTariff"]["year_one_energy_supplied_kwh"])
+            print('Year 1 kWh from grid BAU: ', response['outputs']['Scenario']['Site']["ElectricTariff"]["year_one_energy_supplied_kwh_bau"])
+            
+
+            for item in ['CO2', 'NOx', 'SO2', 'PM']:
+              print('Year 1 {}: '.format(item), response['outputs']['Scenario']['Site']['year_one_emissions_lb_{}'.format(item)])
+              # print('Year 1 {} BAU: '.format(item), response['outputs']['Scenario']['Site']['year_one_emissions_bau_lb_{}'.format(item)])
+              
+            print('fuel_used_gal: ', response['outputs']['Scenario']['Site']['Generator']['fuel_used_gal'])
+            print('year_one_CO2_emissions_from_fuelburn: ', response['outputs']['Scenario']['Site']['year_one_CO2_emissions_from_fuelburn'])
+            print('Lifetime lbs CO2: ', response['outputs']['Scenario']['Site']['lifetime_emissions_lb_CO2'])
+            # print('Lifetime lbs CO2 BAU: ', response['outputs']['Scenario']['Site']['lifetime_emissions_lb_CO2_bau'])
+            print('Lifetime cost CO2: ', response['outputs']['Scenario']['Site']['lifetime_emissions_cost_CO2'])
+            # print('Lifetime cost CO2 BAU: ', response['outputs']['Scenario']['Site']['lifetime_emissions_cost_CO2_bau'])
+
+            print('Lifetime cost Health: ', response['outputs']['Scenario']['Site']['lifetime_emissions_cost_Health'])
+            # print('Lifetime cost Health BAU: ', response['outputs']['Scenario']['Site']['lifetime_emissions_cost_Health_bau'])
+            
+            ## path = 'reo/tests/outputs/'
+            ## json.dump(response, open(path+'/'+"lifetime_emissions_results.json", "w"))
+
+            # output_df = pd.DataFrame()
+            # output_df['load'] = response['outputs']['Scenario']['Site']['LoadProfile']['year_one_electric_load_series_kw']
+            # output_df['load_met'] = response['outputs']['Scenario']['Site']['LoadProfile']['load_met_series_kw']
+
+            #output_df.to_csv('reo/tests/outputs/test_load_constraints.csv')
 
             
         except Exception as e:
