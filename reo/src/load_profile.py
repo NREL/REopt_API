29b8437b9553832c33c9bb45ad771ba5e20ecc69
bill-becker <William.Becker@nrel.gov>
2021-09-27 12:21:15 -0600

    Make def get_climate_zone_and_nearest_city a function in utilities.py
    Move from load_profile, and make DRY using this in BuiltInProfile.city property

--- reo/src/load_profile.py
+++ reo/src/load_profile.py
@@ -34,10 +34,8 @@ import pandas as pd
 import numpy as np
 from datetime import datetime, timedelta
 from collections import namedtuple
-from reo.utilities import degradation_factor
+from reo.utilities import degradation_factor, get_climate_zone_and_nearest_city
 import logging
-import geopandas as gpd
-from shapely import geometry as g
 from reo.exceptions import LoadProfileError
 log = logging.getLogger(__name__)
 import json
@@ -491,27 +489,9 @@ class BuiltInProfile(object):
         if self.nearest_city is None:
             # try shapefile lookup
             log.info("Trying city lookup by shapefile.")
-            gdf = gpd.read_file('reo/src/data/climate_cities.shp')
-            gdf = gdf[gdf.geometry.intersects(g.Point(self.longitude, self.latitude))]
-            if not gdf.empty:
-                self.nearest_city = gdf.city.values[0].replace(' ', '')
-            if self.nearest_city is None:
-                cities_to_search = self.default_cities
-            else:
-                climate_zone = [c for c in self.default_cities if c.name==self.nearest_city][0].zoneid                
-                cities_to_search = [c for c in self.default_cities if c.zoneid ==climate_zone]
-            if len(cities_to_search) > 1:
-                # else use old geometric approach, never fails...but isn't necessarily correct
+            self.climate_zone, self.nearest_city, geometric_flag = get_climate_zone_and_nearest_city(self.latitude, self.longitude, BuiltInProfile.default_cities)
+            if geometric_flag:
                 log.info("Using geometrically nearest city to lat/lng.")
-                min_distance = None
-                for i, c in enumerate(cities_to_search):
-                    distance = math.sqrt((self.latitude - c.lat) ** 2 + (self.longitude - c.lng) ** 2)
-                    if i == 0:
-                        min_distance = distance
-                        self.nearest_city = c.name
-                    elif distance < min_distance:
-                        min_distance = distance
-                        self.nearest_city = c.name
         return self.nearest_city
 
     @property
@@ -818,29 +798,3 @@ class LoadProfile(BuiltInProfile):
             return native_load, existing_pv_kw_list
         return copy.copy(self.unmodified_load_list), existing_pv_kw_list
 
-def get_climate_zone(latitude, longitude):
-    nearest_city = None
-    gdf = gpd.read_file('reo/src/data/climate_cities.shp')
-    gdf = gdf[gdf.geometry.intersects(g.Point(longitude, latitude))]
-    if not gdf.empty:
-        nearest_city = gdf.city.values[0].replace(' ', '')
-    if nearest_city is None:
-        cities_to_search = BuiltInProfile.default_cities
-    else:
-        climate_zone = [c for c in BuiltInProfile.default_cities if c.name==nearest_city][0].zoneid
-        cities_to_search = [c for c in BuiltInProfile.default_cities if c.zoneid==climate_zone]
-    if len(cities_to_search) > 1:
-        # else use old geometric approach, never fails...but isn't necessarily correct
-        min_distance = None
-        for i, c in enumerate(cities_to_search):
-            distance = math.sqrt((latitude - c.lat) ** 2 + (longitude - c.lng) ** 2)
-            if i == 0:
-                min_distance = distance
-                nearest_city = c.name
-            elif distance < min_distance:
-                min_distance = distance
-                nearest_city = c.name
-    
-    climate_zone = [c for c in BuiltInProfile.default_cities if c.name==nearest_city][0].zoneid
-
-    return climate_zone
