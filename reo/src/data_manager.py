270b5c67b89a4d5c866262d1fc70051373fa849b
adfarth <adfarth@umich.edu>
2021-11-16 14:30:29 -0700

    Update data_manager.py

--- reo/src/data_manager.py
+++ reo/src/data_manager.py
@@ -1841,7 +1841,7 @@ class DataManager:
             'SRrequiredPctLoad': self.load.sr_required_pct,
             'SRrequiredPctTechs': sr_required_pct,
             'OtherCapitalCosts': sf.other_capital_costs_us_dollars,
-            'OtherAnnualCosts': sf.other_annual_costs_us_dollars_per_year * pwf_om
+            'OtherAnnualCosts': sf.other_annual_costs_us_dollars_per_year * pwf_om,
             # Emissions
             'CO2_dollars_tonne': co2_cost_us_dollars_per_tonne,
             'NOx_dollars_tonne_grid': nox_cost_us_dollars_per_tonne_grid,
