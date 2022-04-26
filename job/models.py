a96594859dc5a01c64f94c44a356446b6c20615d
Nick Laws <nick.laws@nrel.gov>
2022-04-25 18:20:52 -0600

    pass run_uuid to run_jump_model celery task
    instead of dict with all inputs. this way the redis queue does not have to hold a ton of data in memory

--- job/models.py
+++ job/models.py
@@ -2591,3 +2591,53 @@ class Message(BaseModel, models.Model):
     message = models.TextField(default='')
 
 # TODO other necessary models from reo/models.py
+
+
+def get_input_dict_from_run_uuid(run_uuid:str):
+    """
+    Construct the input dict for REopt.run_reopt
+    """
+    # get inputs that are always created with one DB transaction
+    meta = APIMeta.objects.select_related(
+        "Settings",
+        'FinancialInputs', 
+        'SiteInputs',
+        'ElectricLoadInputs',
+        'ElectricTariffInputs',
+    ).get(run_uuid=run_uuid)
+
+    def filter_none_and_empty_array(d:dict):
+        return {k: v for (k, v) in d.items() if v not in [None, [], {}]}
+
+    d = dict()
+    d["user_uuid"] = meta.user_uuid
+    d["Settings"] = filter_none_and_empty_array(meta.Settings.dict)
+    d["Financial"] = filter_none_and_empty_array(meta.FinancialInputs.dict)
+    d["Site"] = filter_none_and_empty_array(meta.SiteInputs.dict)
+    d["ElectricLoad"] = filter_none_and_empty_array(meta.ElectricLoadInputs.dict)
+    d["ElectricTariff"] = filter_none_and_empty_array(meta.ElectricTariffInputs.dict)
+
+    # We have to try for the following objects because they may or may not be defined
+    try:
+        pvs = meta.PVInputs.all()
+        if len(pvs) == 1:
+            d["PV"] = filter_none_and_empty_array(pvs[0].dict)
+        elif len(pvs) > 1:
+            d["PV"] = []
+            for pv in pvs:
+                d["PV"].append(filter_none_and_empty_array(pv.dict))
+    except: pass
+
+    try: d["ElectricUtility"] = filter_none_and_empty_array(meta.ElectricUtilityInputs.dict)
+    except: pass
+
+    try: d["ElectricStorage"] = filter_none_and_empty_array(meta.ElectricStorageInputs.dict)
+    except: pass
+
+    try: d["Generator"] = filter_none_and_empty_array(meta.GeneratorInputs.dict)
+    except: pass
+
+    try: d["Wind"] = filter_none_and_empty_array(meta.WindInputs.dict)
+    except: pass
+    
+    return d
