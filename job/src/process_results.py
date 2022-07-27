31f25679dc01c0694cf70ac2eabab26d3fa7129a
hdunham <70401017+hdunham@users.noreply.github.com>
2022-07-27 17:22:19 -0600

    get update_inputs_in_database() working and call it

--- job/src/process_results.py
+++ job/src/process_results.py
@@ -28,7 +28,7 @@
 # OF THE POSSIBILITY OF SUCH DAMAGE.
 # *********************************************************************************
 from job.models import FinancialOutputs, APIMeta, PVOutputs, ElectricStorageOutputs, ElectricTariffOutputs,\
-    ElectricUtilityOutputs, GeneratorOutputs, ElectricLoadOutputs, WindOutputs
+    ElectricUtilityOutputs, GeneratorOutputs, ElectricLoadOutputs, WindOutputs, FinancialInputs, ElectricUtilityInputs
 import logging
 log = logging.getLogger(__name__)
 
@@ -65,14 +65,11 @@ def update_inputs_in_database(inputs_to_update: dict, run_uuid: str) -> None:
     which currently is those that use EASIUR or AVERT data.
     Called in job/run_jump_model (a celery task)
     """
-    meta = APIMeta.objects.get(run_uuid=run_uuid)
 
     try:
         # get input models that need updating
-        meta = APIMeta.objects.select_related(
-            'FinancialInputs',
-            'ElectricUtilityInputs'
-        ).get(run_uuid=run_uuid)
+        FinancialInputs.objects.filter(meta__run_uuid=run_uuid).update(**inputs_to_update["Financial"])
+        ElectricUtilityInputs.objects.filter(meta__run_uuid=run_uuid).update(**inputs_to_update["ElectricUtility"])
     except Exception as e:
             exc_type, exc_value, exc_traceback = sys.exc_info()
             debug_msg = "exc_type: {}; exc_value: {}; exc_traceback: {}".format(
@@ -81,5 +78,3 @@ def update_inputs_in_database(inputs_to_update: dict, run_uuid: str) -> None:
                                                                             tb.format_tb(exc_traceback)
                                                                         )
             log.debug(debug_msg)
-    meta.FinancialInputs.update(**inputs_to_update["Financial"])
-    meta.ElectricUtilityInputs.update(**inputs_to_update["ElectricUtility"])
--- job/src/run_jump_model.py
+++ job/src/run_jump_model.py
@@ -36,7 +36,7 @@ from celery import shared_task, Task
 from reo.exceptions import REoptError, OptimizationTimeout, UnexpectedError, NotOptimal, REoptFailedToStartError
 from job.models import APIMeta, Message, get_input_dict_from_run_uuid
 from reo.src.profiler import Profiler
-from job.src.process_results import process_results
+from job.src.process_results import process_results, update_inputs_in_database
 from celery.utils.log import get_task_logger
 logger = get_task_logger(__name__)
 
@@ -128,5 +128,6 @@ def run_jump_model(run_uuid):
 
     profiler.profileEnd()
     # TODO save profile times
+    update_inputs_in_database(inputs_with_defaults_set_in_julia, run_uuid)
     process_results(results, run_uuid)
     return True
