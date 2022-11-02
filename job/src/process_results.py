c32460bba430b6a6a4eb828755a9148afddc36d0
hdunham <70401017+hdunham@users.noreply.github.com>
2022-11-01 22:31:25 -0600

    add mg_storage_upgrade_cost to skipped Outage inputs

--- job/src/process_results.py
+++ job/src/process_results.py
@@ -41,7 +41,7 @@ def process_results(results: dict, run_uuid: str) -> None:
     """
     keys_to_skip = [
                     "storage_upgraded", "Generator_upgraded", "PV_upgraded",
-                    "discharge_from_storage_series", 
+                    "mg_storage_upgrade_cost", "discharge_from_storage_series", 
                     "PVmg_kw", "mg_PV_upgrade_cost", 
                     "mgPV_to_storage_series", "mgPV_curtailed_series", 
                     "mgPV_to_load_series", "Generatormg_kw",
