5c2362901b5b4c207e1e3c2f324a0b7767afb5b2
bill-becker <William.Becker@nrel.gov>
2022-11-10 12:20:40 -0700

    Add more http.jl endpoint tests
    Fix syntax error for CHP defaults from REopt.jl

--- job/test/test_http_endpoints.py
+++ job/test/test_http_endpoints.py
@@ -6,11 +6,9 @@ import logging
 import os
 import requests
 logging.disable(logging.CRITICAL)
-from unittest import skip
 
 class TestHTTPEndpoints(ResourceTestCaseMixin, TestCase):
 
-    @skip("TestHTTPEndpoints until we merge simulated-load into master")
     def test_chp_defaults(self):
 
         inputs = {"existing_boiler_production_type": "hot_water",
@@ -38,35 +36,52 @@ class TestHTTPEndpoints(ResourceTestCaseMixin, TestCase):
         self.assertEqual(http_response["size_class"], 4)
         self.assertGreater(http_response["chp_size_based_on_avg_heating_load_kw"], 3500.0)
 
-    @skip("TestHTTPEndpoints until we merge simulated-load into master")
+        # Check that size_class logic is the same, but we shifted it to 1-indexed instead of 0-indexed
+        # Modify input names for v2
+        inputs_v2 = {
+            "existing_boiler_production_type_steam_or_hw": inputs["existing_boiler_production_type"],
+            "avg_boiler_fuel_load_mmbtu_per_hr": inputs["avg_boiler_fuel_load_mmbtu_per_hour"]
+        }
+        resp = self.api_client.get(f'/v2/chp_defaults', data=inputs_v2)
+        v2_response = json.loads(resp.content)
+        self.assertEqual(http_response["size_class"], v2_response["size_class"]+1)
+
     def test_simulated_load(self):
 
-        inputs = {"existing_boiler_production_type": "hot_water",
-                "avg_boiler_fuel_load_mmbtu_per_hour": 28.0
+        # Test heating load because REopt.jl separates SpaceHeating and DHW, so had to aggregate for this endpoint
+        inputs = {"load_type": "heating",
+                "doe_reference_name": "Hospital",
+                "latitude": 37.78,
+                "longitude": -122.45
         }
 
         # Direct call of the http.jl endpoint /chp_defaults
         julia_host = os.environ.get('JULIA_HOST', "julia")
-        response = requests.get("http://" + julia_host + ":8081/chp_defaults/", json=inputs)
+        response = requests.get("http://" + julia_host + ":8081/simulated_load/", json=inputs)
         http_response = response.json()
 
-        # Call to the django view endpoint /chp_defaults which calls the http.jl endpoint
-        resp = self.api_client.get(f'/dev/chp_defaults', data=inputs)
-        view_response = json.loads(resp.content)
+        # Call to the v2 /simulated_load to check for consistency
+        resp = self.api_client.get(f'/v2/simulated_load', data=inputs)
+        v2_response = json.loads(resp.content)     
+        self.assertAlmostEqual(http_response["annual_mmbtu"], v2_response["annual_mmbtu"], delta=1.0)
 
-        mismatch = []
-        for k, v in http_response["default_inputs"].items():
-            if v != view_response["default_inputs"][k]:
-                mismatch.append(k)
+        # Test blended/hybrid buildings
+        inputs["load_type"] = "electric"
+        inputs["annual_kwh"] = 1.5E7
+        inputs["doe_reference_name[0]"] = "Hospital"
+        inputs["doe_reference_name[1]"] = "LargeOffice"
+        inputs["percent_share[0]"] = 25.0
+        inputs["percent_share[1]"] = 100.0 - inputs["percent_share[0]"]
         
-        self.assertEqual(mismatch, [])        
+        # Direct call of the http.jl endpoint /chp_defaults
+        julia_host = os.environ.get('JULIA_HOST', "julia")
+        response = requests.get("http://" + julia_host + ":8081/simulated_load/", json=inputs)
+        http_response = response.json()
 
-        # Check the endpoint logic with the expected selection
-        self.assertEqual(http_response["prime_mover"], "combustion_turbine")
-        self.assertEqual(http_response["size_class"], 4)
-        self.assertGreater(http_response["chp_size_based_on_avg_heating_load_kw"], 3500.0)
-
-        
+        # Call to the v2 /simulated_load to check for consistency
+        resp = self.api_client.get(f'/v2/simulated_load', data=inputs)
+        v2_response = json.loads(resp.content)     
+        self.assertAlmostEqual(http_response["annual_kwh"], v2_response["annual_kwh"], delta=1.0)        
 
 
 # For POSTing to an endpoint which returns a `run_uuid` to later GET the results from the database
