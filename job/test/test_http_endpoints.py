6e0d43fcb4f301d3f4d0867c6ed49bbba146c6e2
zolanaj <alexander.zolan@nrel.gov>
2022-11-28 22:09:33 -0700

    fix bad inputs test

--- job/test/test_http_endpoints.py
+++ job/test/test_http_endpoints.py
@@ -87,9 +87,9 @@ class TestHTTPEndpoints(ResourceTestCaseMixin, TestCase):
         inputs["invalid_key"] = "invalid_val"
         resp = self.api_client.get(f'/v2/simulated_load', data=inputs)
         v2_response = json.loads(resp.content)   
-        assert("invalid_key is not a valid input parameter" in v2_response.keys())
+        assert("Error" in v2_response.keys())
 
-        inputs.pop("invalid_key!")
+        inputs.pop("invalid_key")
         inputs.pop("load_type")
         inputs.pop("doe_reference_name")
         resp = self.api_client.get(f'/v2/simulated_load', data=inputs)
