e368ae1e4f9a3cc3112a03eaf50f57057c6e4691
hdunham <70401017+hdunham@users.noreply.github.com>
2022-12-22 11:05:26 -0700

    remove "outage" from ...cumulative_outage_survival...

--- resilience_stats/tests/test_erp.py
+++ resilience_stats/tests/test_erp.py
@@ -85,7 +85,7 @@ class ERPTests(ResourceTestCaseMixin, TestCase):
     #     resp = self.get_results_sim(erp_run_uuid)
     #     results = json.loads(resp.content)
     #     #same as last test in julia
-    #     self.assertAlmostEqual(results["outputs"]["mean_cumulative_outage_survival_final_time_step"], 0.904242, places=4)
+    #     self.assertAlmostEqual(results["outputs"]["mean_cumulative_survival_final_time_step"], 0.904242, places=4)
 
     #     #remove inputs that override REopt results and run again
     #     for input_key in [
@@ -106,7 +106,7 @@ class ERPTests(ResourceTestCaseMixin, TestCase):
 
     #     resp = self.get_results_sim(erp_run_uuid)
     #     results = json.loads(resp.content)
-    #     self.assertAlmostEqual(results["outputs"]["mean_cumulative_outage_survival_final_time_step"], 0.817088, places=4)
+    #     self.assertAlmostEqual(results["outputs"]["mean_cumulative_survival_final_time_step"], 0.817088, places=4)
 
     def test_erp_with_no_opt(self):
         """
@@ -124,7 +124,7 @@ class ERPTests(ResourceTestCaseMixin, TestCase):
         resp = self.get_results_sim(erp_run_uuid)
         results = json.loads(resp.content)
 
-        self.assertAlmostEqual(results["outputs"]["unlimited_fuel_cumulative_outage_survival_final_time_step"][1], 0.557056, places=4)
-        self.assertAlmostEqual(results["outputs"]["cumulative_outage_survival_final_time_step"][1], 0.557056, places=4)
+        self.assertAlmostEqual(results["outputs"]["unlimited_fuel_cumulative_survival_final_time_step"][1], 0.557056, places=4)
+        self.assertAlmostEqual(results["outputs"]["cumulative_survival_final_time_step"][1], 0.557056, places=4)
         
-        self.assertAlmostEqual(results["outputs"]["mean_cumulative_outage_survival_final_time_step"], 0.990784, places=4)
+        self.assertAlmostEqual(results["outputs"]["mean_cumulative_survival_final_time_step"], 0.990784, places=4)
