02e0070925b256009281af25019fe696adffa4cc
zolanaj <alexander.zolan@nrel.gov>
2023-01-26 15:56:03 -0700

    Add AbsorptionChiller testing to test_job_endpoint.TestJobEndpoint

--- job/test/test_job_endpoint.py
+++ job/test/test_job_endpoint.py
@@ -166,6 +166,10 @@ class TestJobEndpoint(ResourceTestCaseMixin, TestCase):
             "ColdThermalStorage":{
                 "min_gal":2500,
                 "max_gal":2500
+            },
+            "AbsorptionChiller":{
+                "min_ton":10,
+                "max_ton":10
             }
         }
 
@@ -186,6 +190,7 @@ class TestJobEndpoint(ResourceTestCaseMixin, TestCase):
         self.assertIn("HeatingLoad", list(results.keys()))
         self.assertIn("HotThermalStorage", list(results.keys()))
         self.assertIn("ColdThermalStorage", list(results.keys()))
+        self.assertIn("AbsorptionChiller", list(results.keys()))
 
 
     def test_chp_defaults_from_julia(self):
