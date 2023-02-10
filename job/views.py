d69f6f8a95600d57ff3dc7cf3b74bea0f6e2b42a
Bill Becker <William.Becker@nrel.gov>
2023-02-09 22:01:59 -0700

    Add /ghp_efficiency_thermal_factors to job app

--- job/views.py
+++ job/views.py
@@ -442,3 +442,42 @@ def simulated_load(request):
                                                                             tb.format_tb(exc_traceback))
         log.debug(debug_msg)
         return JsonResponse({"Error": "Unexpected error in simulated_load endpoint. Check log for more."}, status=500)
+
+def ghp_efficiency_thermal_factors(request):
+    """
+    GET default GHP heating and cooling thermal efficiency factors based on the climate zone from the lat/long input
+    param: latitude: latitude of the site location
+    param: longitude: longitude of the site location
+    param: doe_reference_name: commercial reference building name
+    return: climate_zone: climate zone of the site location
+    return: heating_efficiency_thermal_factor: default value for GHP.heating_efficiency_thermal_factor
+    return: cooling_efficiency_thermal_factor: default value for GHP.cooling_efficiency_thermal_factor
+    """    
+    try:
+        latitude = float(request.GET['latitude'])  # need float to convert unicode
+        longitude = float(request.GET['longitude'])
+        doe_reference_name = request.GET['doe_reference_name']
+
+        inputs_dict = {"latitude": latitude,
+                        "longitude": longitude,
+                        "doe_reference_name": doe_reference_name}
+
+        julia_host = os.environ.get('JULIA_HOST', "julia")
+        http_jl_response = requests.get("http://" + julia_host + ":8081/ghp_efficiency_thermal_factors/", json=inputs_dict)
+        response = JsonResponse(
+            http_jl_response.json()
+        )
+        return response
+
+    except ValueError as e:
+        return JsonResponse({"Error": str(e.args[0])}, status=500)
+
+    except KeyError as e:
+        return JsonResponse({"Error. Missing": str(e.args[0])}, status=500)
+
+    except Exception:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        debug_msg = "exc_type: {}; exc_value: {}; exc_traceback: {}".format(exc_type, exc_value.args[0],
+                                                                            tb.format_tb(exc_traceback))
+        log.debug(debug_msg)
+        return JsonResponse({"Error": "Unexpected error in ghp_efficiency_thermal_factors endpoint. Check log for more."}, status=500)
