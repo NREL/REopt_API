9f9646bee2fece1d28a55af28c2a712a9a33c2ce
Rathod, Bhavesh <Bhavesh.Rathod@nrel.gov>
2023-03-04 17:38:13 -0500

    Validated summery and add summary_by_chunk

--- job/urls.py
+++ job/urls.py
@@ -38,7 +38,10 @@ urlpatterns = [
     re_path(r'^job/outputs/?$', views.outputs),
     re_path(r'^chp_defaults/?$', views.chp_defaults),
     re_path(r'^simulated_load/?$', views.simulated_load),
-    re_path(r'^user/(?P<run_uuid>[0-9a-f-]+)/summary/?$', views.summary),
+    re_path(r'^user/(?P<user_uuid>[0-9a-f-]+)/summary/?$', views.summary),
+    re_path(r'^user/(?P<user_uuid>[0-9a-f-]+)/summary_by_chunk/(?P<chunk>[0-9]+)/?$', views.summary_by_chunk),
+    # re_path(r'^user/(?P<user_uuid>[0-9a-f-]+)/unlink/(?P<run_uuid>[0-9a-f-]+)/?$', views.unlink),
+    # re_path(r'^user/(?P<user_uuid>[0-9a-f-]+)/addtorun/(?P<run_uuid>[0-9a-f-]+)/?$', views.add_user_uuid),
 
     re_path(r'^invalid_urdb/?$', reoviews.invalid_urdb),
     re_path(r'^emissions_profile/?$', reoviews.emissions_profile),
