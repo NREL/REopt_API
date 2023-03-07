21f9215b7a9b474f9809408b0785cf5b6e943c69
hdunham <70401017+hdunham@users.noreply.github.com>
2023-03-07 10:52:08 -0700

    update reopt_version in response meta data
    need to make this update process automatic or at least built into PR merge steps

--- resilience_stats/api.py
+++ resilience_stats/api.py
@@ -92,7 +92,7 @@ class ERPJob(ModelResource):
 
         meta_dict = {
             "run_uuid": erp_run_uuid,
-            "reopt_version": "0.18.0",
+            "reopt_version": "0.24.0",
             "status": "Validating..."
         }
 
