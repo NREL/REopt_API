6f4e9d0ad965058fa8abe00e882f0cafeb6199b2
hdunham <70401017+hdunham@users.noreply.github.com>
2023-03-24 11:43:20 -0600

    check outage_durations not empty before calc max

--- resilience_stats/api.py
+++ resilience_stats/api.py
@@ -166,9 +166,10 @@ class ERPJob(ModelResource):
                 ## Outage ##
                 critical_loads_kw = reopt_run_meta.ElectricLoadOutputs.dict["critical_load_series_kw"]
                 update_user_dict_with_values_from_reopt("Outage", {"critical_loads_kw": critical_loads_kw})
-                update_user_dict_with_values_from_reopt("Outage", {
-                    "max_outage_duration": max(reopt_run_meta.ElectricUtilityInputs.dict["outage_durations"])
-                })
+                if len(reopt_run_meta.ElectricUtilityInputs.dict["outage_durations"]) > 0:
+                    update_user_dict_with_values_from_reopt("Outage", {
+                        "max_outage_duration": max(reopt_run_meta.ElectricUtilityInputs.dict["outage_durations"])
+                    })
 
                 ## Generator ##
                 try:
