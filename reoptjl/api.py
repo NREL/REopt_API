36807483247705b0e5a20f5305add232805d465e
Bill Becker <42586683+Bill-Becker@users.noreply.github.com>
2023-10-04 08:52:28 -0600

    Revert "Add GHP proforma metrics and BAU thermal results"

--- reoptjl/api.py
+++ reoptjl/api.py
@@ -96,7 +96,7 @@ class Job(ModelResource):
         meta = {
             "run_uuid": run_uuid,
             "api_version": 3,
-            "reopt_version": "0.35.0",
+            "reopt_version": "0.32.7",
             "status": "Validating..."
         }
         bundle.data.update({"APIMeta": meta})
