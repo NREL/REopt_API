3c30cad96a9ba0733e95d9b3078a4a8162132362
bill-becker <William.Becker@nrel.gov>
2023-10-16 21:03:21 -0600

    Add expected increase in cost for new CHP default

--- reoptjl/test/test_job_endpoint.py
+++ reoptjl/test/test_job_endpoint.py
@@ -32,7 +32,7 @@ class TestJobEndpoint(ResourceTestCaseMixin, TransactionTestCase):
         self.assertAlmostEqual(results["Outages"]["expected_outage_cost"], 0.0, places=-2)
         self.assertAlmostEqual(sum(sum(np.array(results["Outages"]["unserved_load_per_outage_kwh"]))), 0.0, places=0)
         self.assertAlmostEqual(results["Outages"]["microgrid_upgrade_capital_cost"], 1971108, places=-2)
-        self.assertAlmostEqual(results["Financial"]["lcc"], 59658835, places=-3)
+        self.assertAlmostEqual(results["Financial"]["lcc"], 59865241, places=-3)
 
     def test_pv_battery_and_emissions_defaults_from_julia(self):
         """
