e13b9f9a2af2f2af2e521d4efc2b6b1e709aa7a2
bill-becker <William.Becker@nrel.gov>
2024-01-10 09:18:15 -0700

    Avoid /summary error with off-grid
    ElectricTariff doesn't exist for off-grid, and this was causing an error when trying to add None's together

--- reoptjl/views.py
+++ reoptjl/views.py
@@ -855,7 +855,10 @@ def queryset_for_summary(api_metas,summary_dict:dict):
     )
     if len(tariffOuts) > 0:
         for m in tariffOuts:
-            summary_dict[str(m.meta.run_uuid)]['year_one_savings_us_dollars'] = (m.year_one_energy_cost_before_tax_bau + m.year_one_demand_cost_before_tax_bau + m.year_one_fixed_cost_before_tax_bau + m.year_one_min_charge_adder_before_tax_bau) - (m.year_one_energy_cost_before_tax + m.year_one_demand_cost_before_tax + m.year_one_fixed_cost_before_tax + m.year_one_min_charge_adder_before_tax)
+            if len(tariffInputs) > 0:
+                summary_dict[str(m.meta.run_uuid)]['year_one_savings_us_dollars'] = (m.year_one_energy_cost_before_tax_bau + m.year_one_demand_cost_before_tax_bau + m.year_one_fixed_cost_before_tax_bau + m.year_one_min_charge_adder_before_tax_bau) - (m.year_one_energy_cost_before_tax + m.year_one_demand_cost_before_tax + m.year_one_fixed_cost_before_tax + m.year_one_min_charge_adder_before_tax)
+            else:
+                summary_dict[str(m.meta.run_uuid)]['year_one_savings_us_dollars'] = 0.0
 
     load = ElectricLoadInputs.objects.filter(meta__run_uuid__in=run_uuids).only(
         'meta__run_uuid',
