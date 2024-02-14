f8bca387a50615b62ac343efa4dfb7dd8c13cf40
bill-becker <William.Becker@nrel.gov>
2024-02-13 22:06:07 -0700

    Update solver_setup.sh

---
+++ julia_src/solver_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Instructions for setting up the solver
+# The $LICENSESERVER and $TRANSCRYPT_PASSWORD environment variables must be assigned or replaced
+git clone $LICENSESERVER_URL
+cd licenseserver
+# Only need transcrypt for local testing
+transcrypt/transcrypt --flush-credentials -y || true
+transcrypt/transcrypt -c aes-256-cbc -p "$TRANSCRYPT_PASSWORD" -y
+# Needed for deploys to NREL servers
+cp Dockerfile.xpress ..
+# Only need for local testing
+cp docker-compose.xpress.yml ../..
