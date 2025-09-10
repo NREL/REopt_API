a7dd6fa4e0e2a56215633b4965e0e641e946c611
bill-becker <William.Becker@nrel.gov>
2025-09-10 13:54:36 -0600

    Add an example view for EVI-EnLitePy with internal I/O .json file

--- load_builder/views.py
+++ load_builder/views.py
@@ -6,7 +6,31 @@ import sys
 import numpy as np
 from django.http import JsonResponse
 from reo.exceptions import UnexpectedError
+import os
 
+def ensite_view(request):
+    """
+    Attempts to import ensitepy and return the SystemSimulator class.
+    If the package is not available, returns None.
+    """
+    try:
+        from ensitepy.simextension import enlitepyapi
+
+        inputs_path = os.path.join(os.getcwd(), "load_builder", "ensite.json")
+        with open(inputs_path, 'r') as f:
+            enlitepy_json = json.load(f)
+        # print(enlitepy_json)
+        # import remote_pdb
+        # remote_pdb.set_trace(host='0.0.0.0', port=4444)
+        results = enlitepyapi.run(enlitepy_json)
+
+        return JsonResponse({"message": "imported ensitepy and simextension successfully"})
+    except Exception as e:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        err = UnexpectedError(exc_type, exc_value, exc_traceback, task='ensite')
+        # err.save_to_db()
+        return JsonResponse({"Error": err.message}, status=500)
+    
 
 def check_load_builder_inputs(loads_table):
     required_inputs = ["Power (W)", "Quantity", "% Run Time", "Start Mo.", "Stop Mo.", "Start Hr.", "Stop Hr."]
