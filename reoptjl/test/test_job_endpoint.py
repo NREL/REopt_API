59de411650febf810d75321f7af42e67b6695860
Hallie Dunham <70401017+hdunham@users.noreply.github.com>
2025-10-03 17:34:23 -0600

    Update test_job_endpoint.py

--- reoptjl/test/test_job_endpoint.py
+++ reoptjl/test/test_job_endpoint.py
@@ -327,6 +327,7 @@ class TestJobEndpoint(ResourceTestCaseMixin, TransactionTestCase):
         r = json.loads(resp.content)
         
         # TODO: add number_of_boreholes output to API and calculate ghx_residual_value_present_value here
+        self.assertAlmostEqual(r["outputs"]["GHP"]["ghpghx_chosen_outputs"]["number_of_boreholes"], 0, delta=.1)
         total_ghx_ft = r["outputs"]["GHP"]["ghpghx_chosen_outputs"]["number_of_boreholes"] * r["outputs"]["GHP"]["ghpghx_chosen_outputs"]["length_boreholes_ft"]
         ghx_only_capital_cost = total_ghx_ft * r["inputs"]["GHP"]["installed_cost_ghx_per_ft"]
         useful_life = post['GHP']['ghx_useful_life_years']
@@ -337,7 +338,6 @@ class TestJobEndpoint(ResourceTestCaseMixin, TransactionTestCase):
                 (useful_life - analysis_years)/useful_life
             )/((1 + discount_rate)**analysis_years)
         # Note: this value is not independently calculated
-        self.assertAlmostEqual(r["outputs"]["GHP"]["ghpghx_chosen_outputs"]["number_of_boreholes"], 0, delta=.1)
         self.assertAlmostEqual(r["outputs"]["GHP"]["ghx_residual_value_present_value"], ghx_residual_value, delta=5)
 
     # def test_centralghp(self):
