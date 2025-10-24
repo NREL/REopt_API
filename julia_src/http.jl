8243d341d80eaed69001bbf2c2512da2a8c16630
bill-becker <William.Becker@nrel.gov>
2025-10-24 10:24:55 -0600

    Type conversions for simulated_load to be more specific, from Real to Float64

--- julia_src/http.jl
+++ julia_src/http.jl
@@ -548,10 +548,10 @@ function simulated_load(req::HTTP.Request)
                     "monthly_tonhour", "monthly_fraction", "addressable_load_fraction", "load_profile"]
     for key in vector_types
         if key in keys(d) && typeof(d[key]) <: Vector{}
-            d[key] = convert(Vector{Real}, d[key])
+            d[key] = convert(Vector{Float64}, d[key])
         elseif key in keys(d) && key == "addressable_load_fraction"
             # Scalar version of input, convert Any to Real
-            d[key] = convert(Real, d[key])
+            d[key] = convert(Float64, d[key])
         end
     end 
 
